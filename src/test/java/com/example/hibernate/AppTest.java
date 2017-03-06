a78b8ef5ac154046e3b12bf3712b758c3ca25025
weiwei <zhangweiwei@cfilmcloud.com>
2017-03-06 17:11:17 +0800

    使用hibernate5

--- src/test/java/com/example/hibernate/AppTest.java
+++ src/test/java/com/example/hibernate/AppTest.java
@@ -1,5 +1,7 @@
 package com.example.hibernate;
 
+import java.io.File;
+import java.net.URL;
 import java.util.List;
 
 import javax.persistence.Tuple;
@@ -8,11 +10,16 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Root;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 
 import com.example.hibernate.model.ZDoubanFilm;
@@ -43,4 +50,44 @@ public class AppTest {
 			e.printStackTrace();
 		}
 	}
+
+	private static final String local = "D:/docs/hibernate/userguide/";
+	private static final String remote = "http://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/";
+
+	@Test
+	public void downDocs() {
+		search(remote);
+		System.out.println("下载完成！");
+	}
+
+	private void search(String url) {
+		Document doc = null;
+		try {
+			System.out.println(url);
+			doc = Jsoup.connect(url).get();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			return;
+		}
+		Elements body = doc.body().select("table > tbody > tr > td > a");
+		if (body.isEmpty()) {
+			return;
+		}
+		body.stream().filter(a -> !a.text().contentEquals("Parent Directory")).forEach(b -> {
+			String path = b.attr("href");
+			String fullPath = url + path;
+			if (FilenameUtils.getExtension(path).isEmpty()) {
+				search(fullPath);
+			} else {
+				try {
+					URL source = new URL(fullPath);
+					File destination = new File(fullPath.replace(remote, local));
+					FileUtils.copyURLToFile(source, destination);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
 }
