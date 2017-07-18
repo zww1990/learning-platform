6fbf583c43243d23915f8075c015e4fb77c78eda
Alienware <Alienware>
2017-07-18 21:44:06 +0800

    nothing

--- src/test/java/com/example/hibernate/AnnotationTest.java
+++ src/test/java/com/example/hibernate/AnnotationTest.java
@@ -2,13 +2,21 @@ package com.example.hibernate;
 
 import java.lang.reflect.Method;
 import javax.persistence.Column;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Table;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.cfilmcloud.Application;
 
-/**
- * @author zww
- */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
 public class AnnotationTest {
+	@PersistenceContext
+	private EntityManager em;
+
 	@Test
 	public void test1() {
 		try {
@@ -17,7 +25,12 @@ public class AnnotationTest {
 			for (Method m : methods) {
 				Column c = m.getAnnotation(Column.class);
 				if (c != null) {
-					System.err.println("SELECT COUNT(1) FROM " + t.name() + " WHERE " + c.name() + " IS NULL;");
+					String sql = "SELECT COUNT(1) FROM " + t.name() + " WHERE " + c.name() + " IS NULL;";
+					Number res = (Number) this.em.createNativeQuery(sql).getSingleResult();
+					int count = res.intValue();
+					if (count > 0) {
+						System.err.println(m.getName());
+					}
 				}
 			}
 		} catch (Exception e) {
