b6b1580c7f01a97cbaf0fdac255aaa47d68c8651
Alienware <Alienware@DESKTOP-CFKAAT5>
2017-09-01 15:45:15 +0800

    nothing

--- src/test/java/com/cfilmcloud/test/GenuitecTest.java
+++ src/test/java/com/cfilmcloud/test/GenuitecTest.java
@@ -1,16 +1,19 @@
 package com.cfilmcloud.test;
 
 import java.io.File;
-import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.springframework.util.StringUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+@SuppressWarnings("unchecked")
 public class GenuitecTest {
 	@Test
 	public void download() {
@@ -23,14 +26,20 @@ public class GenuitecTest {
 				List<String> data = Arrays.stream(texts).map(x -> x.substring(x.indexOf('{'), x.lastIndexOf('}') + 1))
 						.collect(Collectors.toList());
 				String parent = "D:\\Projects\\zww\\mybatis\\src\\test\\resources";
-				String child = "genuitec%d.json";
-				for (int i = 0; i < data.size(); i++) {
-					File file = new File(parent, String.format(child, i + 1));
-					String value = data.get(i);
-					FileUtils.writeStringToFile(file, value, Charset.defaultCharset());
-				}
+				String child = "genuitec.json";
+				// File file = new File(parent, child);
+				// FileUtils.writeLines(file, data);
+				ObjectMapper om = new ObjectMapper();
+				Map<String, Object> json = om.readValue(data.get(1), Map.class);
+				Map<String, String> win = (Map<String, String>) json.get("win");
+				win.entrySet().stream().forEach(x -> System.out.println(x.getKey() + "=" + x.getValue()));
+				System.out.println("*******************************************************************");
+				url = win.get("url");
+				body = Jsoup.connect(url).timeout(10000).get().body();
+				String cssQuery = "div#page-container > div#et-main-area > div#main-content > div.MyEclipse.me_download > div.thanks > section#thanks_me.thanks_head > div.content a";
+				Elements elements = body.select(cssQuery);
+				elements.stream().forEach(x -> System.out.println(x.attr("href")));
 			}
-			System.out.println("OK!");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
