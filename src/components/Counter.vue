38d3f7c996df9047b47dfe655899098f498e15d5
weiwei <zww1990@foxmail.com>
2017-11-17 18:11:04 +0800

    vuex状态管理模式

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="counter">
       <h1>store.state.count={{count}}</h1>
+      <h1>store.getters.doneTodosCount={{doneTodosCount}}</h1>
       <button @click="clickMe">点我</button>
   </div>
 </template>
 <script>
+import {mapGetters} from 'vuex'
 export default {
   computed:{
       count(){
           return this.$store.state.count
-      }
+      },
+      ...mapGetters([
+          'doneTodos',
+          'doneTodosCount'
+      ])
   },
   methods:{
       clickMe(){
