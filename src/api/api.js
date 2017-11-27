b1f87361ca73db85d9aa63bbc2d9596e1f91dd42
weiwei <zww1990@foxmail.com>
2017-11-27 16:30:54 +0800

    调整el-dialog属性，在element2.0版本中，使用:visible.sync来控制dialog是否显示

--- src/api/api.js
+++ src/api/api.js
@@ -14,10 +14,18 @@ export const getUserListPage = params => {
   return axios.get(`${base}/listpage.json`, { params: params });
 };
 
-export const removeUser = params => { return axios.get(`${base}/user/remove`, { params: params }); };
+export const removeUser = params => {
+  return axios.get(`${base}/user.json`, { params: params });
+};
 
-export const batchRemoveUser = params => { return axios.get(`${base}/user/batchremove`, { params: params }); };
+export const batchRemoveUser = params => {
+  return axios.get(`${base}/user.json`, { params: params });
+};
 
-export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
+export const editUser = params => {
+  return axios.get(`${base}/user.json`, { params: params });
+};
 
-export const addUser = params => { return axios.get(`${base}/user/add`, { params: params }); };
+export const addUser = params => {
+  return axios.get(`${base}/user.json`, { params: params });
+};
