779bcfc55d30c13fbc21c979a810c006a4f2f667
weiwei <zww1990@foxmail.com>
2017-11-28 17:56:45 +0800

    添加网站收藏小图标

--- src/views/nav2/page4.vue
+++ src/views/nav2/page4.vue
@@ -1,12 +1,38 @@
 <template>
   <div>
-    <h1>页面4</h1>
+    <el-button type="primary" @click="openFullScreen" v-loading.fullscreen.lock="fullscreenLoading">
+      指令方式
+    </el-button>
+    <el-button type="primary" @click="openFullScreen2">
+      服务方式
+    </el-button>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      fullscreenLoading: false
+    };
+  },
+  methods: {
+    openFullScreen() {
+      this.fullscreenLoading = true;
+      setTimeout(() => {
+        this.fullscreenLoading = false;
+      }, 2000);
+    },
+    openFullScreen2() {
+      const loading = this.$loading({
+        lock: true,
+        text: "Loading",
+        spinner: "el-icon-loading",
+        background: "rgba(0, 0, 0, 0.7)"
+      });
+      setTimeout(() => {
+        loading.close();
+      }, 2000);
+    }
   }
 };
 </script>
