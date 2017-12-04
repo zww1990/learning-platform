909e2b0a5d3762aefccd0afdb585b3bb11cf7767
weiwei <zww1990@foxmail.com>
2017-12-04 13:42:15 +0800

    修改js

--- cas-server/cas-server-webapp/src/main/webapp/WEB-INF/view/jsp/default/ui/includes/bottom.jsp
+++ cas-server/cas-server-webapp/src/main/webapp/WEB-INF/view/jsp/default/ui/includes/bottom.jsp
@@ -30,7 +30,7 @@
 </footer>
 
 </div> <!-- END #container -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/headjs/1.0.3/head.min.js"></script>
+<script src="http://apps.bdimg.com/libs/headjs/1.0.3/head.min.js"></script>
 <spring:theme code="cas.javascript.file" var="casJavascriptFile" text="" />
 <script type="text/javascript" src="<c:url value="${casJavascriptFile}" />"></script>
 </body>
--- cas-server/cas-server-webapp/src/main/webapp/js/cas.js
+++ cas-server/cas-server-webapp/src/main/webapp/js/cas.js
@@ -17,10 +17,10 @@
  * under the License.
  */
 
-var scripts = [ "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js",
-    "https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js",
-    "https://cdn.rawgit.com/cowboy/javascript-debug/master/ba-debug.min.js"];
+var scripts = [ "http://apps.bdimg.com/libs/jquery/1.10.2/jquery.min.js",
+    "http://apps.bdimg.com/libs/jqueryui/1.10.4/jquery-ui.min.js",
+    "http://apps.bdimg.com/libs/jquery.cookie/1.4.1/jquery.cookie.min.js",
+    "js/ba-debug.min.js"];
 
 head.ready(document, function() {
     head.load(scripts, resourceLoadedSuccessfully);
