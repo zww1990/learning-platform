577db43aa224b8a7515d2aa649b170b2a3044e0e
weiwei <zww1990@foxmail.com>
2017-12-08 11:15:35 +0800

    update page5.vue

--- src/views/nav2/page5.vue
+++ src/views/nav2/page5.vue
@@ -1,32 +1,72 @@
 <template>
   <div>
-    <h1>页面5</h1>
-    <el-button type="primary" @click="onClick">点我</el-button>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>时间：{{now | datetime}}</span>
+        <el-button type="text" style="float: right; padding: 3px 0" @click="resetTime">重置时间</el-button>
+      </div>
+      <div v-for="time of times" :key="time" class="text item">
+        {{time}}
+      </div>
+    </el-card>
   </div>
 </template>
 <script>
-import axios from "axios";
 import moment from "moment";
+moment.locale("zh-cn");
 export default {
   data() {
     return {
-      param: {
-        id: 1,
-        name: "张三",
-        age: 23,
-        date: null
-      }
+      now: Date.now(),
+      times: []
     };
   },
   methods: {
-    onClick() {
-      this.param.date = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
-      console.log(JSON.stringify(this.param));
-      let url = "/demo/api/add";
-      axios.post(url, this.param).then(res => {
-        console.log(res.data);
-      });
+    showTimes() {
+      this.times = JSON.parse(sessionStorage.getItem("times")) || [];
+      setInterval(() => {
+        let time = moment(this.now).fromNow();
+        if (!this.times.includes(time)) {
+          this.times.push(time);
+          sessionStorage.setItem("times", JSON.stringify(this.times));
+        }
+      }, 1000);
+    },
+    resetTime() {
+      sessionStorage.removeItem("times");
+      this.times = [];
+      this.now = Date.now();
+    }
+  },
+  mounted() {
+    this.showTimes();
+  },
+  filters: {
+    datetime(value, format) {
+      if (!format) {
+        format = "YYYY-MM-DD HH:mm:ss";
+      }
+      return moment(value).format(format);
     }
   }
 };
 </script>
+<style scoped>
+.box-card {
+  width: 480px;
+}
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+.text {
+  font-size: 14px;
+}
+.item {
+  margin-bottom: 18px;
+}
+</style>
