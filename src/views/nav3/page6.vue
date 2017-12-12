db486baeff772a10125ec9890bd08e55ed939b1a
weiwei <zww1990@foxmail.com>
2017-12-12 18:46:58 +0800

    input+table实现组合下拉表格

--- src/views/nav3/page6.vue
+++ src/views/nav3/page6.vue
@@ -1,44 +1,60 @@
 <template>
-  <el-select v-model="value6" placeholder="请选择" multiple >
-    <el-option v-for="item in cities" :key="item.value" :label="item.label" :value="item.value">
-      <span style="float: left">{{ item.label }}</span>
-      <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span>
-    </el-option>
-  </el-select>
+  <div>
+    <el-popover ref="popover" placement="bottom-start" width="400" trigger="click">
+      <el-table :data="gridData" @selection-change="handleSelectionChange">
+        <el-table-column type="selection" width="50"></el-table-column>
+        <el-table-column width="150" property="date" label="日期"></el-table-column>
+        <el-table-column width="100" property="name" label="姓名"></el-table-column>
+        <el-table-column width="100" property="address" label="地址" show-overflow-tooltip></el-table-column>
+      </el-table>
+    </el-popover>
+    <el-input suffix-icon="el-icon-arrow-down" placeholder="请输入内容" v-model="input" clearable v-popover:popover></el-input>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      cities: [
+      input: "",
+      gridData: [
         {
-          value: "Beijing",
-          label: "北京"
+          date: "2016-05-02",
+          name: "王小虎1",
+          address: "上海市普陀区金沙江路 1518 弄",
+          id: 1
         },
         {
-          value: "Shanghai",
-          label: "上海"
+          date: "2016-05-04",
+          name: "王小虎2",
+          address: "上海市普陀区金沙江路 1518 弄",
+          id: 2
         },
         {
-          value: "Nanjing",
-          label: "南京"
+          date: "2016-05-01",
+          name: "王小虎3",
+          address: "上海市普陀区金沙江路 1518 弄",
+          id: 3
         },
         {
-          value: "Chengdu",
-          label: "成都"
-        },
-        {
-          value: "Shenzhen",
-          label: "深圳"
-        },
-        {
-          value: "Guangzhou",
-          label: "广州"
+          date: "2016-05-03",
+          name: "王小虎4",
+          address: "上海市普陀区金沙江路 1518 弄",
+          id: 4
         }
-      ],
-      value6: []
+      ]
     };
+  },
+  methods: {
+    handleSelectionChange(values) {
+      this.input = values.map(v => v.name).toString();
+    }
   }
 };
 </script>
+
+<style scoped>
+.el-input {
+  width: 200px;
+}
+</style>
