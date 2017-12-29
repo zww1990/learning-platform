4d96ecbb55d1d15c4488052f3b70a00b2ebad2c8
weiwei <zww1990@foxmail.com>
2017-12-29 13:40:24 +0800

    添加table行内编辑功能

--- src/views/nav1/user.vue
+++ src/views/nav1/user.vue
@@ -14,39 +14,44 @@
     <!--列表-->
     <template>
       <el-table :data="users" highlight-current-row v-loading="loading" style="width: 100%;" @cell-click="onCellDbclick">
-        <el-table-column type="index" width="60">
+        <el-table-column type="index" width="40">
         </el-table-column>
         <el-table-column prop="name" label="姓名" width="120" sortable>
           <template slot-scope="scope">
             <span v-show="!scope.row.editable">{{scope.row.name}}</span>
-            <el-input v-model="scope.row.name" placeholder="请输入姓名" v-show="scope.row.editable"></el-input>
+            <el-input v-model="scope.row.name" placeholder="请输入姓名" v-show="scope.row.editable" size="small"></el-input>
           </template>
         </el-table-column>
-        <el-table-column prop="sex" label="性别" width="150" sortable>
+        <el-table-column prop="sex" label="性别" width="100" sortable>
           <template slot-scope="scope">
             <span v-show="!scope.row.editable">{{formatSex(scope.row)}}</span>
-            <el-radio-group v-model="scope.row.sex" v-show="scope.row.editable">
-              <el-radio :label="1">男</el-radio>
-              <el-radio :label="0">女</el-radio>
-            </el-radio-group>
+            <el-select v-model="scope.row.sex" placeholder="请选择性别" v-show="scope.row.editable" size="small">
+              <el-option :value="1" :label="'男'"></el-option>
+              <el-option :value="0" :label="'女'"></el-option>
+            </el-select>
           </template>
         </el-table-column>
-        <el-table-column prop="age" label="年龄" width="200" sortable>
+        <el-table-column prop="age" label="年龄" width="150" sortable>
           <template slot-scope="scope">
             <span v-show="!scope.row.editable">{{scope.row.age}}</span>
-            <el-input-number v-model="scope.row.age" v-show="scope.row.editable" :min="1" :max="100"></el-input-number>
+            <el-input-number v-model="scope.row.age" v-show="scope.row.editable" :min="1" :max="100" controls-position="right" size="small"></el-input-number>
           </template>
         </el-table-column>
-        <el-table-column prop="birth" label="生日" width="250" sortable>
+        <el-table-column prop="birth" label="生日" width="240" sortable>
           <template slot-scope="scope">
             <span v-show="!scope.row.editable">{{scope.row.birth}}</span>
-            <el-date-picker v-model="scope.row.birth" placeholder="请选择日期" v-show="scope.row.editable"></el-date-picker>
+            <el-date-picker v-model="scope.row.birth" placeholder="请选择日期" v-show="scope.row.editable" :editable="false" :clearable="false" size="small" value-format="yyyy-MM-dd"></el-date-picker>
           </template>
         </el-table-column>
-        <el-table-column prop="addr" label="地址" min-width="200" sortable>
+        <el-table-column prop="addr" label="地址" min-width="180" sortable>
           <template slot-scope="scope">
             <span v-show="!scope.row.editable">{{scope.row.addr}}</span>
-            <el-input v-model="scope.row.addr" placeholder="请输入地址" v-show="scope.row.editable"></el-input>
+            <el-input v-model="scope.row.addr" placeholder="请输入地址" v-show="scope.row.editable" size="small"></el-input>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button type="primary" v-show="scope.row.editable" @click="onSaveClick(scope.row)">保存</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -85,7 +90,19 @@ export default {
       });
     },
     onCellDbclick(row, column, cell, event) {
+      if (column.label === "操作" || column.type === "index") {
+        return;
+      }
       row.editable = true;
+    },
+    onSaveClick(row) {
+      if (!row.name.trim()) {
+        return;
+      }
+      if (!row.addr.trim()) {
+        return;
+      }
+      row.editable = false;
     }
   },
   mounted() {
