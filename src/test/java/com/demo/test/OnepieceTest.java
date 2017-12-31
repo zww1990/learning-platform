124a7aa39d5add757be336034c5e373871c33d97
zww <zww1990@foxmail.com>
2017-12-31 15:51:19 +0800

    nothing

--- src/test/java/com/demo/test/OnepieceTest.java
+++ src/test/java/com/demo/test/OnepieceTest.java
@@ -1,6 +1,7 @@
 package com.demo.test;
 
 import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,17 +10,28 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Test;
-import org.springframework.core.io.ClassPathResource;
 
 public class OnepieceTest {
 	private static final String charsetName = "UTF-8";
-	private static final String PARENT = "D:\\Projects\\zww\\mybatis\\src\\test\\resources";
+	private static final String PARENT = "e:\\Projects\\zww\\mybatis\\src\\test\\resources";
+
+	@Test
+	public void download() {
+		try {
+			URL source = new URL("https://one-piece.cn/anime/");
+			File destination = new File(PARENT, "index.html");
+			FileUtils.copyURLToFile(source, destination);
+			System.out.println("OK!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Test
 	public void writeFile() {
 		try {
-			ClassPathResource resource = new ClassPathResource("index.html");
-			Element body = Jsoup.parse(resource.getFile(), charsetName).body();
+			File destination = new File(PARENT, "index.html");
+			Element body = Jsoup.parse(destination, charsetName).body();
 			String cssQuery = "body#anime > div#page > div#contents > div#main-area > article > div.contents-section-wrap > div.contents-section-inner > div#tab-main";
 			String html = body.select(cssQuery).html();
 			File file = new File(PARENT, "index2.html");
@@ -33,8 +45,8 @@ public class OnepieceTest {
 	@Test
 	public void readFile() {
 		try {
-			ClassPathResource resource = new ClassPathResource("index2.html");
-			Element body = Jsoup.parse(resource.getFile(), charsetName).body();
+			File destination = new File(PARENT, "index2.html");
+			Element body = Jsoup.parse(destination, charsetName).body();
 			Elements tables = body.select("div.chapter > table > tbody > tr");
 			List<String> titles = new ArrayList<String>();
 			for (Element tab : tables) {
