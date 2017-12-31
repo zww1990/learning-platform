3c90ef9e3af456589faf58790978e8b47b7e9570
zww <zww1990@foxmail.com>
2017-12-31 22:15:22 +0800

    nothing

--- src/test/java/com/demo/test/ExerciseSunlandsTest.java
+++ src/test/java/com/demo/test/ExerciseSunlandsTest.java
@@ -13,7 +13,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -24,15 +23,16 @@ public class ExerciseSunlandsTest {
 	private HttpHeaders headers = new HttpHeaders();
 	private ObjectMapper mapper = new ObjectMapper();
 	private static final String PARENT = "E:\\projects\\zww\\mybatis\\src\\test\\resources";
+	private String fileName;
 
 	@Before
 	public void init() {
 		headers.setContentType(MediaType.parseMediaType("application/x-www-form-urlencoded; charset=UTF-8"));
 		List<String> cookies = new ArrayList<>();
-//		cookies.add("Hm_lvt_042f1b4fd18a22ee217f0673c4c1b92f=1513400586");
 		cookies.add("JSESSIONID=6C1F638D87822070F1C219F65E30FE32");
 		cookies.add("stuToken=b3f0d0e152decd967f2804be17e9e576");
 		headers.put(HttpHeaders.COOKIE, cookies);
+		fileName = "retrievePaperUserRecords_1219";
 	}
 
 	@Test
@@ -40,12 +40,12 @@ public class ExerciseSunlandsTest {
 		try {
 			String url = "http://exercise.sunlands.com/exercise/student/retrievePaperUserRecords";
 			MultiValueMap<String, String> param = new LinkedMultiValueMap<>();
-			param.add("paperId", "7640");
-			param.add("recordId", "2041307");
+			param.add("paperId", "7503");
+			param.add("recordId", "2041972");
 			HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(param, headers);
 			Map<String, Object> map = this.restTemplate.postForObject(url, entity, Map.class);
 			List<Map<String, Object>> data = (List<Map<String, Object>>) map.get("data");
-			File file = new File(PARENT, "retrievePaperUserRecords_1220.json");
+			File file = new File(PARENT, fileName + ".json");
 			this.mapper.writeValue(file, data);
 			System.err.println("OK!");
 		} catch (Exception e) {
@@ -56,19 +56,11 @@ public class ExerciseSunlandsTest {
 	@Test
 	public void readJsonFile() {
 		try {
-			List<Map<String, Object>> data = this.mapper.readValue(new File(PARENT, "retrievePaperUserRecords_1220.json"),
-					List.class);
+			List<Map<String, Object>> data = this.mapper.readValue(new File(PARENT, fileName + ".json"), List.class);
 			List<String> lines = new ArrayList<>();
 			data.forEach(x -> {
 				Integer sequence = (Integer) x.get("sequence");// 序号
 				String content = (String) x.get("content");// 题目
-				// content = StringUtils.trimAllWhitespace(content);
-				// String correctAnswer = (String) x.get("correctAnswer");// 正确答案
-				// if (content.contains("（）")) {
-				// content = content.replace("（）", "（" + correctAnswer + "）");
-				// } else {
-				// content += "（" + correctAnswer + "）";
-				// }
 				List<Map<String, Object>> resPaperQuestionOptionsList = (List<Map<String, Object>>) x
 						.get("resPaperQuestionOptionsList");// 选项
 				lines.add(sequence + "、" + content);
@@ -79,7 +71,7 @@ public class ExerciseSunlandsTest {
 					lines.add("\t" + optionTitle + " " + _content + (isCorrect == 1 ? "（√）" : ""));
 				});
 			});
-			FileUtils.writeLines(new File(PARENT, "retrievePaperUserRecords_1220.txt"), lines);
+			FileUtils.writeLines(new File(PARENT, fileName + ".txt"), lines);
 			System.err.println("OK!");
 		} catch (Exception e) {
 			e.printStackTrace();
