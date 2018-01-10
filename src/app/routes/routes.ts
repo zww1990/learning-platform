d6477fe30e036a587514caf1cddff69903e4973e
zww1990 <zww1990@foxmail.com>
2018-01-10 16:51:50 +0800

    添加页面。

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -3,12 +3,20 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from '../layout/layout.component';
 import { LoginComponent } from '../login/login.component';
 import { AuthGuard } from '../auth/auth-guard';
+import { IndexComponent } from '../index/index.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    children: [
+      {
+        path: 'index',
+        component: IndexComponent
+      }
+    ]
   },
   {
     path: 'login',
