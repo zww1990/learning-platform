222544a1b27416ff60000290a698c18cdb8e0677
weiwei <zww1990@foxmail.com>
2018-01-23 22:16:08 +0800

    修改页面。

--- src/views/nav2/page1.vue
+++ src/views/nav2/page1.vue
@@ -98,9 +98,12 @@ export default {
         return;
       }
       const wb = new Workbook();
-      const ws = XLSX.utils.json_to_sheet(this.tableData, {
-        header: this.tableHeader
-      });
+      const ws = XLSX.utils.json_to_sheet(
+        [this.tableHeader, ...this.tableData],
+        {
+          skipHeader: true
+        }
+      );
       wb.SheetNames.push(this.params.mySheetName);
       wb.Sheets[this.params.mySheetName] = ws;
       const wbout = XLSX.write(wb, {
@@ -112,46 +115,6 @@ export default {
         new Blob([wbout], { type: 'application/octet-stream' }),
         this.params.myName + '-' + this.params.fileName
       );
-    },
-    sheet_from_array_of_arrays(data) {
-      const ws = {};
-      const range = { s: { c: 10000000, r: 10000000 }, e: { c: 0, r: 0 } };
-      for (let R = 0; R !== data.length; ++R) {
-        for (let C = 0; C !== data[R].length; ++C) {
-          if (range.s.r > R) range.s.r = R;
-          if (range.s.c > C) range.s.c = C;
-          if (range.e.r < R) range.e.r = R;
-          if (range.e.c < C) range.e.c = C;
-          let cell = { v: data[R][C] };
-          if (cell.v === null) {
-            continue;
-          }
-          let cellRef = XLSX.utils.encode_cell({ c: C, r: R });
-          if (typeof cell.v === 'number') {
-            cell.t = 'n';
-          } else if (typeof cell.v === 'boolean') {
-            cell.t = 'b';
-          } else if (cell.v instanceof Date) {
-            cell.t = 'n';
-            cell.z = XLSX.SSF._table[14];
-            cell.v = this.datenum(cell.v);
-          } else {
-            cell.t = 's';
-          }
-          ws[cellRef] = cell;
-        }
-      }
-      if (range.s.c < 10000000) {
-        ws['!ref'] = XLSX.utils.encode_range(range);
-      }
-      return ws;
-    },
-    datenum(v, date1904) {
-      if (date1904) {
-        v += 1462;
-      }
-      const epoch = Date.parse(v);
-      return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
     },
     beforeUpload(file) {
       this.params.fileName = file.name;
@@ -218,15 +181,7 @@ export default {
               '0',
               '990000',
               x[this.params.overtimeIndex],
-              x[this.params.deadlineIndex],
-              '',
-              '',
-              '',
-              '',
-              '',
-              '',
-              '',
-              ''
+              x[this.params.deadlineIndex]
             ]);
             if (this.isAfterTime(x[this.params.deadlineIndex])) {
               this.tableData.push([
@@ -251,12 +206,7 @@ export default {
                 x[this.params.deadlineIndex],
                 '公司（来广营朝来科技园）',
                 '',
-                this.params.myEndPoint,
-                '',
-                '',
-                '',
-                '',
-                ''
+                this.params.myEndPoint
               ]);
             }
           });
