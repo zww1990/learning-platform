132aa3be8433605da2821354b0e5a0c10d3f1897
zww <zww1990@foxmail.com>
2018-02-04 22:02:49 +0800

    xml to json

--- src/views/nav3/page9.vue
+++ src/views/nav3/page9.vue
@@ -1,26 +1,58 @@
 <template>
-  <el-card style="width:640px;">
-    <el-carousel type="card" :interval="5000" arrow="never">
-      <el-carousel-item v-for="(item,index) of images" :key="item">
-        <img :src="item" width="400" height="400" :alt="index">
-      </el-carousel-item>
-    </el-carousel>
-  </el-card>
+  <el-row>
+    <el-col :span="24">
+      <el-card>
+        <el-input v-model="xmlData" placeholder="请输入内容" type="textarea" clearable :autosize="true"></el-input>
+      </el-card>
+    </el-col>
+    <el-col :span="24">
+      <el-button type="primary" @click="xmlToJsonData">xml转换json</el-button>
+      <el-button type="danger" @click="cleanData">清空内容</el-button>
+      <el-input-number v-model="options.spaces" label="间距" :min="0" :max="10" controls-position="right" size="small"></el-input-number>
+      <el-switch v-model="options.compact" active-text="简化" inactive-text="非简化"></el-switch>
+      <el-switch v-model="options.trim" active-text="忽略首尾空格" inactive-text="保留首尾空格"></el-switch>
+      <el-switch v-model="options.ignoreComment" active-text="忽略注释" inactive-text="保留注释"></el-switch>
+      <el-switch v-model="options.ignoreDoctype" active-text="忽略文档类型" inactive-text="保留文档类型"></el-switch>
+      <el-switch v-model="options.ignoreDeclaration" active-text="忽略声明" inactive-text="保留声明"></el-switch>
+      <el-switch v-model="options.ignoreInstruction" active-text="忽略指令" inactive-text="保留指令"></el-switch>
+    </el-col>
+    <el-col :span="24">
+      <el-card>
+        <el-input v-model="jsonData" placeholder="" type="textarea" readonly :autosize="true"></el-input>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 <script>
+import xmlJs from 'xml-js';
 export default {
   data() {
     return {
-      images: [
-        '/static/images/001.jpg',
-        '/static/images/002.jpg',
-        '/static/images/003.jpg',
-        '/static/images/004.jpg',
-        '/static/images/005.jpg',
-        '/static/images/006.jpg',
-        '/static/images/007.jpg'
-      ]
+      xmlData: '',
+      jsonData: '',
+      options: {
+        compact: true, //简化
+        spaces: 2, //间距
+        trim: true, //忽略首尾空格
+        ignoreComment: true, //忽略注释
+        ignoreDoctype: true, //忽略文档类型
+        ignoreDeclaration: true, //忽略声明
+        ignoreInstruction: true //忽略指令
+      }
     };
+  },
+  methods: {
+    cleanData() {
+      this.xmlData = '';
+      this.jsonData = '';
+    },
+    xmlToJsonData() {
+      const value = this.xmlData.trim();
+      if (!value) {
+        return;
+      }
+      this.jsonData = xmlJs.xml2json(value, this.options);
+    }
   }
 };
 </script>
