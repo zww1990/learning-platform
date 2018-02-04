1f0b685740e2076b04c8c104f973589d9cbfaeb3
zww <zww1990@foxmail.com>
2018-02-04 15:20:36 +0800

    update package.

--- src/views/nav3/page9.vue
+++ src/views/nav3/page9.vue
@@ -1,8 +1,8 @@
 <template>
   <el-card style="width:640px;">
     <el-carousel type="card" :interval="5000" arrow="never">
-      <el-carousel-item v-for="item of images" :key="item">
-        <img :src="item" width="400" height="400">
+      <el-carousel-item v-for="(item,index) of images" :key="item">
+        <img :src="item" width="400" height="400" :alt="index">
       </el-carousel-item>
     </el-carousel>
   </el-card>
@@ -12,13 +12,13 @@ export default {
   data() {
     return {
       images: [
-        '/static/images/yuzexue/001.jpg',
-        '/static/images/yuzexue/002.jpg',
-        '/static/images/yuzexue/003.jpg',
-        '/static/images/yuzexue/004.jpg',
-        '/static/images/yuzexue/005.jpg',
-        '/static/images/yuzexue/006.jpg',
-        '/static/images/yuzexue/007.jpg'
+        '/static/images/001.jpg',
+        '/static/images/002.jpg',
+        '/static/images/003.jpg',
+        '/static/images/004.jpg',
+        '/static/images/005.jpg',
+        '/static/images/006.jpg',
+        '/static/images/007.jpg'
       ]
     };
   }
