7a13dd596feed36c67bf45c413efeb9ddfcc1ce1
zww1990 <zww1990@foxmail.com>
2018-02-15 00:03:44 +0800

    rebuild

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -8,6 +8,7 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { Page3Component } from './page3/page3.component';
 import { Page4Component } from './page4/page4.component';
+import { IndexComponent } from './index/index.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     canActivateChild: [AuthGuard],
     children: [
+      {
+        path: 'index',
+        component: IndexComponent
+      },
       {
         path: 'page1',
         component: Page1Component
@@ -43,6 +48,7 @@ const routes: Routes = [
 @NgModule({
   imports: [CommonModule, RouterModule.forRoot(routes)],
   declarations: [
+    IndexComponent,
     Page1Component,
     Page2Component,
     Page3Component,
--- src/app/routes/routes.route.ts
+++ src/app/routes/routes.route.ts
@@ -1,26 +1,11 @@
 import { Routes } from '@angular/router';
 
-import { LayoutComponent } from '../layout/layout.component';
 import { LoginComponent } from '../login/login.component';
-import { AuthGuard } from '../auth/auth-guard.service';
-import { IndexComponent } from '../index/index.component';
 
 export const routes: Routes = [
-  // {
-  //   path: '',
-  //   component: LayoutComponent,
-  //   canActivate: [AuthGuard],
-  //   canActivateChild: [AuthGuard],
-  //   children: [
-  //     {
-  //       path: '',
-  //       component: IndexComponent
-  //     }
-  //   ]
-  // },
   {
     path: '',
-    redirectTo: '/demo/page1',
+    redirectTo: '/demo/index',
     pathMatch: 'full'
   },
   {
