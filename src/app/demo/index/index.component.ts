dfe47f798cba9427118f7f8dfb2d5701de9a3660
zww <zww1990@foxmail.com>
2018-02-15 16:44:03 +0800

    nothing.

--- src/app/demo/index/index.component.ts
+++ src/app/demo/index/index.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+import { CanComponentDeactivate } from '../../auth/auth-guard.service';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.less']
 })
-export class IndexComponent implements OnInit {
+export class IndexComponent implements OnInit, CanComponentDeactivate {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  canDeactivate() {
+    const confirmation = window.confirm('你确定要离开首页?');
+    return Observable.of(confirmation);
   }
-
 }
