82e96ae282d5c2395f73852ea5d5fe7d7e269552
zww <zww1990@foxmail.com>
2018-02-21 00:26:21 +0800

    Vuetify

--- src/main.js
+++ src/main.js
@@ -4,8 +4,21 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store';
+import Vuetify from 'vuetify';
+import './main.styl';
 
 Vue.config.productionTip = false;
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#ee44aa',
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  }
+});
 
 /* eslint-disable no-new */
 new Vue({
