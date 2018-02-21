c1e4e21a9f73330e39434b0e25d9a04b5d663110
zww <zww1990@foxmail.com>
2018-02-21 21:25:19 +0800

    修改页面样式。

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,10 +4,10 @@
       <v-layout column align-center>
         <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
         <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
+          &#8220;what the fuck are you doing?&#8221;
           <footer>
             <small>
-              <em>&mdash;John Johnson</em>
+              <em>&mdash;&mdash;zhang wei wei</em>
             </small>
           </footer>
         </blockquote>
