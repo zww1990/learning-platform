33c661b766ce3706ffe43b50be227912a16e1756
zww1990 <zww1990@foxmail.com>
2018-02-25 22:34:17 +0800

    将平铺的菜单形式转换成树形的菜单形式。

--- src/app/layout/menu-item.model.ts
+++ src/app/layout/menu-item.model.ts
@@ -47,4 +47,31 @@ export class MenuItem {
     }
     return null;
   }
+
+  /**
+   * 将平铺的菜单形式转换成树形的菜单形式
+   * @param flatMenus 平铺的菜单
+   */
+  static queryTreeMenus(flatMenus: MenuItem[]): MenuItem[] {
+    const treeMenus: MenuItem[] = [];
+    this.flat2Tree(flatMenus, treeMenus);
+    return treeMenus;
+  }
+
+  /**
+   * 将平铺的菜单形式递归转换成树形的菜单形式
+   * @param flatMenus 平铺的菜单
+   * @param treeMenus 树形的菜单
+   */
+  private static flat2Tree(flatMenus: MenuItem[], treeMenus: MenuItem[]) {
+    flatMenus.forEach(menu => {
+      if (!menu.parentMenuId) {
+        treeMenus.push(menu);
+      }
+      menu.children = flatMenus.filter(m => m.parentMenuId === menu.menuId);
+      if (menu.children.length) {
+        this.flat2Tree(menu.children, treeMenus);
+      }
+    });
+  }
 }
