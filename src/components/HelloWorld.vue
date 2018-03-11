ea92eeb14753193ae12fb35ef5da89afdc3f52cf
zww <zww1990@foxmail.com>
2018-03-11 17:34:12 +0800

    update package

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
+      <v-layout column align-center ref="table2image">
         <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
         <blockquote>
           &#8220;what the fuck are you doing?&#8221;
@@ -11,14 +11,34 @@
             </small>
           </footer>
         </blockquote>
+        <v-btn color="info" @click="exportImage">导出表格为图片</v-btn>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
 
+<script>
+import html2canvas from 'html2canvas';
+import fileSaver from 'file-saver';
+export default {
+  methods: {
+    exportImage() {
+      html2canvas(this.$refs.table2image, { logging: false, scale: 2 }).then(
+        canvas => {
+          canvas.toBlob(blob => {
+            fileSaver.saveAs(blob, 'image.png');
+          });
+        }
+      );
+    }
+  }
+};
+</script>
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
