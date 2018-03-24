85c27182cd37690f603d54e04f26759ee036ea2f
zww1990 <zww1990@foxmail.com>
2018-03-24 21:33:25 +0800

    update package

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -33,39 +33,47 @@ export class LoginComponent implements OnInit {
    * @description 用户登录
    */
   submitForm() {
-    this.cas
-      .casCreateTGT(this.loginForm.value)
-      .then(res1 => {
-        const location = this.cas.parseLocation(res1);
-        this.cas
-          .casCreateST(location)
-          .then(res2 => {
-            this.cas.casServiceValidate(res2).then(res3 => {
-              const result = this.cas.parseXml(res3);
-              if (result.status) {
-                const user = new User(
-                  result.text,
-                  this.loginForm.get('username').value
-                );
-                sessionStorage.setItem(
-                  SessionKey.CAS_USER,
-                  JSON.stringify(user)
-                );
-                sessionStorage.setItem(SessionKey.CAS_TGT, location);
-                this.router.navigate(['']);
-              } else {
-                this.message.error(result.text);
-              }
-            });
-          })
-          .catch(err => {
-            this.message.error('创建TGT票据失败！');
-          });
-      })
-      .catch(err => {
-        this.message.error('用户名或密码错误！');
-      });
+    const user = new User(
+      this.loginForm.get('username').value,
+      this.loginForm.get('username').value
+    );
+    sessionStorage.setItem(SessionKey.CAS_USER, JSON.stringify(user));
+    this.router.navigate(['']);
   }
+  // submitForm() {
+  //   this.cas
+  //     .casCreateTGT(this.loginForm.value)
+  //     .then(res1 => {
+  //       const location = this.cas.parseLocation(res1);
+  //       this.cas
+  //         .casCreateST(location)
+  //         .then(res2 => {
+  //           this.cas.casServiceValidate(res2).then(res3 => {
+  //             const result = this.cas.parseXml(res3);
+  //             if (result.status) {
+  //               const user = new User(
+  //                 result.text,
+  //                 this.loginForm.get('username').value
+  //               );
+  //               sessionStorage.setItem(
+  //                 SessionKey.CAS_USER,
+  //                 JSON.stringify(user)
+  //               );
+  //               sessionStorage.setItem(SessionKey.CAS_TGT, location);
+  //               this.router.navigate(['']);
+  //             } else {
+  //               this.message.error(result.text);
+  //             }
+  //           });
+  //         })
+  //         .catch(err => {
+  //           this.message.error('创建TGT票据失败！');
+  //         });
+  //     })
+  //     .catch(err => {
+  //       this.message.error('用户名或密码错误！');
+  //     });
+  // }
 
   /**
    * @description 找回密码
@@ -76,17 +84,17 @@ export class LoginComponent implements OnInit {
       username.markAsDirty();
       return;
     }
-    const value = username.value.trim();
-    if (!isNaN(value)) {
-      this.message.error('您输入的用户名不正确！');
-      return;
-    }
-    const res = this.cas.casForgotPassword(value);
-    if (!res.data) {
-      this.message.error('您输入的用户名不存在！');
-      return;
-    }
-    const url = this.cas.casForgotPasswordUrl(res.data, value);
-    window.open(url);
+    // const value = username.value.trim();
+    // if (!isNaN(value)) {
+    //   this.message.error('您输入的用户名不正确！');
+    //   return;
+    // }
+    // const res = this.cas.casForgotPassword(value);
+    // if (!res.data) {
+    //   this.message.error('您输入的用户名不存在！');
+    //   return;
+    // }
+    // const url = this.cas.casForgotPasswordUrl(res.data, value);
+    // window.open(url);
   }
 }
