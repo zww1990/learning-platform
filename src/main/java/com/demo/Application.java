21daa080b0a796f9b965bb1962e2b037d8ba0812
zww <zww1990@foxmail.com>
2018-03-25 17:15:53 +0800

    升级spring boot 1.x至2.x

--- src/main/java/com/demo/Application.java
+++ src/main/java/com/demo/Application.java
@@ -1,13 +1,18 @@
 package com.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.dao.PersistenceExceptionTranslationAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication(exclude = { PersistenceExceptionTranslationAutoConfiguration.class })
+@SpringBootApplication
 public class Application {
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		log.info("应用程序上下文Bean定义计数={}", context.getBeanDefinitionCount());
 	}
 
 }
--- src/test/java/com/example/hibernate/HibernateTest.java
+++ src/test/java/com/example/hibernate/HibernateTest.java
@@ -2,60 +2,20 @@ package com.example.hibernate;
 
 import java.io.File;
 import java.net.URL;
-import java.util.List;
-
-import javax.persistence.Tuple;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Path;
-import javax.persistence.criteria.Root;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.junit.Test;
 
-import com.example.hibernate.model.ZDoubanFilm;
-
 /**
  * Unit test for simple App.
  */
 public class HibernateTest {
-	@Test
-	public void testApp() {
-		try {
-			StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder().configure();
-			StandardServiceRegistry registry = registryBuilder.build();
-			MetadataSources sources = new MetadataSources();
-			Metadata metadata = sources.buildMetadata(registry);
-			SessionFactory factory = metadata.buildSessionFactory();
-			Session session = factory.openSession();
-			CriteriaBuilder builder = session.getCriteriaBuilder();
-			CriteriaQuery<Tuple> query = builder.createQuery(Tuple.class);
-			Root<ZDoubanFilm> root = query.from(ZDoubanFilm.class);
-			Path<String> filmId = root.get("filmId");
-			Path<String> filmName = root.get("filmName");
-			query.multiselect(filmId, filmName);
-			query.where(builder.equal(root.get("year"), "2017"), builder.isNotNull(root.get("durations")));
-			query.orderBy(builder.desc(filmId));
-			List<Tuple> list = session.createQuery(query).getResultList();
-			list.stream().forEach(a -> {
-				System.out.println(a.get(filmId) + "\t" + a.get(filmName));
-			});
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
-	private static final String local = "D:/TDDOWNLOAD/hibernate/userguide/";
+	private static final String local = "E:/TDDOWNLOAD/hibernate/userguide/";
 	private static final String remote = "http://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/";
 
 	@Test
