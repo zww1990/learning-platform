600cce4b8c195e52c38436da19021f3d71dd41b8
weiwei <zww1990@foxmail.com>
2018-03-26 17:12:03 +0800

    enter commit message.

---
+++ src/test/java/com/wiwj/test/SpringTest.java
@@ -0,0 +1,28 @@
+package com.wiwj.test;
+
+import javax.annotation.Resource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.demo.config.SpringConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
+public class SpringTest {
+	@Resource
+	private ApplicationContext context;
+
+	@Test
+	public void testContextLoad() {
+		try {
+			String[] names = this.context.getBeanDefinitionNames();
+			for (int i = 0, len = names.length; i < len; i++) {
+				System.err.println((i + 1) + "\t" + names[i]);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
