949106dfdd8452cfbe07cd1d7c3e3d59af9643ec
weiwei <zww1990@foxmail.com>
2018-03-26 17:11:34 +0800

    enter commit message.

--- src/main/java/com/demo/config/AppProperties.java
+++ src/main/java/com/demo/config/AppProperties.java
@@ -13,6 +13,14 @@ public class AppProperties {
 	private String appName;
 	@Value("${dubbo.registry.address}")
 	private String registryAddress;
+	@Value("${jdbc.driver.class}")
+	private String jdbcDriverClass;
+	@Value("${jdbc.url}")
+	private String jdbcUrl;
+	@Value("${jdbc.user}")
+	private String jdbcUser;
+	@Value("${jdbc.password}")
+	private String jdbcPassword;
 
 	public String getAnnotationPackage() {
 		return annotationPackage;
@@ -36,5 +44,37 @@ public class AppProperties {
 
 	public void setRegistryAddress(String registryAddress) {
 		this.registryAddress = registryAddress;
+	}
+
+	public String getJdbcDriverClass() {
+		return jdbcDriverClass;
+	}
+
+	public void setJdbcDriverClass(String jdbcDriverClass) {
+		this.jdbcDriverClass = jdbcDriverClass;
+	}
+
+	public String getJdbcUrl() {
+		return jdbcUrl;
+	}
+
+	public void setJdbcUrl(String jdbcUrl) {
+		this.jdbcUrl = jdbcUrl;
+	}
+
+	public String getJdbcUser() {
+		return jdbcUser;
+	}
+
+	public void setJdbcUser(String jdbcUser) {
+		this.jdbcUser = jdbcUser;
+	}
+
+	public String getJdbcPassword() {
+		return jdbcPassword;
+	}
+
+	public void setJdbcPassword(String jdbcPassword) {
+		this.jdbcPassword = jdbcPassword;
 	}
 }
--- src/main/java/com/demo/config/SpringConfig.java
+++ src/main/java/com/demo/config/SpringConfig.java
@@ -1,18 +1,23 @@
 package com.demo.config;
 
+import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import com.alibaba.dubbo.config.ApplicationConfig;
 import com.alibaba.dubbo.config.RegistryConfig;
 import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 
 @Configuration
 @ComponentScan("com.demo")
-@DubboComponentScan("dubbo.annotation.package")
+@DubboComponentScan("com.demo.service.impl")
+@MapperScan("com.wiwj.bdm.mq.mapper")
 public class SpringConfig {
-
 	@Bean
 	public ApplicationConfig applicationConfig(AppProperties props) {
 		ApplicationConfig config = new ApplicationConfig();
@@ -25,5 +30,27 @@ public class SpringConfig {
 		RegistryConfig config = new RegistryConfig();
 		config.setAddress(props.getRegistryAddress());
 		return config;
+	}
+
+	@Bean
+	public DataSource dataSource(AppProperties props) {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(props.getJdbcDriverClass());
+		dataSource.setUrl(props.getJdbcUrl());
+		dataSource.setUsername(props.getJdbcUser());
+		dataSource.setPassword(props.getJdbcPassword());
+		return dataSource;
+	}
+
+	@Bean
+	public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(dataSource);
+		return bean.getObject();
 	}
 }
