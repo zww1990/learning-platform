09a11eeaf538112c04040f309e9b76d2a1de0a85
zww <zww1990@foxmail.com>
2018-04-05 17:29:05 +0800

    自定义mybatis文档注释生成器.

--- src/test/java/com/demo/test/MybatisTest.java
+++ src/test/java/com/demo/test/MybatisTest.java
@@ -2,6 +2,8 @@ package com.demo.test;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.junit.Test;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
@@ -10,15 +12,20 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 public class MybatisTest {
-	public static void main(String[] args) throws Exception {
-		List<String> warnings = new ArrayList<String>();
-		boolean overwrite = true;
-		Resource resource = new ClassPathResource("generatorConfig.xml");
-		ConfigurationParser cp = new ConfigurationParser(warnings);
-		Configuration config = cp.parseConfiguration(resource.getInputStream());
-		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-		myBatisGenerator.generate(null);
-		System.out.println("自动生成完毕！");
+	@Test
+	public void generate() {
+		try {
+			List<String> warnings = new ArrayList<String>();
+			boolean overwrite = true;
+			Resource resource = new ClassPathResource("generatorConfig.xml");
+			ConfigurationParser cp = new ConfigurationParser(warnings);
+			Configuration config = cp.parseConfiguration(resource.getInputStream());
+			DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+			MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+			myBatisGenerator.generate(null);
+			System.out.println("自动生成完毕！");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
