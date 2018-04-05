46d18e6d3821c6c7cd1c276ce9618f301ce27160
zww <zww1990@foxmail.com>
2018-04-05 16:28:36 +0800

    自定义mybatis文档注释生成器.

--- src/main/java/org/mybatis/generator/internal/MyDefaultCommentGenerator.java
+++ src/main/java/org/mybatis/generator/internal/MyDefaultCommentGenerator.java
@@ -1,8 +1,5 @@
 package org.mybatis.generator.internal;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.dom.java.Field;
@@ -11,26 +8,37 @@ import org.mybatis.generator.api.dom.java.Method;
 import org.mybatis.generator.api.dom.java.Parameter;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
 import org.mybatis.generator.api.dom.xml.XmlElement;
-import org.mybatis.generator.internal.util.StringUtility;
 
 /**
  * @author ZhangWeiWei
  * @date 2018年4月4日,上午9:29:29
  * @description 自定义mybatis文档注释生成器
  */
-@SuppressWarnings("serial")
 public class MyDefaultCommentGenerator extends DefaultCommentGenerator {
-	private static final Map<String, String> methodComments = new HashMap<String, String>() {
-		{
-			put("deleteByPrimaryKey", "按主键删除记录");
-			put("insert", "插入记录");
-			put("insertSelective", "选择性插入记录");
-			put("selectByPrimaryKey", "按主键查询记录");
-			put("updateByPrimaryKeySelective", "按主键选择性更新记录");
-			put("updateByPrimaryKeyWithBLOBs", "按主键更新记录包含所有BLOB类型的字段");
-			put("updateByPrimaryKey", "按主键更新记录排除所有BLOB类型的字段");
+	enum MethodComments {
+		/*** 按主键删除记录 */
+		deleteByPrimaryKey("按主键删除记录", true),
+		/*** 插入记录 */
+		insert("插入记录", false),
+		/*** 选择性插入记录 */
+		insertSelective("选择性插入记录", false),
+		/*** 按主键查询记录 */
+		selectByPrimaryKey("按主键查询记录", true),
+		/*** 按主键选择性更新记录 */
+		updateByPrimaryKeySelective("按主键选择性更新记录", false),
+		/*** 按主键更新记录包含所有BLOB类型的字段 */
+		updateByPrimaryKeyWithBLOBs("按主键更新记录包含所有BLOB类型的字段", false),
+		/*** 按主键更新记录排除所有BLOB类型的字段 */
+		updateByPrimaryKey("按主键更新记录排除所有BLOB类型的字段", false);
+		private String comment;
+		private boolean isPk;
+
+		private MethodComments(String comment, boolean isPk) {
+			this.comment = comment;
+			this.isPk = isPk;
 		}
-	};
+
+	}
 
 	@Override
 	public void addComment(XmlElement xmlElement) {
@@ -39,14 +47,21 @@ public class MyDefaultCommentGenerator extends DefaultCommentGenerator {
 
 	@Override
 	public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
-		String methodName = method.getName();
-		if (methodComments.containsKey(methodName)) {
-			StringBuilder sb = new StringBuilder();
+		MethodComments comment = MethodComments.valueOf(method.getName());
+		if (comment != null) {
+			StringBuilder sb1 = new StringBuilder();
 			method.addJavaDocLine("/**"); //$NON-NLS-1$
 			method.addJavaDocLine(" * @author ZhangWeiWei"); //$NON-NLS-1$
-			sb.append(" * @description "); //$NON-NLS-1$
-			sb.append(methodComments.get(methodName)); // $NON-NLS-1$
-			method.addJavaDocLine(sb.toString());
+			sb1.append(" * @description "); //$NON-NLS-1$
+			sb1.append(comment.comment); // $NON-NLS-1$
+			method.addJavaDocLine(sb1.toString());
+			StringBuilder sb2 = new StringBuilder();
+			Parameter parm = method.getParameters().get(0);
+			sb2.append(" * @param "); //$NON-NLS-1$
+			sb2.append(parm.getName());
+			sb2.append(" "); //$NON-NLS-1$
+			sb2.append(comment.isPk ? "主键" : introspectedTable.getRemarks());
+			method.addJavaDocLine(sb2.toString());
 			addJavadocTag(method, false);
 			method.addJavaDocLine(" */"); //$NON-NLS-1$
 		}
@@ -57,71 +72,60 @@ public class MyDefaultCommentGenerator extends DefaultCommentGenerator {
 		topLevelClass.addJavaDocLine("/**"); //$NON-NLS-1$
 		topLevelClass.addJavaDocLine(" * @author ZhangWeiWei"); //$NON-NLS-1$
 		String remarks = introspectedTable.getRemarks();
-		if (StringUtility.stringHasValue(remarks)) {
-			StringBuilder sb = new StringBuilder();
-			sb.append(" * @description ");
-			sb.append(remarks);
-			topLevelClass.addJavaDocLine(sb.toString()); // $NON-NLS-1$
-		}
+		StringBuilder sb = new StringBuilder();
+		sb.append(" * @description ");
+		sb.append(remarks);
+		topLevelClass.addJavaDocLine(sb.toString()); // $NON-NLS-1$
 		addJavadocTag(topLevelClass, true);
 		topLevelClass.addJavaDocLine(" */"); //$NON-NLS-1$
 	}
 
 	@Override
 	public void addJavadocTag(JavaElement javaElement, boolean markAsDoNotDelete) {
-		String s = getDateString();
-		if (s != null) {
-			StringBuilder sb = new StringBuilder();
-			sb.append(" * @date ");
-			sb.append(s);
-			javaElement.addJavaDocLine(sb.toString());
-		}
+		StringBuilder sb = new StringBuilder();
+		sb.append(" * @date ");
+		sb.append(getDateString());
+		javaElement.addJavaDocLine(sb.toString());
 	}
 
 	@Override
 	public void addFieldComment(Field field, IntrospectedTable introspectedTable,
 			IntrospectedColumn introspectedColumn) {
 		String remarks = introspectedColumn.getRemarks();
-		if (StringUtility.stringHasValue(remarks)) {
-			StringBuilder sb = new StringBuilder();
-			sb.append("/*** "); //$NON-NLS-1$
-			sb.append(trimAllWhitespace(remarks)); // $NON-NLS-1$
-			sb.append(" */"); //$NON-NLS-1$
-			field.addJavaDocLine(sb.toString()); // $NON-NLS-1$
-		}
+		StringBuilder sb = new StringBuilder();
+		sb.append("/*** "); //$NON-NLS-1$
+		sb.append(trimAllWhitespace(remarks)); // $NON-NLS-1$
+		sb.append(" */"); //$NON-NLS-1$
+		field.addJavaDocLine(sb.toString()); // $NON-NLS-1$
 	}
 
 	@Override
 	public void addGetterComment(Method method, IntrospectedTable introspectedTable,
 			IntrospectedColumn introspectedColumn) {
 		String remarks = introspectedColumn.getRemarks();
-		if (StringUtility.stringHasValue(remarks)) {
-			StringBuilder sb = new StringBuilder();
-			method.addJavaDocLine("/**"); //$NON-NLS-1$
-			sb.append(" * @return "); //$NON-NLS-1$
-			sb.append(remarks); // $NON-NLS-1$
-			method.addJavaDocLine(sb.toString());
-			addJavadocTag(method, false);
-			method.addJavaDocLine(" */"); //$NON-NLS-1$
-		}
+		StringBuilder sb = new StringBuilder();
+		method.addJavaDocLine("/**"); //$NON-NLS-1$
+		sb.append(" * @return "); //$NON-NLS-1$
+		sb.append(remarks); // $NON-NLS-1$
+		method.addJavaDocLine(sb.toString());
+		addJavadocTag(method, false);
+		method.addJavaDocLine(" */"); //$NON-NLS-1$
 	}
 
 	@Override
 	public void addSetterComment(Method method, IntrospectedTable introspectedTable,
 			IntrospectedColumn introspectedColumn) {
 		String remarks = introspectedColumn.getRemarks();
-		if (StringUtility.stringHasValue(remarks)) {
-			StringBuilder sb = new StringBuilder();
-			method.addJavaDocLine("/**"); //$NON-NLS-1$
-			Parameter parm = method.getParameters().get(0);
-			sb.append(" * @param "); //$NON-NLS-1$
-			sb.append(parm.getName());
-			sb.append(" "); //$NON-NLS-1$
-			sb.append(remarks);
-			method.addJavaDocLine(sb.toString());
-			addJavadocTag(method, false);
-			method.addJavaDocLine(" */"); //$NON-NLS-1$
-		}
+		StringBuilder sb = new StringBuilder();
+		method.addJavaDocLine("/**"); //$NON-NLS-1$
+		Parameter parm = method.getParameters().get(0);
+		sb.append(" * @param "); //$NON-NLS-1$
+		sb.append(parm.getName());
+		sb.append(" "); //$NON-NLS-1$
+		sb.append(remarks);
+		method.addJavaDocLine(sb.toString());
+		addJavadocTag(method, false);
+		method.addJavaDocLine(" */"); //$NON-NLS-1$
 	}
 
 	private static String trimAllWhitespace(String str) {
