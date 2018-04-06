947186139b0aeb6eb843e0546604fb7791fc0307
zww <zww1990@foxmail.com>
2018-04-06 16:28:25 +0800

    自定义mybatis文档注释生成器.

--- src/main/java/org/mybatis/generator/internal/MyDefaultCommentGenerator.java
+++ src/main/java/org/mybatis/generator/internal/MyDefaultCommentGenerator.java
@@ -23,16 +23,50 @@ import org.mybatis.generator.internal.util.StringUtility;
  */
 public class MyDefaultCommentGenerator extends DefaultCommentGenerator {
 	public enum MethodComments {
-		deleteByPrimaryKey("按主键删除记录", true), insert("插入记录", false), insertSelective("选择性插入记录",
-				false), selectByPrimaryKey("按主键查询记录", true), updateByPrimaryKeySelective("按主键选择性更新记录",
-						false), updateByPrimaryKeyWithBLOBs("按主键更新记录包含所有BLOB类型的字段",
-								false), updateByPrimaryKey("按主键更新记录", false);
+		/***  */
+		deleteByPrimaryKey("按主键删除记录", true, "主键"),
+		/***  */
+		insert("插入记录", false, "插入语句"),
+		/***  */
+		insertSelective("选择性插入记录", false, ""),
+		/***  */
+		selectByPrimaryKey("按主键查询记录", true, "主键"),
+		/***  */
+		updateByPrimaryKeySelective("按主键选择性更新记录", false, ""),
+		/***  */
+		updateByPrimaryKeyWithBLOBs("按主键更新记录包含所有BLOB类型的字段", false, ""),
+		/***  */
+		updateByPrimaryKey("按主键更新记录", false, ""),
+		/***  */
+		count("查询计数", false, "查询语句"),
+		/***  */
+		delete("删除记录", false, "删除语句"),
+		/***  */
+		selectOne("查询一条记录", false, "查询语句"),
+		/***  */
+		selectMany("查询多条记录", false, "查询语句"),
+		/***  */
+		update("更新记录", false, "更新语句"),
+		/***  */
+		countByExample("返回表中与指定示例对象相匹配的行数", false, ""),
+		/***  */
+		deleteByExample("删除表中与指定示例对象相匹配的行数", false, ""),
+		/***  */
+		selectByExample("查询表中与指定示例对象相匹配的记录", false, ""),
+		/***  */
+		selectDistinctByExample("查询表中与指定示例对象相匹配的不重复记录", false, ""),
+		/***  */
+		updateByExample("更新表中与指定示例对象相匹配的行数", false, ""),
+		/***  */
+		updateByExampleSelective("选择性更新表中与指定示例对象相匹配的行数", false, "");
 		private String comment;
 		private boolean isPk;
+		private String remark;
 
-		private MethodComments(String comment, boolean isPk) {
+		private MethodComments(String comment, boolean isPk, String remark) {
 			this.comment = comment;
 			this.isPk = isPk;
+			this.remark = remark;
 		}
 
 		public static MethodComments value(String name) {
@@ -104,7 +138,7 @@ public class MyDefaultCommentGenerator extends DefaultCommentGenerator {
 			sb2.append(params.get(0).getName());
 			sb2.append(" ");
 			if (comment != null) {
-				sb2.append(comment.isPk ? "主键" : introspectedTable.getRemarks());
+				sb2.append(comment.remark);
 			}
 			method.addJavaDocLine(sb2.toString());
 		}
