db809c4cbe21089af372313e05eacb9a29859df3
zww <zww1990@foxmail.com>
2018-04-07 19:50:39 +0800

    初始化工程

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,15 +2,7 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-/**
- * @description 开发环境配置
- */
+
 export const environment = {
-  production: false,
-  mode: 'DEV',
-  api: {
-    menu: {
-      queryMenus: '/assets/data/menu.json'
-    }
-  }
+  production: false
 };
