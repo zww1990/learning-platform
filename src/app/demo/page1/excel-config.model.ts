3bae9d055eb5efbd3c77d264874c51fb13d2f741
weiwei <zww1990@foxmail.com>
2018-04-23 19:24:40 +0800

    修改页面。

---
+++ src/app/demo/page1/excel-config.model.ts
@@ -0,0 +1,89 @@
+/**
+ * excel配置类
+ */
+export class ExcelConfig {
+  /**
+   * 员工号
+   */
+  myID: string;
+  /**
+   * 下车地点
+   */
+  myEndPoint: string;
+  /**
+   * 员工姓名
+   */
+  myName: string;
+  /**
+   * 员工号索引
+   */
+  idIndex: number;
+  /**
+   * 员工姓名索引
+   */
+  nameIndex: number;
+  /**
+   * 部门索引
+   */
+  deptIndex: number;
+  /**
+   * 审批单号索引
+   */
+  approvalIndex: number;
+  /**
+   * 加班日期索引
+   */
+  overtimeIndex: number;
+  /**
+   * 加班截止时间索引
+   */
+  deadlineIndex: number;
+  /**
+   * 员工加班记录明细页索引
+   */
+  empPageIndex: number;
+  /**
+   * 个人加班记录明细页索引
+   */
+  myPageIndex: number;
+  /**
+   * 解析字符串日期样式
+   */
+  dateParse: string;
+  /**
+   * 格式化日期样式
+   */
+  dateFormat: string;
+  /**
+   * 格式化时间样式
+   */
+  timeFormat: string;
+  /**
+   * 中国时区
+   */
+  timezone: string;
+  /**
+   * 规定打车开始时间
+   */
+  taxiTime: string;
+  /**
+   * sheet页名称
+   */
+  mySheetName: string;
+  /**
+   * 导出文件名称
+   */
+  fileName: string;
+  /**
+   * 餐费
+   */
+  mealFee: string[];
+  /**
+   * 交通费
+   */
+  trafficFee1: string[];
+  /**
+   * 交通费
+   */
+  trafficFee2: string[];
+}
