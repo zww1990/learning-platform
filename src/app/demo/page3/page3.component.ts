f857cf5e1ba74b0124503e6b1f2e01594bfe0b23
weiwei <zww1990@foxmail.com>
2018-05-21 09:40:26 +0800

    修改页面样式。

--- src/app/demo/page3/page3.component.ts
+++ src/app/demo/page3/page3.component.ts
@@ -6,10 +6,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page3.component.less']
 })
 export class Page3Component implements OnInit {
+  i = 1;
+  editCache = {};
+  dataSet = [
+    {
+      key: '0',
+      name: 'Edward King 0',
+      age: '32',
+      address: 'London, Park Lane no. 0'
+    },
+    {
+      key: '1',
+      name: 'Edward King 1',
+      age: '32',
+      address: 'London, Park Lane no. 1'
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.updateEditCache();
   }
 
+  updateEditCache(): void {
+    this.dataSet.forEach(item => {
+      if (!this.editCache[item.key]) {
+        this.editCache[item.key] = {
+          edit: false,
+          name: item.name
+        };
+      }
+    });
+  }
+
+  finishEdit(key: string): void {
+    this.editCache[key].edit = false;
+    this.dataSet.find(item => item.key === key).name = this.editCache[key].name;
+  }
+
+  startEdit(key: string): void {
+    this.editCache[key].edit = true;
+  }
+
+  deleteRow(i: string): void {
+    const dataSet = this.dataSet.filter(d => d.key !== i);
+    this.dataSet = dataSet;
+  }
+
+  addRow(): void {
+    this.i++;
+    this.dataSet = [
+      ...this.dataSet,
+      {
+        key: `${this.i}`,
+        name: `Edward King ${this.i}`,
+        age: '32',
+        address: `London, Park Lane no. ${this.i}`
+      }
+    ];
+    this.updateEditCache();
+  }
 }
