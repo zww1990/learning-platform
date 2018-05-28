042cbb24be98405448e9c94ca9ca6ffbe5ab5687
zww1990 <zww1990@foxmail.com>
2018-05-28 23:38:57 +0800

    集成Ant Design UI

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import Antd from 'vue-antd-ui';
+import zhCN from 'vue-antd-ui/lib/locale-provider/zh_CN';
+import 'moment/locale/zh-cn';
+import 'babel-polyfill';
+import App from './App';
+import router from './router';
+import './App.less';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Antd);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
-})
+  render: h => (
+    <a-locale-provider locale={zhCN}>
+      <App />
+    </a-locale-provider>
+  )
+});
