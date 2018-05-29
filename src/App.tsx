56146c4fe6e301553b30ab2cf18d2010e7945e4d
weiwei <zww1990@foxmail.com>
2018-05-29 16:40:29 +0800

    集成Ant Design UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import * as React from 'react';
+import { Button } from 'antd';
 import './App.css';
-
-import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Button type="primary">按钮</Button>
       </div>
     );
   }
