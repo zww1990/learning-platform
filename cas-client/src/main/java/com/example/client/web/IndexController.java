a6396c9f0231c61339de149ea40e2db27f31166a
zww1990 <zww1990@foxmail.com>
2018-06-24 21:47:06 +0800

    添加cas功能开关

--- cas-client/src/main/java/com/example/client/web/IndexController.java
+++ cas-client/src/main/java/com/example/client/web/IndexController.java
@@ -2,6 +2,8 @@ package com.example.client.web;
 
 import java.security.Principal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping
 public class IndexController {
+	private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+
 	@GetMapping("/")
 	public ModelAndView index(Principal principal) {
-		System.err.println("当前用户：" + principal);
+		log.info("当前用户：{}", principal);
 		ModelAndView mav = new ModelAndView("index");
 		mav.addObject("user", principal);
 		return mav;
