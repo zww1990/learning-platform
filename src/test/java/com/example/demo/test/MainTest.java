8baef24efb8478fd737413c3c8b9e8d634ce0880
weiwei <zww1990@foxmail.com>
2018-07-31 13:43:17 +0800

    enter commit message.

---
+++ src/test/java/com/example/demo/test/MainTest.java
@@ -0,0 +1,9 @@
+package com.example.demo.test;
+
+import java.util.TimeZone;
+
+public class MainTest {
+	public static void main(String[] args) {
+		System.err.println(TimeZone.getDefault());
+	}
+}
