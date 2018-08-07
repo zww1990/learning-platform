6faaf15dcf80329a6e76663bed0a31659bd71278
weiwei <zww1990@foxmail.com>
2018-08-07 10:19:12 +0800

    enter commit message.

--- src/main/java/com/demo/service/impl/CServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.demo.service.impl;
-
-import org.springframework.stereotype.Service;
-import com.demo.service.DemoService;
-
-@Service
-public class CServiceImpl implements DemoService {
-}
