ab8c2d1008a10ed7597777be2575152c594e43c5
weiwei <zww1990@foxmail.com>
2018-08-13 19:55:34 +0800

    修改组件库名称为ant-design-vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import Antd from 'vue-antd-ui';
-import zhCN from 'vue-antd-ui/lib/locale-provider/zh_CN';
+import Antd from 'ant-design-vue';
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN';
 import 'moment/locale/zh-cn';
 import 'babel-polyfill';
 import App from './App';
