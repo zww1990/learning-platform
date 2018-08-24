b63dd67387139e60dd9b597d391d3c36fedabb3c
weiwei <zww1990@foxmail.com>
2018-08-24 16:40:13 +0800

    测试发送邮件

--- src/main/java/com/example/springschedule/config/SpringConfig.java
+++ src/main/java/com/example/springschedule/config/SpringConfig.java
@@ -1,18 +1,23 @@
 package com.example.springschedule.config;
 
-import org.quartz.CronScheduleBuilder;
-import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 import com.example.springschedule.service.JobService;
 
 @Configuration
+@ComponentScan("com.example.springschedule")
 public class SpringConfig {
 	@Bean
 	public JavaMailSender mailSender() {
@@ -27,19 +32,44 @@ public class SpringConfig {
 	}
 
 	@Bean
-	public SchedulerFactoryBean quartzScheduler() {
-		return new SchedulerFactoryBean();
+	public SchedulerFactoryBean quartzScheduler(ApplicationContext context, Trigger[] triggers) {
+		SchedulerFactoryBean quartzScheduler = new SchedulerFactoryBean();
+		quartzScheduler.setJobFactory(new AutowireCapableBeanJobFactory(context.getAutowireCapableBeanFactory()));
+		quartzScheduler.setTriggers(triggers);
+		return quartzScheduler;
 	}
 
 	@Bean
-	public JobDetail jobDetail() {
+	public JobDetailFactoryBean jobDetail() {
 		Class<JobService> job = JobService.class;
-		return JobBuilder.newJob(job).withIdentity(job.getName()).storeDurably().build();
+		JobDetailFactoryBean jobDetail = new JobDetailFactoryBean();
+		jobDetail.setJobClass(job);
+		jobDetail.setName(job.getName());
+		return jobDetail;
 	}
 
 	@Bean
-	public Trigger trigger(JobDetail jobDetail) {
-		return TriggerBuilder.newTrigger().forJob(jobDetail).withIdentity(JobService.class.getName())
-				.withSchedule(CronScheduleBuilder.cronSchedule("*/10 * * * * ?")).build();
+	public CronTriggerFactoryBean trigger(JobDetail jobDetail) {
+		CronTriggerFactoryBean trigger = new CronTriggerFactoryBean();
+		trigger.setCronExpression("*/10 * * * * ?");
+		trigger.setJobDetail(jobDetail);
+		trigger.setName(jobDetail.getKey().getName());
+		return trigger;
+	}
+
+	protected class AutowireCapableBeanJobFactory extends SpringBeanJobFactory {
+		private final AutowireCapableBeanFactory beanFactory;
+
+		protected AutowireCapableBeanJobFactory(AutowireCapableBeanFactory beanFactory) {
+			this.beanFactory = beanFactory;
+		}
+
+		@Override
+		protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
+			Object jobInstance = super.createJobInstance(bundle);
+			this.beanFactory.autowireBean(jobInstance);
+			this.beanFactory.initializeBean(jobInstance, null);
+			return jobInstance;
+		}
 	}
 }
