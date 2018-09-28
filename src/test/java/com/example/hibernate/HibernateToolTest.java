e058e5ab7f59081dbc57b62cc016aca9630f6148
weiwei <zww1990@foxmail.com>
2018-09-28 14:10:44 +0800

    java调用hibernate-tools类库反向生成pojo。

--- src/test/java/com/example/hibernate/HibernateToolTest.java
+++ src/test/java/com/example/hibernate/HibernateToolTest.java
@@ -10,20 +10,19 @@ import org.hibernate.tool.ant.JDBCConfigurationTask;
 public class HibernateToolTest {
 	public static void main(String[] args) {
 		try {
-			HibernateToolTask hibernateToolTask = new HibernateToolTask();
-			JDBCConfigurationTask jdbcConfigurationTask = hibernateToolTask.createJDBCConfiguration();
-			jdbcConfigurationTask.setPackageName("com.demo.domain");
-			jdbcConfigurationTask.setConfigurationFile(
-					new File("D:\\Projects\\zww\\hibernate5\\src\\main\\resources\\hibernate.cfg.xml"));
-			jdbcConfigurationTask.setRevEngFile(new Path(new Project(), "src\\main\\resources\\hibernate.reveng.xml"));
-			Hbm2JavaExporterTask hbm2JavaExporterTask = (Hbm2JavaExporterTask) hibernateToolTask.createHbm2Java();
-			hbm2JavaExporterTask.setEjb3(true);
-			hbm2JavaExporterTask.setJdk5(true);
-			hbm2JavaExporterTask.setDestdir(new File("D:\\zzzzzzzzzz\\"));
 			Project project = new Project();
-			project.setBaseDir(new File("D:\\zzzzzzzzzz\\"));
-			hibernateToolTask.setProject(project);
-			hibernateToolTask.execute();
+			File baseDir = project.getBaseDir();
+			HibernateToolTask toolTask = new HibernateToolTask();
+			JDBCConfigurationTask configurationTask = toolTask.createJDBCConfiguration();
+			configurationTask.setPackageName("com.demo.domain");
+			configurationTask.setConfigurationFile(new File(baseDir, "src\\main\\resources\\hibernate.cfg.xml"));
+			configurationTask.setRevEngFile(new Path(project, "src\\main\\resources\\hibernate.reveng.xml"));
+			Hbm2JavaExporterTask exporterTask = (Hbm2JavaExporterTask) toolTask.createHbm2Java();
+			exporterTask.setEjb3(true);
+			exporterTask.setJdk5(true);
+			exporterTask.setDestdir(baseDir);
+			toolTask.setProject(project);
+			toolTask.execute();
 			System.exit(0);
 		} catch (Exception e) {
 			e.printStackTrace();
