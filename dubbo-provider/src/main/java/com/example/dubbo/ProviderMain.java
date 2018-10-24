e44b88ef309c2f7789f85ca66947b4e2068b428c
zww <zww1990@foxmail.com>
2018-10-24 19:46:13 +0800

    update package

--- dubbo-provider/src/main/java/com/example/dubbo/ProviderMain.java
+++ dubbo-provider/src/main/java/com/example/dubbo/ProviderMain.java
@@ -1,10 +1,7 @@
 package com.example.dubbo;
 
 import java.util.Arrays;
-
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 import com.example.dubbo.config.RootConfig;
 
 public class ProviderMain {
@@ -12,11 +9,13 @@ public class ProviderMain {
 		try {
 			AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(RootConfig.class);
 			context.start();
-			RabbitTemplate template = context.getBean(RabbitTemplate.class);
-			String exchange = "ex.demo";
-			template.convertAndSend(exchange, "rk.demo1", Arrays.asList("这是来自rk.demo1的消息"));
-			template.convertAndSend(exchange, "rk.demo2", Arrays.asList("这是来自rk.demo2的消息"));
-			System.in.read();
+//			RabbitTemplate template = context.getBean(RabbitTemplate.class);
+//			String exchange = "ex.demo";
+//			template.convertAndSend(exchange, "rk.demo1", Arrays.asList("这是来自rk.demo1的消息"));
+//			template.convertAndSend(exchange, "rk.demo2", Arrays.asList("这是来自rk.demo2的消息"));
+//			System.in.read();
+			System.err.println(context.getBeanDefinitionCount());
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.err::println);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
