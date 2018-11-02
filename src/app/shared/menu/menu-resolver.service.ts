1e95a0fa355a67f23957ba8d15a12ec117264a3c
zww <zww1990@foxmail.com>
2018-11-02 17:40:43 +0800

    添加菜单访问权限，以阻止在浏览器随意输入菜单链接。

--- src/app/shared/menu/menu-resolver.service.ts
+++ src/app/shared/menu/menu-resolver.service.ts
@@ -6,6 +6,7 @@ import {
 } from '@angular/router';
 import { MenuItem } from './menu-item.model';
 import { MenuService } from './menu.service';
+import { SessionKey } from '../session-key.enum';
 
 /**
  * @description 菜单解析器，导航前预先加载菜单列表
@@ -20,12 +21,20 @@ export class MenuResolverService implements Resolve<MenuItem[]> {
   constructor(private menuService: MenuService) {}
 
   /**
-   * @description 导航前预先加载菜单列表
+   * @description 导航前预先加载菜单列表，再将菜单链接存入浏览器sessionStorage中。
    * @param route 即将被激活的路由
    * @param state 即将到达的状态
    */
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     return this.menuService.queryMenus().then(resp => {
+      const userMenuUrl = resp.filter(v => !!v.menuUrl).map(v => v.menuUrl);
+      if (!userMenuUrl.includes('/demo/index')) {
+        userMenuUrl.push('/demo/index');
+      }
+      sessionStorage.setItem(
+        SessionKey.USER_MENU_URL,
+        JSON.stringify(userMenuUrl)
+      );
       return MenuItem.queryTreeMenus(resp);
     });
   }
--- src/app/shared/menu/menu.service.ts
+++ src/app/shared/menu/menu.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { MenuItem } from './menu-item.model';
+import { SessionKey } from '../session-key.enum';
 
 /**
  * @description 菜单服务
@@ -18,5 +19,13 @@ export class MenuService {
     return this.http
       .get<MenuItem[]>(environment.api.menu.queryMenus)
       .toPromise();
+  }
+  /**
+   * @description 查询用户菜单链接
+   */
+  queryUserMenuUrls(): string[] {
+    return <string[]>(
+      JSON.parse(sessionStorage.getItem(SessionKey.USER_MENU_URL))
+    );
   }
 }
