ba56d67b74745137b8da0baeae87190b4f393d09
zww1990 <zww1990@foxmail.com>
2018-11-02 20:47:51 +0800

    关闭自动打开浏览器。

--- scripts/start.js
+++ scripts/start.js
@@ -97,8 +97,8 @@ checkBrowsers(paths.appPath, isInteractive)
       if (isInteractive) {
         clearConsole();
       }
+      console.log(chalk.cyan('Starting the development server...\n'));
       // 关闭自动打开浏览器。
-      // console.log(chalk.cyan('Starting the development server...\n'));
       // openBrowser(urls.localUrlForBrowser);
     });
 
