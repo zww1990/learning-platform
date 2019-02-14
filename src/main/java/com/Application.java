8a422451eaf4f7b427169e69d128d4ca23f55f8f
zww1990 <zww1990@foxmail.com>
2019-02-14 15:27:29 +0800

    Java的统一查询。Querydsl紧凑，安全而且易于学习。

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,10 +1,13 @@
 package com;
 
+import javax.persistence.EntityManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @SpringBootApplication
 public class Application {
@@ -14,5 +17,10 @@ public class Application {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 		log.info("应用程序上下文Bean定义计数={}", context.getBeanDefinitionCount());
 //		java.util.Arrays.stream(context.getBeanDefinitionNames()).forEach(System.err::println);
+	}
+	
+	@Bean
+	public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+		return new JPAQueryFactory(entityManager);
 	}
 }
