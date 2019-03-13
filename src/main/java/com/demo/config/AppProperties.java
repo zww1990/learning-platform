6b531191a078f6db0bd54e13c27cb1d557429362
zww1990 <zww1990@foxmail.com>
2019-03-13 16:16:42 +0800

    删除dubbo

--- src/main/java/com/demo/config/AppProperties.java
+++ src/main/java/com/demo/config/AppProperties.java
@@ -7,41 +7,6 @@ import org.springframework.context.annotation.PropertySource;
 public class AppProperties {
 	@Configuration
 	@PropertySource("classpath:application.properties")
-	public static class DubboProperties {
-		@Value("${dubbo.annotation.package}")
-		private String annotationPackage;
-		@Value("${dubbo.app.name}")
-		private String appName;
-		@Value("${dubbo.registry.address}")
-		private String registryAddress;
-
-		public String getAnnotationPackage() {
-			return annotationPackage;
-		}
-
-		public void setAnnotationPackage(String annotationPackage) {
-			this.annotationPackage = annotationPackage;
-		}
-
-		public String getAppName() {
-			return appName;
-		}
-
-		public void setAppName(String appName) {
-			this.appName = appName;
-		}
-
-		public String getRegistryAddress() {
-			return registryAddress;
-		}
-
-		public void setRegistryAddress(String registryAddress) {
-			this.registryAddress = registryAddress;
-		}
-	}
-
-	@Configuration
-	@PropertySource("classpath:application.properties")
 	public static class JdbcProperties {
 		@Value("${jdbc.driver.class}")
 		private String driverClass;
--- src/main/java/com/demo/config/SpringConfig.java
+++ src/main/java/com/demo/config/SpringConfig.java
@@ -2,7 +2,7 @@ package com.demo.config;
 
 import java.util.Properties;
 import javax.sql.DataSource;
-import org.apache.commons.lang3.ArrayUtils;
+import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.type.JdbcType;
 import org.mybatis.spring.SqlSessionFactoryBean;
@@ -12,42 +12,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import com.alibaba.dubbo.config.ApplicationConfig;
-import com.alibaba.dubbo.config.RegistryConfig;
-import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
-import com.dangdang.ddframe.job.config.JobCoreConfiguration;
-import com.dangdang.ddframe.job.config.simple.SimpleJobConfiguration;
-import com.dangdang.ddframe.job.lite.config.LiteJobConfiguration;
-import com.dangdang.ddframe.job.lite.spring.api.SpringJobScheduler;
-import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
-import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
-import com.demo.config.AppProperties.DubboProperties;
 import com.demo.config.AppProperties.JdbcProperties;
-import com.demo.service.JobService;
 import com.github.pagehelper.PageInterceptor;
 
 @Configuration
 @ComponentScan("com.demo")
-@DubboComponentScan("com.demo.service.impl")
 @MapperScan({ "com.wiwj.bdm.mq.mapper", "com.wiwj.bdm.base.mapper" })
 public class SpringConfig {
-	@Bean
-	public ApplicationConfig applicationConfig(DubboProperties props) {
-		ApplicationConfig config = new ApplicationConfig();
-		config.setName(props.getAppName());
-		return config;
-	}
-
-	@Bean
-	public RegistryConfig registryConfig(DubboProperties props) {
-		RegistryConfig config = new RegistryConfig();
-		config.setProtocol("zookeeper");
-		config.setAddress(props.getRegistryAddress());
-		return config;
-	}
-
 	@Bean
 	public DataSource dataSource(JdbcProperties props) {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -69,9 +40,9 @@ public class SpringConfig {
 		bean.setDataSource(dataSource);
 		PageInterceptor page = new PageInterceptor();
 		Properties props = new Properties();
-		props.put("supportMethodsArguments", true);
+		props.put("supportMethodsArguments", "true");
 		page.setProperties(props);
-		bean.setPlugins(ArrayUtils.toArray(page));
+		bean.setPlugins(new Interceptor[] { page });
 		org.apache.ibatis.session.Configuration config = new org.apache.ibatis.session.Configuration();
 		// 全局地开启或关闭配置文件中的所有映射器已经配置的任何缓存。
 		config.setCacheEnabled(false);
@@ -86,32 +57,5 @@ public class SpringConfig {
 		config.setJdbcTypeForNull(JdbcType.NULL);
 		bean.setConfiguration(config);
 		return bean.getObject();
-	}
-
-	@Bean(initMethod = "init", destroyMethod = "close")
-	public ZookeeperRegistryCenter zookeeperRegistryCenter(DubboProperties props) {
-		return new ZookeeperRegistryCenter(new ZookeeperConfiguration(props.getRegistryAddress(), props.getAppName()));
-	}
-
-	@Bean(initMethod = "init")
-	public SpringJobScheduler springJobScheduler(JobService jobService, ZookeeperRegistryCenter regCenter) {
-		String jobName = jobService.getClass().getName();
-		return new SpringJobScheduler(jobService, regCenter,
-				LiteJobConfiguration
-						.newBuilder(new SimpleJobConfiguration(
-								JobCoreConfiguration.newBuilder(jobName, "0 0/1 * * * ?", 1).build(), jobName))
-						.overwrite(true).build());
-	}
-
-	@Bean
-	public JavaMailSender mailSender() {
-		JavaMailSenderImpl ms = new JavaMailSenderImpl();
-		ms.setDefaultEncoding("UTF-8");
-		ms.setProtocol("smtp");
-		ms.setHost("smtp.sohu.com");
-		ms.setPort(25);
-		ms.setUsername("nokia0561861@sohu.com");
-		ms.setPassword("zhangWW@1021");
-		return ms;
 	}
 }
