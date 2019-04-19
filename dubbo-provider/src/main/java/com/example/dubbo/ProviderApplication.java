6d25b594be8d3986e1eadff56384dccc7a521a7e
zww1990 <zww1990@foxmail.com>
2019-04-19 11:59:12 +0800

    升级依赖库版本

--- dubbo-provider/src/main/java/com/example/dubbo/ProviderApplication.java
+++ dubbo-provider/src/main/java/com/example/dubbo/ProviderApplication.java
@@ -1,24 +1,19 @@
 package com.example.dubbo;
 
-import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ProviderApplication {
 	private static final Logger log = LoggerFactory.getLogger(ProviderApplication.class);
 
-	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(ProviderApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		ConfigurableApplicationContext context = SpringApplication.run(ProviderApplication.class, args);
+		context.start();
 		log.info("应用程序上下文Bean定义计数={}", context.getBeanDefinitionCount());
-		RabbitTemplate template = context.getBean(RabbitTemplate.class);
-		String exchange = "ex.demo";
-		template.convertAndSend(exchange, "rk.demo1", Arrays.asList("这是来自rk.demo1的消息"));
-		template.convertAndSend(exchange, "rk.demo2", Arrays.asList("这是来自rk.demo2的消息"));
-		log.info("消息已发送");
+		System.in.read();
 	}
 }
