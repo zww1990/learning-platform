62f3f616816a9298002afe2f695160973a2c6197
zww1990 <zww1990@foxmail.com>
2019-04-19 12:08:49 +0800

    升级依赖库版本

--- dubbo-consumer/src/main/java/com/example/dubbo/ConsumerApplication.java
+++ dubbo-consumer/src/main/java/com/example/dubbo/ConsumerApplication.java
@@ -2,14 +2,12 @@ package com.example.dubbo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import com.example.dubbo.action.BarAction;
 
 @SpringBootApplication
-@EnableRabbit
 public class ConsumerApplication {
 	private static final Logger log = LoggerFactory.getLogger(ConsumerApplication.class);
 
