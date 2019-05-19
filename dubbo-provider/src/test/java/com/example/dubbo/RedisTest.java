7fccbcdd3c9530c4dcfea818ddcf80ed51eae074
home <home@DESKTOP-KIK121B>
2019-05-19 20:16:09 +0800

    测试数据

--- dubbo-provider/src/test/java/com/example/dubbo/RedisTest.java
+++ dubbo-provider/src/test/java/com/example/dubbo/RedisTest.java
@@ -1,13 +1,13 @@
 package com.example.dubbo;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.redis.core.BoundValueOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -21,15 +21,29 @@ import com.example.dubbo.model.User;
 public class RedisTest {
 	@Resource
 	private ApplicationContext context;
+	@Resource
+	private RedisTemplate<String, Object> redisTemplate;
 
 	@Test
-	public void testRedis() {
+	public void initData() {
 		try {
-			RedisTemplate<String, Object> temp = this.context.getBean(RedisTemplate.class);
-			User a1 = new User(1001, "张三", 11, "男");
-			BoundValueOperations<String, Object> ops = temp.boundValueOps("users");
-			System.err.println(ops.get());
-			ops.set(Arrays.asList(a1));
+			List<User> users = new ArrayList<>();
+			for (int i = 1; i <= 6000; i++) {
+				users.add(new User(1000 + i, "张三" + i, 11, "男"));
+			}
+			this.redisTemplate.boundValueOps("users").set(users);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testSelect() {
+		try {
+			List<User> users = (List<User>) this.redisTemplate.boundValueOps("users").get();
+			for (User user : users) {
+				System.err.println(user.getName());
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
