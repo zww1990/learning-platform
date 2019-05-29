ef35a88a41e4d22546ac4dd143898e70c6c2ffb3
zww1990 <zww1990@foxmail.com>
2019-05-29 15:28:46 +0800

    重构

---
+++ dubbo-consumer/src/main/java/com/example/dubbo/config/RabbitmqConfiguration.java
@@ -0,0 +1,33 @@
+package com.example.dubbo.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.example.dubbo.consumer.Demo1ConsumerListener;
+import com.example.dubbo.consumer.Demo2ConsumerListener;
+import com.example.dubbo.consumer.Demo3ConsumerListener;
+import com.example.dubbo.consumer.Demo4ConsumerListener;
+
+@Configuration
+@ConditionalOnProperty(name = "rabbitmq.enable", matchIfMissing = true)
+public class RabbitmqConfiguration {
+	@Bean
+	public Demo1ConsumerListener demo1ConsumerListener() {
+		return new Demo1ConsumerListener();
+	}
+
+	@Bean
+	public Demo2ConsumerListener demo2ConsumerListener() {
+		return new Demo2ConsumerListener();
+	}
+
+	@Bean
+	public Demo3ConsumerListener demo3ConsumerListener() {
+		return new Demo3ConsumerListener();
+	}
+
+	@Bean
+	public Demo4ConsumerListener demo4ConsumerListener() {
+		return new Demo4ConsumerListener();
+	}
+}
