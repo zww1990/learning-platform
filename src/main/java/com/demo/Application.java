b667971534949c4dbac855ecc792eb0a70e855b6
zhangweiwei01 <zhangweiwei01@5i5j.com>
2019-06-12 14:58:17 +0800

    nothing

--- src/main/java/com/demo/Application.java
+++ src/main/java/com/demo/Application.java
@@ -5,20 +5,12 @@ import com.demo.config.SpringConfig;
 
 public class Application {
 	@SuppressWarnings("resource")
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 		context.registerShutdownHook();
 		context.start();
 		System.err.println(context.getBeanDefinitionCount());
 //		java.util.Arrays.stream(context.getBeanDefinitionNames()).forEach(System.err::println);
-		synchronized (Application.class) {
-			while (true) {
-				try {
-					Application.class.wait();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
+		Thread.currentThread().join();
 	}
 }
