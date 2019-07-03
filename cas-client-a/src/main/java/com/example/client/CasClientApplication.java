d958de5b649b15ca18d7f76891823594d1a08fec
zhangweiwei01 <zhangweiwei01@5i5j.com>
2019-07-03 16:03:11 +0800

    use spring boot

--- cas-client-a/src/main/java/com/example/client/CasClientApplication.java
+++ cas-client-a/src/main/java/com/example/client/CasClientApplication.java
@@ -4,21 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class CasClientApplication extends SpringBootServletInitializer {
+public class CasClientApplication {
 	private static final Logger log = LoggerFactory.getLogger(CasClientApplication.class);
 
 	public static void main(String[] args) throws Exception {
 		ApplicationContext context = SpringApplication.run(CasClientApplication.class, args);
 		log.info("应用程序上下文Bean定义计数={}", context.getBeanDefinitionCount());
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(CasClientApplication.class);
 	}
 }
