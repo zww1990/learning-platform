e4b48d60de4260fce1ce90a0de2e5221eed34e13
zww1990 <zww1990@foxmail.com>
2019-08-16 19:32:02 +0800

    修改文件

--- proxy.conf.js
+++ proxy.conf.js
@@ -18,9 +18,6 @@ if (Object.keys(proxy).length > 0) {
   console.log();
 }
 
-const notifier = require('node-notifier');
-notifier.notify({ title: '欢迎来到大前端', message: '大家一起来学习Angular哟！😄😄', icon: './src/assets/icons/icon-512x512.png' });
-
 /**
  * @author zww
  * @description API代理配置
