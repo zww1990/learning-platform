5813b19a5ad18bce254af56e4e16e0cbe1586e71
zww1990 <zww1990@foxmail.com>
2019-08-19 21:55:16 +0800

    添加测试代码。

---
+++ src/test/java/com/wiwj/test/ExcelTest.java
@@ -0,0 +1,40 @@
+package com.wiwj.test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class ExcelTest {
+
+	public static void main(String[] args) {
+		File file = new File("E:\\TDDOWNLOAD\\20190815北京门店电话更新.xlsx");
+		try (Workbook workbook = WorkbookFactory.create(file)) {
+			Sheet sheet = workbook.getSheetAt(0);
+			int lastRowNum = sheet.getLastRowNum();
+			for (int i = 1; i <= lastRowNum; i++) {
+				Row row = sheet.getRow(i);
+				short lastCellNum = row.getLastCellNum();
+				List<String> values = new ArrayList<String>();
+				for (int j = 0; j < lastCellNum; j++) {
+					Cell cell = row.getCell(j);
+					cell.setCellType(CellType.STRING);
+					values.add(cell.getStringCellValue());
+				}
+				System.err.println(String.format(
+						"UPDATE bdm_shops t SET t.service_phone = '%s', t.update_time = systimestamp WHERE t.shop_name = '%s';",
+						values.get(6), values.get(1)));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
