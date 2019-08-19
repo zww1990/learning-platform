c7c7caab6f14cad0672d97187d83cf4df3301907
zhangweiwei01 <zhangweiwei01@5i5j.com>
2019-08-19 22:07:32 +0800

    添加测试代码。

--- src/test/java/com/wiwj/test/ExcelTest.java
+++ src/test/java/com/wiwj/test/ExcelTest.java
@@ -3,7 +3,6 @@ package com.wiwj.test;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
@@ -12,16 +11,15 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class ExcelTest {
-
 	public static void main(String[] args) {
-		File file = new File("E:\\TDDOWNLOAD\\20190815北京门店电话更新.xlsx");
+		File file = new File("D:\\20190815北京门店电话更新.xlsx");
 		try (Workbook workbook = WorkbookFactory.create(file)) {
 			Sheet sheet = workbook.getSheetAt(0);
 			int lastRowNum = sheet.getLastRowNum();
 			for (int i = 1; i <= lastRowNum; i++) {
 				Row row = sheet.getRow(i);
 				short lastCellNum = row.getLastCellNum();
-				List<String> values = new ArrayList<String>();
+				List<String> values = new ArrayList<>();
 				for (int j = 0; j < lastCellNum; j++) {
 					Cell cell = row.getCell(j);
 					cell.setCellType(CellType.STRING);
@@ -34,7 +32,5 @@ public class ExcelTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
-
 }
