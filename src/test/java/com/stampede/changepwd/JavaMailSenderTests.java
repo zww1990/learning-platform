e7affe5999263766c98c6435e358a9a8ef0c805b
zhangweiwei01 <zhangweiwei01@5i5j.com>
2020-02-14 14:05:52 +0800

    采用加密传输方式进行邮件发送。

--- src/test/java/com/stampede/changepwd/JavaMailSenderTests.java
+++ src/test/java/com/stampede/changepwd/JavaMailSenderTests.java
@@ -2,6 +2,7 @@ package com.stampede.changepwd;
 
 import javax.annotation.Resource;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,15 +11,17 @@ import org.springframework.mail.javamail.JavaMailSender;
 public class JavaMailSenderTests {
 	@Resource
 	private JavaMailSender javaMailSender;
+	@Resource
+	private MailProperties mailProperties;
 
 	@Test
 	public void testSendMail() {
 		try {
 			SimpleMailMessage message = new SimpleMailMessage();
-			message.setFrom("siqianwen@5i5j.com");
+			message.setFrom(this.mailProperties.getUsername());
 			message.setTo("zhangweiwei01@5i5j.com");
-			message.setSubject("主题：简单邮件");
-			message.setText("测试邮件内容");
+			message.setSubject("主题：简单邮件（采用加密传输方式）");
+			message.setText("测试邮件内容（采用加密传输方式）");
 			this.javaMailSender.send(message);
 		} catch (Exception e) {
 			e.printStackTrace();
