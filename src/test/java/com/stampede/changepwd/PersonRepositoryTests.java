9b9d75f5e4ae0f277594e6796ab5ed992cb1c365
zhangweiwei01 <zhangweiwei01@5i5j.com>
2020-02-15 18:18:16 +0800

    java unit test case

--- src/test/java/com/stampede/changepwd/PersonRepositoryTests.java
+++ src/test/java/com/stampede/changepwd/PersonRepositoryTests.java
@@ -1,9 +1,14 @@
 package com.stampede.changepwd;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Base64;
+import java.util.Optional;
 import javax.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.ldap.query.LdapQueryBuilder;
+import com.stampede.changepwd.domain.Person;
 import com.stampede.changepwd.repository.PersonRepository;
 
 @SpringBootTest
@@ -12,7 +17,7 @@ public class PersonRepositoryTests {
 	private PersonRepository personRepository;
 
 	@Test
-	public void test() {
+	public void testFindOne() {
 		try {
 			this.personRepository.findOne(LdapQueryBuilder.query().where("uid").is("zhangweiwei1")).ifPresent(c -> {
 				System.err.println(c.getCname());
@@ -27,6 +32,43 @@ public class PersonRepositoryTests {
 			});
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testSave() {
+		try {
+			Optional<Person> optional = this.personRepository
+					.findOne(LdapQueryBuilder.query().where("uid").is("zhangweiwei1"));
+			if (optional.isPresent()) {
+				Person person = optional.get();
+				person.setUserPassword(md5Password("1q2w3e4r"));
+				this.personRepository.save(person);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testMD5() {
+		try {
+			System.err.println(md5Password("1q2w3e4r"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String md5Password(String text) {
+		try {
+			String algorithm = "MD5";
+			MessageDigest instance = MessageDigest.getInstance(algorithm);
+			byte[] digest = instance.digest(text.getBytes(StandardCharsets.UTF_8));
+			byte[] encode = Base64.getEncoder().encode(digest);
+			String password = String.format("{%s}%s", algorithm, new String(encode, StandardCharsets.UTF_8));
+			return password;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
