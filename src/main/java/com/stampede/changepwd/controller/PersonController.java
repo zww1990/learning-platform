72ebea0ba49274c518c73fd7b1fc819e269469c2
zhangweiwei01 <zhangweiwei01@5i5j.com>
2020-02-15 22:00:14 +0800

    创建index.html文件。

--- src/main/java/com/stampede/changepwd/controller/PersonController.java
+++ src/main/java/com/stampede/changepwd/controller/PersonController.java
@@ -1,7 +1,13 @@
 package com.stampede.changepwd.controller;
 
+import javax.annotation.Resource;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.stampede.changepwd.service.PersonService;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
+	@Resource
+	private PersonService personService;
 }
