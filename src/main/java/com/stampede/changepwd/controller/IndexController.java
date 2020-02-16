0046a5100869048da849b7bd2726163c4eb1311e
zhangweiwei01 <zhangweiwei01@5i5j.com>
2020-02-16 17:50:40 +0800

    设置随机背景图片。

--- src/main/java/com/stampede/changepwd/controller/IndexController.java
+++ src/main/java/com/stampede/changepwd/controller/IndexController.java
@@ -1,13 +1,16 @@
 package com.stampede.changepwd.controller;
 
+import java.util.Random;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
+	private static final String[] images = { "clouds", "sky", "space", "stars" };
+
 	@GetMapping
 	public ModelAndView index() {
-		return new ModelAndView("index");
+		return new ModelAndView("index").addObject("image", images[new Random().nextInt(images.length)]);
 	}
 }
