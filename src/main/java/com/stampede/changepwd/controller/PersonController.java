ab730f09de2d4377149c77ebdfa5a10aabfc4697
zhangweiwei01 <zhangweiwei01@5i5j.com>
2020-02-16 23:09:54 +0800

    完善首页功能。

--- src/main/java/com/stampede/changepwd/controller/PersonController.java
+++ src/main/java/com/stampede/changepwd/controller/PersonController.java
@@ -1,13 +1,26 @@
 package com.stampede.changepwd.controller;
 
 import javax.annotation.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import com.stampede.changepwd.domain.PersonParam;
 import com.stampede.changepwd.service.PersonService;
 
-@RestController
+@Controller
 @RequestMapping("/person")
 public class PersonController {
 	@Resource
 	private PersonService personService;
+
+	@PostMapping("/update")
+	public ModelAndView update(@ModelAttribute PersonParam param) {
+		System.err.println(param.getUsername());
+		System.err.println(param.getPassword());
+		System.err.println(param.getNewpassword());
+		System.err.println(param.getRepassword());
+		return new ModelAndView("person/success");
+	}
 }
