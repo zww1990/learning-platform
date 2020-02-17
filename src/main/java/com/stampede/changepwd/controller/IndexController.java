922c7a5e254b02f036dc5a9b0f94d125ede1a00d
zhangweiwei01 <zhangweiwei01@5i5j.com>
2020-02-17 13:48:27 +0800

    MVC控制器类包

--- src/main/java/com/stampede/changepwd/controller/IndexController.java
+++ src/main/java/com/stampede/changepwd/controller/IndexController.java
@@ -1,16 +1,24 @@
 package com.stampede.changepwd.controller;
 
-import java.util.Random;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import com.stampede.changepwd.constant.Constants;
 
+/**
+ * @author ZhangWeiWei
+ * @date 2020年2月17日,下午1:27:50
+ * @description 首页控制器
+ */
 @Controller
 public class IndexController {
-	private static final String[] images = { "clouds", "sky", "space", "stars" };
-
+	/**
+	 * @author ZhangWeiWei
+	 * @date 2020年2月17日,下午1:28:03
+	 * @return 首页
+	 */
 	@GetMapping
 	public ModelAndView index() {
-		return new ModelAndView("index").addObject("image", images[new Random().nextInt(images.length)]);
+		return new ModelAndView("index").addObject("image", Constants.randomImage());
 	}
 }
--- src/main/java/com/stampede/changepwd/controller/PersonController.java
+++ src/main/java/com/stampede/changepwd/controller/PersonController.java
@@ -2,25 +2,61 @@ package com.stampede.changepwd.controller;
 
 import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import com.stampede.changepwd.constant.Constants;
 import com.stampede.changepwd.domain.PersonParam;
 import com.stampede.changepwd.service.PersonService;
 
+/**
+ * @author ZhangWeiWei
+ * @date 2020年2月17日,下午1:33:42
+ * @description 人员控制器
+ */
 @Controller
 @RequestMapping("/person")
 public class PersonController {
 	@Resource
 	private PersonService personService;
 
+	/**
+	 * @author ZhangWeiWei
+	 * @date 2020年2月17日,下午1:34:42
+	 * @param param 用户密码参数类
+	 * @return 更新人员密码
+	 */
 	@PostMapping("/update")
 	public ModelAndView update(@ModelAttribute PersonParam param) {
+		ModelAndView mav = new ModelAndView("index").addObject("image", Constants.randomImage());
+		if (!StringUtils.hasText(param.getUsername())) {
+			return mav.addObject("message", "请输入用户名！");
+		}
+		if (!StringUtils.hasText(param.getPassword())) {
+			return mav.addObject("message", "请输入旧密码！");
+		}
+		if (!StringUtils.hasText(param.getNewpassword())) {
+			return mav.addObject("message", "请输入新密码！");
+		}
+		if (!StringUtils.hasText(param.getRepassword())) {
+			return mav.addObject("message", "请输入确认密码！");
+		}
+		if (param.getNewpassword().length()<6) {
+			return mav.addObject("message", "您的新密码太短！");
+		}
+		if (param.getNewpassword().length()>14) {
+			return mav.addObject("message", "您的新密码太长！");
+		}
+		if (!this.personService.checkUserPassword(param)) {
+			return mav.addObject("message", "您输入的用户名或旧密码不正确！");
+		}
 		System.err.println(param.getUsername());
 		System.err.println(param.getPassword());
 		System.err.println(param.getNewpassword());
 		System.err.println(param.getRepassword());
-		return new ModelAndView("person/success");
+		mav.setViewName("person/success");
+		return mav;
 	}
 }
