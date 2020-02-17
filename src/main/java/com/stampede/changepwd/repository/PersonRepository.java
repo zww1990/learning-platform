c305f6917accc7b4808260c9318d0087e85e2b3f
zhangweiwei01 <zhangweiwei01@5i5j.com>
2020-02-17 13:48:54 +0800

    数据访问操作类包

--- src/main/java/com/stampede/changepwd/repository/PersonRepository.java
+++ src/main/java/com/stampede/changepwd/repository/PersonRepository.java
@@ -3,5 +3,10 @@ package com.stampede.changepwd.repository;
 import org.springframework.data.ldap.repository.LdapRepository;
 import com.stampede.changepwd.domain.Person;
 
+/**
+ * @author ZhangWeiWei
+ * @date 2020年2月17日,下午1:38:11
+ * @description 人员数据访问操作类
+ */
 public interface PersonRepository extends LdapRepository<Person> {
 }
--- src/main/java/com/stampede/changepwd/repository/package-info.java
+++ src/main/java/com/stampede/changepwd/repository/package-info.java
@@ -1,1 +1,4 @@
+/**
+ * 数据访问操作类包
+ */
 package com.stampede.changepwd.repository;
