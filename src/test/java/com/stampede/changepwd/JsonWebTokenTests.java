e82ec9f8e8f67305a18ff7761c249e506c9326d2
zhangweiwei01 <zhangweiwei01@5i5j.com>
2020-02-18 18:02:09 +0800

    json web token

--- src/test/java/com/stampede/changepwd/JsonWebTokenTests.java
+++ src/test/java/com/stampede/changepwd/JsonWebTokenTests.java
@@ -1,19 +1,13 @@
 package com.stampede.changepwd;
 
-import java.util.Date;
-import java.util.UUID;
 import org.junit.jupiter.api.Test;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.stampede.changepwd.util.LdapPasswordUtils;
 
 public class JsonWebTokenTests {
 	@Test
 	public void testEncode() {
 		try {
-			System.out.println(Jwts.builder().setId(UUID.randomUUID().toString()).setSubject("zww")// 用户名
-					.setIssuedAt(new Date())// 签发时间
-					.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 5))// 过期时间：5分钟
-					.signWith(SignatureAlgorithm.HS256, "hello").compact());
+			System.err.println(LdapPasswordUtils.jwtEncode("zhangweiwei1", 1000 * 60 * 5));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -22,8 +16,8 @@ public class JsonWebTokenTests {
 	@Test
 	public void testDecode() {
 		try {
-			String string = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4NWM3NDliMC0wYzA1LTQwMjktYTgwNy0wOWNkMjBkOGNmNmIiLCJzdWIiOiJ6d3ciLCJpYXQiOjE1ODIwMTgyOTUsImV4cCI6MTU4MjAxODMyNX0.JteLPk4f9BjTVilRPdANbFbxSrNixWrvrEqm0TO0Kb0";
-			System.out.println(Jwts.parser().setSigningKey("hello").parseClaimsJws(string).getBody());
+			String string = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjZjViZDIyNi1lNmE4LTQ1MzAtYWMxYi1lM2I5M2NlNzA4MTUiLCJzdWIiOiJ6aGFuZ3dlaXdlaTEiLCJpYXQiOjE1ODIwMjAwNzYsImV4cCI6MTU4MjAyMDM3Nn0.pZlBvsLqO3-w1dxv_bTlOXE1t0lexC0e5m0AtV_JB1A";
+			System.err.println(LdapPasswordUtils.jwtDecode(string));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
