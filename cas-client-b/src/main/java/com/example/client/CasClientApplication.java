955f5e55b592f1df92ddf349d65b2ecf62763fc8
zhangweiwei01 <zhangweiwei01@5i5j.com>
2020-04-12 20:21:53 +0800

    升级依赖库

--- cas-client-b/src/main/java/com/example/client/CasClientApplication.java
+++ cas-client-b/src/main/java/com/example/client/CasClientApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.dao.PersistenceExceptionTranslatio
 import org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
 import org.springframework.boot.autoconfigure.info.ProjectInfoAutoConfiguration;
-import org.springframework.boot.autoconfigure.reactor.core.ReactorCoreAutoConfiguration;
 import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
 import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
@@ -17,7 +16,7 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(exclude = { ProjectInfoAutoConfiguration.class, ValidationAutoConfiguration.class,
 		PersistenceExceptionTranslationAutoConfiguration.class, RedisReactiveAutoConfiguration.class,
-		ReactorCoreAutoConfiguration.class, MultipartAutoConfiguration.class, SpringDataWebAutoConfiguration.class,
+		MultipartAutoConfiguration.class, SpringDataWebAutoConfiguration.class,
 		TaskExecutionAutoConfiguration.class, TaskSchedulingAutoConfiguration.class })
 public class CasClientApplication {
 	private static final Logger log = LoggerFactory.getLogger(CasClientApplication.class);
