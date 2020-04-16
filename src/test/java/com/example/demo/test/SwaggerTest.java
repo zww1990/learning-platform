cca7bca1a97912d9156f4bc9542d192a3c7d99d5
zhangweiwei01 <zhangweiwei01@5i5j.com>
2020-04-16 21:41:41 +0800

    update

--- src/test/java/com/example/demo/test/SwaggerTest.java
+++ src/test/java/com/example/demo/test/SwaggerTest.java
@@ -1,71 +1,71 @@
-package com.example.demo.test;
-
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.nio.file.Paths;
-import org.junit.Test;
-import io.github.swagger2markup.Swagger2MarkupConfig;
-import io.github.swagger2markup.Swagger2MarkupConverter;
-import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
-import io.github.swagger2markup.markup.builder.MarkupLanguage;
-
-public class SwaggerTest {
-	private static final String API_DOCS = "http://localhost:8080/spring-swagger/v2/api-docs";
-
-	@Test
-	public void generateAsciiDocs() throws Exception {
-		// 输出Ascii格式
-		Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder().withMarkupLanguage(MarkupLanguage.ASCIIDOC)
-				.build();
-		Swagger2MarkupConverter.from(new URL(API_DOCS)).withConfig(config).build()
-				.toFolder(Paths.get("src/docs/asciidoc/generated"));
-	}
-
-	@Test
-	public void generateMarkdownDocs() throws Exception {
-		// 输出Markdown格式
-		Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder().withMarkupLanguage(MarkupLanguage.MARKDOWN)
-				.build();
-		Swagger2MarkupConverter.from(new URL(API_DOCS)).withConfig(config).build()
-				.toFolder(Paths.get("src/docs/markdown/generated"));
-	}
-
-	@Test
-	public void generateConfluenceDocs() throws Exception {
-		// 输出Confluence使用的格式
-		Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
-				.withMarkupLanguage(MarkupLanguage.CONFLUENCE_MARKUP).build();
-		Swagger2MarkupConverter.from(new URL(API_DOCS)).withConfig(config).build()
-				.toFolder(Paths.get("src/docs/confluence/generated"));
-	}
-
-	@Test
-	public void generateAsciiDocsToFile() throws Exception {
-		// 输出Ascii到单文件
-		Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder().withMarkupLanguage(MarkupLanguage.ASCIIDOC)
-				.build();
-		Swagger2MarkupConverter.from(new URL(API_DOCS)).withConfig(config).build()
-				.toFile(Paths.get("src/docs/asciidoc/generated/all"));
-	}
-
-	@Test
-	public void generateMarkdownDocsToFile() throws Exception {
-		// 输出Markdown到单文件
-		Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder().withMarkupLanguage(MarkupLanguage.MARKDOWN)
-				.build();
-		Swagger2MarkupConverter.from(new URL(API_DOCS)).withConfig(config).build()
-				.toFile(Paths.get("src/docs/markdown/generated/all"));
-	}
-
-	@Test
-	public void test() {
-		try {
-			System.err.println(Charset.defaultCharset().name());
-			System.err.println(Charset.defaultCharset().toString());
-			System.err.println(Charset.defaultCharset().displayName());
-			System.err.println(Charset.defaultCharset().aliases());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-}
+//package com.example.demo.test;
+//
+//import java.net.URL;
+//import java.nio.charset.Charset;
+//import java.nio.file.Paths;
+//import org.junit.Test;
+//import io.github.swagger2markup.Swagger2MarkupConfig;
+//import io.github.swagger2markup.Swagger2MarkupConverter;
+//import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
+//import io.github.swagger2markup.markup.builder.MarkupLanguage;
+//
+//public class SwaggerTest {
+//	private static final String API_DOCS = "http://localhost:8080/spring-swagger/v2/api-docs";
+//
+//	@Test
+//	public void generateAsciiDocs() throws Exception {
+//		// 输出Ascii格式
+//		Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder().withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+//				.build();
+//		Swagger2MarkupConverter.from(new URL(API_DOCS)).withConfig(config).build()
+//				.toFolder(Paths.get("src/docs/asciidoc/generated"));
+//	}
+//
+//	@Test
+//	public void generateMarkdownDocs() throws Exception {
+//		// 输出Markdown格式
+//		Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder().withMarkupLanguage(MarkupLanguage.MARKDOWN)
+//				.build();
+//		Swagger2MarkupConverter.from(new URL(API_DOCS)).withConfig(config).build()
+//				.toFolder(Paths.get("src/docs/markdown/generated"));
+//	}
+//
+//	@Test
+//	public void generateConfluenceDocs() throws Exception {
+//		// 输出Confluence使用的格式
+//		Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+//				.withMarkupLanguage(MarkupLanguage.CONFLUENCE_MARKUP).build();
+//		Swagger2MarkupConverter.from(new URL(API_DOCS)).withConfig(config).build()
+//				.toFolder(Paths.get("src/docs/confluence/generated"));
+//	}
+//
+//	@Test
+//	public void generateAsciiDocsToFile() throws Exception {
+//		// 输出Ascii到单文件
+//		Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder().withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+//				.build();
+//		Swagger2MarkupConverter.from(new URL(API_DOCS)).withConfig(config).build()
+//				.toFile(Paths.get("src/docs/asciidoc/generated/all"));
+//	}
+//
+//	@Test
+//	public void generateMarkdownDocsToFile() throws Exception {
+//		// 输出Markdown到单文件
+//		Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder().withMarkupLanguage(MarkupLanguage.MARKDOWN)
+//				.build();
+//		Swagger2MarkupConverter.from(new URL(API_DOCS)).withConfig(config).build()
+//				.toFile(Paths.get("src/docs/markdown/generated/all"));
+//	}
+//
+//	@Test
+//	public void test() {
+//		try {
+//			System.err.println(Charset.defaultCharset().name());
+//			System.err.println(Charset.defaultCharset().toString());
+//			System.err.println(Charset.defaultCharset().displayName());
+//			System.err.println(Charset.defaultCharset().aliases());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//}
