f54898288123540941b6c059f49b0e3c4dd5a531
zhangweiwei01 <zhangweiwei01@5i5j.com>
2020-04-20 17:40:37 +0800

    use junit 5 to 4

--- src/test/java/com/example/demo/test/AppTest.java
+++ src/test/java/com/example/demo/test/AppTest.java
@@ -1,17 +1,14 @@
 package com.example.demo.test;
 
 import javax.annotation.Resource;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.example.demo.SpringSwaggerApplication;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = { SpringSwaggerApplication.class })
+@SpringBootTest
 public class AppTest {
 	@Resource
 	private ApplicationContext context;
