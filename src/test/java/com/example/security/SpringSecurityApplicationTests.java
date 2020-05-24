8ef45ed04d77c5bd0b6f5806716c0649aaa2b15a
zww1990 <zww1990@foxmail.com>
2020-05-24 18:51:55 +0800

    SpringSecurityApplicationTests.java

--- src/test/java/com/example/security/SpringSecurityApplicationTests.java
+++ src/test/java/com/example/security/SpringSecurityApplicationTests.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -28,35 +30,11 @@ public class SpringSecurityApplicationTests {
 	}
 
 	public static void main(String[] args) {
-		StringBuilder sb = new StringBuilder();
-
-		sb.append("<!DOCTYPE html>\n"
-				+ "<html lang=\"en\">\n"
-				+ "  <head>\n"
-				+ "    <meta charset=\"utf-8\">\n"
-				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\n"
-				+ "    <meta name=\"description\" content=\"\">\n"
-				+ "    <meta name=\"author\" content=\"\">\n"
-				+ "    <title>Please sign in</title>\n"
-				+ "    <link href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M\" crossorigin=\"anonymous\">\n"
-				+ "    <link href=\"https://getbootstrap.com/docs/4.0/examples/signin/signin.css\" rel=\"stylesheet\" crossorigin=\"anonymous\"/>\n"
-				+ "  </head>\n"
-				+ "  <body>\n"
-				+ "     <div class=\"container\">\n");
-		sb.append("      <form class=\"form-signin\" method=\"post\" action=\"\">\n"
-				+ "        <h2 class=\"form-signin-heading\">Please sign in</h2>\n"
-				+ "        <p>\n"
-				+ "          <label for=\"username\" class=\"sr-only\">Username</label>\n"
-				+ "          <input type=\"text\" id=\"username\" name=\"username\" class=\"form-control\" placeholder=\"Username\" required autofocus>\n"
-				+ "        </p>\n"
-				+ "        <p>\n"
-				+ "          <label for=\"password\" class=\"sr-only\">Password</label>\n"
-				+ "          <input type=\"password\" id=\"password\" name=\"password\" class=\"form-control\" placeholder=\"Password\" required>\n"
-				+ "        </p>\n"
-				+ "        <button class=\"btn btn-lg btn-primary btn-block\" type=\"submit\">Sign in</button>\n"
-				+ "      </form>\n");
-		sb.append("</div>\n");
-		sb.append("</body></html>");
-		System.err.println("<div class=\"alert alert-danger\" role=\"alert\"></div>");
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.err.println(encoder.encode("admin"));
+		System.err.println(encoder.encode("guest1"));
+		System.err.println(encoder.encode("guest2"));
+		System.err.println(encoder.encode("guest3"));
+		System.err.println(encoder.encode("guest4"));
 	}
 }
