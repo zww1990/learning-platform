1db69a1f5b1910af0b8f206aaa5aa71ea80f6add
zww1990 <zww1990@foxmail.com>
2020-05-24 16:47:07 +0800

    IndexController.java

--- src/main/java/com/example/security/controller/IndexController.java
+++ src/main/java/com/example/security/controller/IndexController.java
@@ -1,12 +1,22 @@
 package com.example.security.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+	private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+
 	@GetMapping("/")
-	public Object index() {
+	public String index(Authentication authentication) {
+		log.info("getCredentials={}", authentication.getCredentials());
+		log.info("getDetails={}", authentication.getDetails());
+		log.info("getPrincipal={}", authentication.getPrincipal());
+		log.info("getAuthorities={}", authentication.getAuthorities());
+		log.info("getName={}", authentication.getName());
 		return "index";
 	}
 
