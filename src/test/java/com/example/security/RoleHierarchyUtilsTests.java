7759b226f0705aaf7a6243434bb3578b4c6a8042
zww1990 <zww1990@foxmail.com>
2020-05-26 22:36:45 +0800

    RoleHierarchyUtilsTests.java

---
+++ src/test/java/com/example/security/RoleHierarchyUtilsTests.java
@@ -0,0 +1,22 @@
+package com.example.security;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyUtils;
+
+public class RoleHierarchyUtilsTests {
+	@Test
+	public void testRoleHierarchyFromMap() {
+		try {
+			Map<String, List<String>> map = new HashMap<>();
+			map.put("ROLE_ADMIN", Arrays.asList("ROLE_GUEST"));
+			System.err.println(RoleHierarchyUtils.roleHierarchyFromMap(map));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
