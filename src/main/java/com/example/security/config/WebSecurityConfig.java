49dd2357e7c10c2d89b54a2a69792c74816d00eb
zww1990 <zww1990@foxmail.com>
2020-05-28 23:34:53 +0800

    WebSecurityConfig.java

--- src/main/java/com/example/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/security/config/WebSecurityConfig.java
@@ -6,22 +6,20 @@ import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
-import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyUtils;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import com.example.security.support.PlaintextPasswordEncoder;
+import com.example.security.service.UserService;
 
 /**
  * Spring Security配置类
@@ -31,28 +29,13 @@ import com.example.security.support.PlaintextPasswordEncoder;
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Resource
-	private DataSource dataSource;
+	private UserService userService;
 
-	/**
-	 * 加载用户特定数据的核心接口。
-	 */
 	@Override
-	@Bean
-	protected UserDetailsService userDetailsService() {
-		// 将内存认证添加到AuthenticationManagerBuilder中，并返回一个InMemoryUserDetailsManagerConfigurer来允许自定义内存认证。
-//		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-		// Jdbc用户管理服务，基于与其父类JdbcDaoImpl相同的表结构。
-		JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
-		// 允许将用户添加到正在创建的UserDetailsManager。 可以多次调用此方法来添加多个用户。
-		if (!manager.userExists("admin")) {
-			manager.createUser(User.withUsername("admin").password("admin")
-					.passwordEncoder(this.passwordEncoder()::encode).roles("ADMIN").build());
-		}
-		if (!manager.userExists("guest")) {
-			manager.createUser(User.withUsername("guest").password("guest")
-					.passwordEncoder(this.passwordEncoder()::encode).roles("GUEST").build());
-		}
-		return manager;
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(this.userService)// 根据传入的自定义UserDetailsService添加身份验证。
+													// 然后，它返回DaoAuthenticationConfigurer以允许自定义身份验证。
+		;
 	}
 
 	@Override
@@ -89,7 +72,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new PlaintextPasswordEncoder();
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 
 	/**
