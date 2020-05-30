e34b8a40f8674ada1d6c967cee77bd7e397e3777
zww1990 <zww1990@foxmail.com>
2020-05-30 19:00:40 +0800

    UserRepositoryTests.java

--- src/test/java/com/example/security/UserRepositoryTests.java
+++ src/test/java/com/example/security/UserRepositoryTests.java
@@ -25,6 +25,14 @@ public class UserRepositoryTests {
 	@Test
 	public void testSave() {
 		try {
+			Authority a1 = new Authority();
+			a1.setAuthName("超级管理员");
+			a1.setAuthority("ROLE_ADMIN");
+
+			Authority a2 = new Authority();
+			a2.setAuthName("访客");
+			a2.setAuthority("ROLE_GUEST");
+
 			User u1 = new User();
 			u1.setAccountNonExpired(true);
 			u1.setAccountNonLocked(true);
@@ -32,9 +40,6 @@ public class UserRepositoryTests {
 			u1.setEnabled(true);
 			u1.setPassword(this.encoder.encode("admin"));
 			u1.setUsername("admin");
-			Authority a1 = new Authority();
-			a1.setAuthName("超级管理员");
-			a1.setAuthority("ROLE_ADMIN");
 			u1.setAuthorities(Arrays.asList(a1));
 			this.userdao.save(u1);
 
@@ -45,11 +50,9 @@ public class UserRepositoryTests {
 			u2.setEnabled(true);
 			u2.setPassword(this.encoder.encode("guest"));
 			u2.setUsername("guest");
-			Authority a2 = new Authority();
-			a2.setAuthName("访客");
-			a2.setAuthority("ROLE_GUEST");
 			u2.setAuthorities(Arrays.asList(a2));
 			this.userdao.save(u2);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
