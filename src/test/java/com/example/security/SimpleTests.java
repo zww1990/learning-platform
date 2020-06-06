61ee538953dff535ddd5a22aded7e0aa5c9a918c
zww1990 <zww1990@foxmail.com>
2020-06-06 18:28:48 +0800

    SimpleTests.java

--- src/test/java/com/example/security/SimpleTests.java
+++ src/test/java/com/example/security/SimpleTests.java
@@ -1,5 +1,8 @@
 package com.example.security;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+
 import org.junit.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,5 +16,20 @@ public class SimpleTests {
 		System.err.println(encoder.encode("guest2"));
 		System.err.println(encoder.encode("guest3"));
 		System.err.println(encoder.encode("guest4"));
+	}
+
+	@Test
+	public void testNetworkInterface() {
+		try {
+			byte[] bytes = NetworkInterface.getByInetAddress(InetAddress.getLocalHost()).getHardwareAddress();
+			StringBuilder sb = new StringBuilder();
+			for (byte bt : bytes) {
+				sb.append(Integer.toHexString(bt & 0xff)).append('-');
+			}
+			sb.delete(sb.length() - 1, sb.length());
+			System.err.println(sb.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
