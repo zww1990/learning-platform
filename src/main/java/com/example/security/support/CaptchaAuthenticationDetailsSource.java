f9793ad50ac2a6b77c9ebefdd69665b9edfdbf56
zww1990 <zww1990@foxmail.com>
2020-06-07 22:56:34 +0800

    CaptchaAuthenticationDetailsSource.java

---
+++ src/main/java/com/example/security/support/CaptchaAuthenticationDetailsSource.java
@@ -0,0 +1,20 @@
+package com.example.security.support;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.authentication.AuthenticationDetailsSource;
+
+/**
+ * 验证码身份验证详细信息源
+ * 
+ * @author home
+ */
+public class CaptchaAuthenticationDetailsSource
+		implements AuthenticationDetailsSource<HttpServletRequest, CaptchaAuthenticationDetails> {
+
+	@Override
+	public CaptchaAuthenticationDetails buildDetails(HttpServletRequest context) {
+		return new CaptchaAuthenticationDetails(context);
+	}
+
+}
