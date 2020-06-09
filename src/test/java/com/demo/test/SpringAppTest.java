f27576205bcd07d74f3261cfc719fab9d22683c1
zhangweiwei01 <zhangweiwei01@5i5j.com>
2020-06-09 16:15:01 +0800

    SpringAppTest.java

--- src/test/java/com/demo/test/SpringAppTest.java
+++ src/test/java/com/demo/test/SpringAppTest.java
@@ -1,21 +1,12 @@
 package com.demo.test;
 
-import java.util.List;
 import javax.annotation.Resource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mybatis.dynamic.sql.SqlBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.demo.Application;
-import com.demo.dao.UserDao;
-import com.demo.dao.UserDynamicSqlSupport;
-import com.demo.model.User;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
@@ -34,37 +25,4 @@ public class SpringAppTest {
 			e.printStackTrace();
 		}
 	}
-
-	@Resource
-	private UserDao userDao;
-	@Resource
-	private ObjectMapper objectMapper;
-
-	@Test
-	public void testList() {
-		try {
-			List<User> users = this.userDao.selectByExample().where(UserDynamicSqlSupport.id, SqlBuilder.isIn(1, 2, 3))
-					.and(UserDynamicSqlSupport.age, SqlBuilder.isBetween(11).and(22))
-					.and(UserDynamicSqlSupport.name, SqlBuilder.isLikeCaseInsensitive("%张%"))
-					.orderBy(UserDynamicSqlSupport.birthday.descending(), UserDynamicSqlSupport.id).build().execute();
-			System.err.println(this.objectMapper.writeValueAsString(users));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Test
-	public void testPage() {
-		try {
-			Page<User> page = PageHelper.startPage(1, 10)
-					.doSelectPage(() -> this.userDao.selectByExample().build().execute());
-			System.err.println(page);
-			PageInfo<User> pageInfo = PageHelper.startPage(1, 10)
-					.doSelectPageInfo(() -> this.userDao.selectByExample().build().execute());
-			System.err.println(pageInfo);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 }
