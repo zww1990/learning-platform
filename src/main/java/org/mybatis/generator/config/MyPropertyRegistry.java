a64348ace427ef5e893cd8390e24e3b531a546da
zhangweiwei01 <zhangweiwei01@5i5j.com>
2020-06-10 10:39:55 +0800

    MyPropertyRegistry.java

--- src/main/java/org/mybatis/generator/config/MyPropertyRegistry.java
+++ src/main/java/org/mybatis/generator/config/MyPropertyRegistry.java
@@ -5,7 +5,7 @@ package org.mybatis.generator.config;
  * @date 2018年4月8日,上午9:46:01
  * @description
  */
-public class MyPropertyRegistry extends PropertyRegistry {
+public class MyPropertyRegistry {
 	public static final String COMMENT_GENERATOR_AUTHOR = "author";
 	public static final String COMMENT_GENERATOR_SWAGGER_ANNOTATIONS = "addSwaggerAnnotations";
 }
