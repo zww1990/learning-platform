cd64c01b65dd24bdfeb20cbe56c5f8e80b0c2cae
zww1990 <zww1990@foxmail.com>
2020-06-14 22:30:37 +0800

    HelloController.java

--- src/main/java/com/example/security/controller/HelloController.java
+++ src/main/java/com/example/security/controller/HelloController.java
@@ -2,23 +2,29 @@ package com.example.security.controller;
 
 import java.util.Arrays;
 
+import javax.annotation.Resource;
+
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	@Resource
+	private ServerProperties properties;
+
 	@GetMapping("/hello")
 	public Object hello() {
-		return Arrays.asList("hello");
+		return Arrays.asList("hello", this.properties.getPort());
 	}
 
 	@GetMapping("/admin/hello")
 	public Object admin() {
-		return Arrays.asList("admin");
+		return Arrays.asList("admin", this.properties.getPort());
 	}
 
 	@GetMapping("/guest/hello")
 	public Object guest() {
-		return Arrays.asList("guest");
+		return Arrays.asList("guest", this.properties.getPort());
 	}
 }
