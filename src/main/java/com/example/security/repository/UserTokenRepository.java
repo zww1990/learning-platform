57bfa75bbe91524eaa829e6c869d7a2737ba1b51
zww1990 <zww1990@foxmail.com>
2020-06-14 15:50:37 +0800

    refactor

--- src/main/java/com/example/security/repository/UserTokenRepository.java
+++ src/main/java/com/example/security/repository/UserTokenRepository.java
@@ -2,13 +2,13 @@ package com.example.security.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.security.model.RememberMe;
+import com.example.security.model.UserToken;
 
 /**
  * 自动登录持久化
  * 
  * @author home
  */
-public interface RememberMeRepository extends JpaRepository<RememberMe, String> {
+public interface UserTokenRepository extends JpaRepository<UserToken, String> {
 	int deleteByUsername(String username);
 }
