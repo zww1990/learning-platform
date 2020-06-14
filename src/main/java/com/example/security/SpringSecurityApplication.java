96b889233d0f6edf7a33bb638f21d6d9517dabac
zww1990 <zww1990@foxmail.com>
2020-06-14 18:23:15 +0800

    SpringSecurityApplication.java

--- src/main/java/com/example/security/SpringSecurityApplication.java
+++ src/main/java/com/example/security/SpringSecurityApplication.java
@@ -6,10 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 import org.springframework.boot.autoconfigure.dao.PersistenceExceptionTranslationAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
 import org.springframework.boot.autoconfigure.info.ProjectInfoAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
-import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
-import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
 import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
@@ -21,11 +20,19 @@ import org.springframework.context.ConfigurableApplicationContext;
  * 
  * @author home
  */
-@SpringBootApplication(exclude = { TaskExecutionAutoConfiguration.class, TaskSchedulingAutoConfiguration.class,
-		ProjectInfoAutoConfiguration.class, ValidationAutoConfiguration.class, AopAutoConfiguration.class,
-		MultipartAutoConfiguration.class, PersistenceExceptionTranslationAutoConfiguration.class,
-		RestTemplateAutoConfiguration.class, JdbcTemplateAutoConfiguration.class,
-		WebSocketServletAutoConfiguration.class })
+@SpringBootApplication(exclude = { //
+//		TaskExecutionAutoConfiguration.class, // 任务执行自动配置
+//		TaskSchedulingAutoConfiguration.class, // 任务计划自动配置
+		ProjectInfoAutoConfiguration.class, // 项目信息自动配置
+		ValidationAutoConfiguration.class, // 验证自动配置
+		AopAutoConfiguration.class, // AOP自动配置
+		MultipartAutoConfiguration.class, // 分段上传自动配置
+		PersistenceExceptionTranslationAutoConfiguration.class, // 持久化异常转换自动配置
+		RestTemplateAutoConfiguration.class, // REST模板自动配置
+		JdbcTemplateAutoConfiguration.class, // JDBC模板自动配置
+		WebSocketServletAutoConfiguration.class, // WebSocket自动配置
+		SpringDataWebAutoConfiguration.class,// Spring Data Web自动配置
+})
 public class SpringSecurityApplication {
 	private static final Logger log = LoggerFactory.getLogger(SpringSecurityApplication.class);
 
