cff73f45855bd60a6cbb33c1eeed6ea7f0256d9e
zww1990 <zww1990@foxmail.com>
2020-06-26 19:13:38 +0800

    CasProperties.java

--- src/main/java/com/example/security/config/CasProperties.java
+++ src/main/java/com/example/security/config/CasProperties.java
@@ -3,10 +3,17 @@ package com.example.security.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
+/**
+ * CAS属性配置类
+ * 
+ * @author home
+ */
 @ConfigurationProperties(prefix = "cas")
 public class CasProperties {
+	/** CAS服务端配置 */
 	@NestedConfigurationProperty
 	private Server server = new Server();
+	/** CAS客户端配置 */
 	@NestedConfigurationProperty
 	private Client client = new Client();
 
@@ -26,9 +33,17 @@ public class CasProperties {
 		this.client = client;
 	}
 
+	/**
+	 * CAS服务端配置类
+	 * 
+	 * @author home
+	 */
 	public static class Server {
+		/** 服务端URL前缀 */
 		private String prefix;
+		/** 服务端登录URL */
 		private String login;
+		/** 服务端注销URL */
 		private String logout;
 
 		public String getPrefix() {
@@ -56,9 +71,17 @@ public class CasProperties {
 		}
 	}
 
+	/**
+	 * CAS客户端配置类
+	 * 
+	 * @author home
+	 */
 	public static class Client {
+		/** 客户端URL前缀 */
 		private String prefix;
+		/** 客户端登录URL */
 		private String login;
+		/** 客户端注销URL */
 		private String logout;
 
 		public String getPrefix() {
