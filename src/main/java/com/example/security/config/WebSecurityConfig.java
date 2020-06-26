993020575a522a07a20c8e9d8de44d54ed1b7f3f
zww1990 <zww1990@foxmail.com>
2020-06-26 19:13:53 +0800

    WebSecurityConfig.java

--- src/main/java/com/example/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/security/config/WebSecurityConfig.java
@@ -67,6 +67,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Resource
 	private CasProperties casProperties;
 
+	/**
+	 * @return 获取网卡MAC地址
+	 * @throws Exception
+	 */
 	private String getByInetAddress() throws Exception {
 		byte[] bytes = NetworkInterface.getByInetAddress(InetAddress.getLocalHost()).getHardwareAddress();
 		StringBuilder sb = new StringBuilder();
@@ -79,6 +83,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// 根据传递的自定义身份验证提供器添加身份验证。
+		// 由于身份验证提供器实现未知，因此所有自定义必须在外部完成，并且立即返回身份验证管理器构建器。
 		auth.authenticationProvider(this.authenticationProvider());
 	}
 
@@ -90,12 +96,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/guest/**")//
 				.hasRole("GUEST")//
 				.antMatchers("/login/cas")//
-				.permitAll()//
+				.permitAll()// 指定任何人都可以允许URL。
 				.anyRequest()// 映射任何请求。
 				.authenticated()// 指定任何经过身份验证的用户允许的URL。
 				.and()// 使用SecurityConfigurer完成后返回SecurityBuilder。这对于方法链接很有用。
-				.exceptionHandling()//
-				.authenticationEntryPoint(this.authenticationEntryPoint())//
+				.exceptionHandling()// 允许配置异常处理。当使用 WebSecurityCongurerAdapter时，会自动应用此功能。
+				.authenticationEntryPoint(this.authenticationEntryPoint())// 设置要使用的身份验证输入点。
 				.and()//
 				.csrf()// 添加了CSRF支持。
 				.csrfTokenRepository(new LazyCsrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))// 指定要使用的CsrfTokenRepository。
@@ -106,11 +112,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //				.tokenRepository(this.userTokenService())// 指定要使用的PersistentTokenRepository实例。
 //				.tokenValiditySeconds(60 * 60 * 24)// 允许指定令牌有效的时间（以秒为单位）
 //				.and()//
-				.logout()//
-				.disable()//
-				.addFilter(this.casAuthenticationFilter())//
-				.addFilterBefore(this.singleSignOutFilter(), CasAuthenticationFilter.class)//
-				.addFilterAt(this.logoutFilter(), LogoutFilter.class)//
+				.logout()// 提供注销支持。
+				.disable()// 禁用注销
+				.addFilter(this.casAuthenticationFilter())// 添加过滤器
+				.addFilterBefore(this.singleSignOutFilter(), CasAuthenticationFilter.class)// 允许在一个已知过滤器类之前添加过滤器。
+				.addFilterAt(this.logoutFilter(), LogoutFilter.class)// 将过滤器添加到指定过滤器类的位置。
 				.sessionManagement()// 允许配置会话管理。
 				.maximumSessions(1)// 控制用户的最大会话数。 默认值为允许任意数量的用户。
 				.expiredSessionStrategy(this.sessionInformationExpiredStrategy())// 确定检测到过期会话时的行为。
@@ -126,8 +132,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 		web.debug(true)// 控制对Spring Security的调试支持。
 				.ignoring()// 允许添加Spring Security应该忽略的RequestMatcher实例。
-							// Spring Security提供的Web Security（包括SecurityContext）在匹配的HttpServletRequest上将不可用。
-							// 通常，注册的请求应该仅是静态资源的请求。 对于动态请求，请考虑映射请求以允许所有用户使用。
 				.antMatchers("/css/**"// 样式文件
 						, "/fonts/**"// 字体文件
 						, "/images/**"// 图片文件
@@ -232,13 +236,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	/**
-	 * @return 维护SessionInformation实例的注册表。
+	 * @return 从Spring会话检索会话信息而不是维护其本身的会话注册表。这允许在群集环境中与Spring Security并发会话管理。
 	 */
 	@Bean
 	public SessionRegistry sessionRegistry() {
 		return new SpringSessionBackedSessionRegistry<>(this.sessionRepository);
 	}
 
+	/**
+	 * @return 存储与此CAS服务相关的属性。
+	 */
 	@Bean
 	public ServiceProperties serviceProperties() {
 		ServiceProperties service = new ServiceProperties();
@@ -246,6 +253,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return service;
 	}
 
+	/**
+	 * @return 异常转换过滤器用于通过JA-SIG中央身份验证服务（CAS）开始身份验证。
+	 */
 	@Bean
 	@Primary
 	public AuthenticationEntryPoint authenticationEntryPoint() {
@@ -255,11 +265,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return point;
 	}
 
+	/**
+	 * @return CAS协议v3的服务和代理票证验证服务。
+	 */
 	@Bean
 	public TicketValidator ticketValidator() {
 		return new Cas30ProxyTicketValidator(this.casProperties.getServer().getPrefix());
 	}
 
+	/**
+	 * @return 处理CAS服务票证，获取代理授予票证，并处理代理票证。
+	 * @throws Exception
+	 */
 	@Bean
 	public CasAuthenticationFilter casAuthenticationFilter() throws Exception {
 		CasAuthenticationFilter filter = new CasAuthenticationFilter();
@@ -268,6 +285,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return filter;
 	}
 
+	/**
+	 * @return 实现单点注销协议。它处理注册会话和销毁会话。
+	 */
 	@Bean
 	public SingleSignOutFilter singleSignOutFilter() {
 		SingleSignOutFilter filter = new SingleSignOutFilter();
@@ -275,6 +295,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return filter;
 	}
 
+	/**
+	 * @return 注销过滤器
+	 */
 	@Bean
 	public LogoutFilter logoutFilter() {
 		LogoutFilter filter = new LogoutFilter(this.casProperties.getServer().getLogout(),
