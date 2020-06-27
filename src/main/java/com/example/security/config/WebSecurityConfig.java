8dbfb481fb8f6acb900a719f7f5b1281a7e21099
zww1990 <zww1990@foxmail.com>
2020-06-27 21:39:49 +0800

    WebSecurityConfig.java

--- src/main/java/com/example/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/security/config/WebSecurityConfig.java
@@ -61,6 +61,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Resource
 	private FindByIndexNameSessionRepository<Session> sessionRepository;
 
+	/**
+	 * @return 获取网卡MAC地址
+	 * @throws Exception
+	 */
 	private String getByInetAddress() throws Exception {
 		byte[] bytes = NetworkInterface.getByInetAddress(InetAddress.getLocalHost()).getHardwareAddress();
 		StringBuilder sb = new StringBuilder();
@@ -73,22 +77,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(this.userDetailsService())// 根据传入的自定义UserDetailsService添加身份验证。
-		// 然后，它返回DaoAuthenticationConfigurer以允许自定义身份验证。
-		;
+		// 根据传递的自定义用户详细信息服务添加身份验证。然后返回Dao身份验证配置器，以允许自定义身份验证。
+		auth.userDetailsService(this.userDetailsService());
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()// 允许基于HttpServletRequest使用限制访问
-				.antMatchers("/admin/**")// 创建未指定HttpMethod的AntPathRequestMatcher实例列表。
-				.hasRole("ADMIN")// 指定URL的快捷方式需要特定的角色。
-				.antMatchers("/guest/**")//
-				.hasRole("GUEST")//
-				.antMatchers("/ajax")//
-				.permitAll()//
-				.anyRequest()// 映射任何请求。
-				.authenticated()// 指定任何经过身份验证的用户允许的URL。
+		http.authorizeRequests()// 允许使用RequestMatcher实现（即通过URL模式）基于HttpServletRequest限制访问。
+				.antMatchers("/admin/**").hasRole("ADMIN")// 指定URL的快捷方式需要特定的角色。
+				.antMatchers("/guest/**").hasRole("GUEST")//
+				.antMatchers("/ajax").permitAll()// 指定任何人都可以允许URL。
+				.anyRequest().authenticated()// 指定任何经过身份验证的用户允许的URL。
 				.and()// 使用SecurityConfigurer完成后返回SecurityBuilder。这对于方法链接很有用。
 				.formLogin()// 指定支持基于表单的身份验证。 如果没有指定，将会生成一个默认的登录页面。
 				.loginPage("/login")// 如果需要登录，指定发送用户的URL。 则在未指定此属性时将会生成默认登录页面。
@@ -119,14 +118,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 		web.debug(true)// 控制对Spring Security的调试支持。
 				.ignoring()// 允许添加Spring Security应该忽略的RequestMatcher实例。
-							// Spring Security提供的Web Security（包括SecurityContext）在匹配的HttpServletRequest上将不可用。
-							// 通常，注册的请求应该仅是静态资源的请求。 对于动态请求，请考虑映射请求以允许所有用户使用。
-				.antMatchers("/css/**"// 样式文件
-						, "/fonts/**"// 字体文件
-						, "/images/**"// 图片文件
-						, "/js/**"// 脚本文件
-						, "/captcha"// 验证码
-				);
+				.antMatchers("/css/**", "/fonts/**", "/images/**", "/js/**", "/captcha");
 	}
 
 	@Bean
