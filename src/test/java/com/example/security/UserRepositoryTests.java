dee3932a12a40b765aa13ee4223cc88667f0e12b
zww1990 <zww1990@foxmail.com>
2020-06-28 23:37:24 +0800

    单元测试

--- src/test/java/com/example/security/UserRepositoryTests.java
+++ src/test/java/com/example/security/UserRepositoryTests.java
@@ -11,7 +11,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.security.model.Authority;
+import com.example.security.model.Menu;
 import com.example.security.model.User;
+import com.example.security.repository.AuthorityRepository;
+import com.example.security.repository.MenuRepository;
 import com.example.security.repository.UserRepository;
 
 @RunWith(SpringRunner.class)
@@ -21,13 +24,19 @@ public class UserRepositoryTests {
 	private UserRepository userdao;
 	@Resource
 	private PasswordEncoder encoder;
+	@Resource
+	private MenuRepository menudao;
+	@Resource
+	private AuthorityRepository authdao;
 
 	@Test
 	public void testSave() {
 		try {
 			Authority a1 = new Authority("超级管理员", "ROLE_ADMIN");
+			this.authdao.save(a1);
 
 			Authority a2 = new Authority("访客", "ROLE_GUEST");
+			this.authdao.save(a2);
 
 			User u1 = new User("admin", this.encoder.encode("admin"), true, true, true, true, Arrays.asList(a1));
 			this.userdao.save(u1);
@@ -35,6 +44,14 @@ public class UserRepositoryTests {
 			User u2 = new User("guest", this.encoder.encode("guest"), true, true, true, true, Arrays.asList(a2));
 			this.userdao.save(u2);
 
+			Menu m1 = new Menu("管理页面", "/admin/**", true, null, Arrays.asList(a1));
+			this.menudao.save(m1);
+
+			Menu m2 = new Menu("访客页面", "/guest/**", true, null, Arrays.asList(a2));
+			this.menudao.save(m2);
+
+			Menu m3 = new Menu("测试页面", "/ajax", true, null, Arrays.asList(a1, a2));
+			this.menudao.save(m3);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
