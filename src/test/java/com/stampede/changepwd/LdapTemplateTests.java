9fd04b98a4a05610b747b08068ef60856c53d4ef
zww1990 <zww1990@foxmail.com>
2020-07-07 13:05:10 +0800

    LdapTemplateTests.java

--- src/test/java/com/stampede/changepwd/LdapTemplateTests.java
+++ src/test/java/com/stampede/changepwd/LdapTemplateTests.java
@@ -1,13 +1,11 @@
 package com.stampede.changepwd;
 
 import javax.annotation.Resource;
-import javax.naming.NamingException;
 import javax.naming.directory.Attributes;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.ldap.core.AttributesMapper;
-import org.springframework.ldap.core.ContextMapper;
 import org.springframework.ldap.core.DirContextAdapter;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.filter.AndFilter;
@@ -26,7 +24,18 @@ public class LdapTemplateTests {
 			AndFilter filter = new AndFilter();
 			filter.and(new EqualsFilter("objectclass", "person"));
 			filter.and(new EqualsFilter("uid", ""));
-			this.ldapTemplate.search("", filter.encode(), new PersonAttributesMapper());
+			this.ldapTemplate.search("", filter.encode(), (Attributes attributes) -> {
+				System.err.println(attributes.get("givenname").get());
+				System.err.println(attributes.get("sn").get());
+				byte[] bs = (byte[]) attributes.get("userpassword").get();
+				System.err.println(new String(bs));
+				System.err.println(attributes.get("uidnumber").get());
+				System.err.println(attributes.get("gidnumber").get());
+				System.err.println(attributes.get("mail").get());
+				System.err.println(attributes.get("uid").get());
+				System.err.println(attributes.get("cn").get());
+				return attributes;
+			});
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -38,7 +47,19 @@ public class LdapTemplateTests {
 			AndFilter filter = new AndFilter();
 			filter.and(new EqualsFilter("objectclass", "person"));
 			filter.and(new EqualsFilter("uid", ""));
-			this.ldapTemplate.search("", filter.encode(), new PersonContextMapper());
+			this.ldapTemplate.search("", filter.encode(), (Object ctx) -> {
+				DirContextAdapter adapter = (DirContextAdapter) ctx;
+				System.err.println(adapter.getObjectAttribute("givenName"));
+				System.err.println(adapter.getObjectAttribute("sn"));
+				byte[] bs = (byte[]) adapter.getObjectAttribute("userPassword");
+				System.err.println(new String(bs));
+				System.err.println(adapter.getObjectAttribute("uidNumber"));
+				System.err.println(adapter.getObjectAttribute("gidNumber"));
+				System.err.println(adapter.getObjectAttribute("mail"));
+				System.err.println(adapter.getObjectAttribute("uid"));
+				System.err.println(adapter.getObjectAttribute("cn"));
+				return ctx;
+			});
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -57,36 +78,4 @@ public class LdapTemplateTests {
 		}
 	}
 
-	private static class PersonAttributesMapper implements AttributesMapper<Object> {
-		@Override
-		public Object mapFromAttributes(Attributes attributes) throws NamingException {
-			System.err.println(attributes.get("givenname").get());
-			System.err.println(attributes.get("sn").get());
-			byte[] bs = (byte[]) attributes.get("userpassword").get();
-			System.err.println(new String(bs));
-			System.err.println(attributes.get("uidnumber").get());
-			System.err.println(attributes.get("gidnumber").get());
-			System.err.println(attributes.get("mail").get());
-			System.err.println(attributes.get("uid").get());
-			System.err.println(attributes.get("cn").get());
-			return attributes;
-		}
-	}
-
-	private static class PersonContextMapper implements ContextMapper<Object> {
-		@Override
-		public Object mapFromContext(Object ctx) throws NamingException {
-			DirContextAdapter adapter = (DirContextAdapter) ctx;
-			System.err.println(adapter.getObjectAttribute("givenName"));
-			System.err.println(adapter.getObjectAttribute("sn"));
-			byte[] bs = (byte[]) adapter.getObjectAttribute("userPassword");
-			System.err.println(new String(bs));
-			System.err.println(adapter.getObjectAttribute("uidNumber"));
-			System.err.println(adapter.getObjectAttribute("gidNumber"));
-			System.err.println(adapter.getObjectAttribute("mail"));
-			System.err.println(adapter.getObjectAttribute("uid"));
-			System.err.println(adapter.getObjectAttribute("cn"));
-			return ctx;
-		}
-	}
 }
