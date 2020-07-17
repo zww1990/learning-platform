b6824b69d02b1aad3a4667e8d8e622961516d950
zww1990 <zww1990@foxmail.com>
2020-07-17 23:12:00 +0800

    SwaggerConfiguration.java

--- src/main/java/com/example/demo/web/config/SwaggerConfiguration.java
+++ src/main/java/com/example/demo/web/config/SwaggerConfiguration.java
@@ -2,28 +2,26 @@ package com.example.demo.web.config;
 
 import java.util.Collections;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
-@ConditionalOnProperty(name = "swagger.enable", matchIfMissing = true)
+@EnableOpenApi
 @ConditionalOnWebApplication
 @EnableConfigurationProperties(SwaggerProperties.class)
 public class SwaggerConfiguration {
 	@Bean
 	public Docket api(SwaggerProperties props) {
-		return new Docket(DocumentationType.SWAGGER_2).select()
+		return new Docket(DocumentationType.OAS_30).select()
 				.apis(RequestHandlerSelectors.basePackage(props.getBasePackage())).build().apiInfo(this.apiInfo(props));
 	}
 
