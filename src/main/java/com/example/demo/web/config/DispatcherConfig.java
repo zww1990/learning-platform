ad3f8a173c63248337a8d3e315bc681d4d7b1bbe
zww1990 <zww1990@foxmail.com>
2020-07-17 22:59:51 +0800

    DispatcherConfig.java

--- src/main/java/com/example/demo/web/config/DispatcherConfig.java
+++ src/main/java/com/example/demo/web/config/DispatcherConfig.java
@@ -2,14 +2,16 @@ package com.example.demo.web.config;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+
 import javax.annotation.Resource;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.AbstractHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
@@ -33,9 +35,4 @@ public class DispatcherConfig implements WebMvcConfigurer {
 		argumentResolvers.add(new UserInfoHandlerMethodArgumentResolver(this.objectMapper));
 	}
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-	}
 }
