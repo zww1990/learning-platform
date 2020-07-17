e2667126a5620eef26297951bef11c49bbbad9b8
zww1990 <zww1990@foxmail.com>
2020-07-17 23:09:24 +0800

    SwaggerProperties.java

--- src/main/java/com/example/demo/web/config/SwaggerProperties.java
+++ src/main/java/com/example/demo/web/config/SwaggerProperties.java
@@ -2,7 +2,7 @@ package com.example.demo.web.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("swagger")
+@ConfigurationProperties(prefix = "springfox.swagger")
 public class SwaggerProperties {
 	private String title;
 	private String description;
@@ -13,7 +13,6 @@ public class SwaggerProperties {
 	private String email;
 	private String license;
 	private String licenseUrl;
-	private boolean enable;
 	private String basePackage;
 
 	public String getTitle() {
@@ -88,31 +87,12 @@ public class SwaggerProperties {
 		this.licenseUrl = licenseUrl;
 	}
 
-	public boolean isEnable() {
-		return enable;
-	}
-
-	public void setEnable(boolean enable) {
-		this.enable = enable;
-	}
-
 	public String getBasePackage() {
 		return basePackage;
 	}
 
 	public void setBasePackage(String basePackage) {
 		this.basePackage = basePackage;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("[title=").append(title).append(", description=").append(description).append(", version=")
-				.append(version).append(", termsOfServiceUrl=").append(termsOfServiceUrl).append(", name=").append(name)
-				.append(", url=").append(url).append(", email=").append(email).append(", license=").append(license)
-				.append(", licenseUrl=").append(licenseUrl).append(", enable=").append(enable).append(", basePackage=")
-				.append(basePackage).append("]");
-		return builder.toString();
 	}
 
 }
