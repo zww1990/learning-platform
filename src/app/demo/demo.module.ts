cd60e594b0ad5ebb4616211070d0644becf42c12
zww1990 <zww1990@foxmail.com>
2020-10-09 15:33:54 +0800

    共享模块

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -6,24 +6,6 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { Page3Component } from './page3/page3.component';
 import { Page4Component } from './page4/page4.component';
-import { NzCalendarModule } from 'ng-zorro-antd/calendar';
-import { NzDrawerModule } from 'ng-zorro-antd/drawer';
-import { NzUploadModule } from 'ng-zorro-antd/upload';
-import { NzFormModule } from 'ng-zorro-antd/form';
-import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
-import { NzTableModule } from 'ng-zorro-antd/table';
-import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
-import { NzSelectModule } from 'ng-zorro-antd/select';
-import { NzSliderModule } from 'ng-zorro-antd/slider';
-import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
-import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
-import { NzSwitchModule } from 'ng-zorro-antd/switch';
-import { NzRadioModule } from 'ng-zorro-antd/radio';
-import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
-import { NzIconModule } from 'ng-zorro-antd/icon';
 
 const routes: Routes = [
   { path: 'index', component: IndexComponent },
@@ -41,24 +23,6 @@ const routes: Routes = [
   imports: [
     RouterModule.forChild(routes),
     SharedModule,
-    NzCalendarModule,
-    NzDrawerModule,
-    NzUploadModule,
-    NzFormModule,
-    NzAutocompleteModule,
-    NzTableModule,
-    NzInputNumberModule,
-    NzSelectModule,
-    NzSliderModule,
-    NzDatePickerModule,
-    NzTimePickerModule,
-    NzSwitchModule,
-    NzRadioModule,
-    NzCheckboxModule,
-    NzButtonModule,
-    NzInputModule,
-    NzPopconfirmModule,
-    NzIconModule
   ],
   declarations: [
     IndexComponent,
