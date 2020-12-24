9612d70469b23f5dedb5fcce58753d6471fcd69a
zww1990 <zww1990@foxmail.com>
2020-12-24 17:47:24 +0800

    SpringConfig.java

--- src/main/java/com/example/springschedule/config/SpringConfig.java
+++ src/main/java/com/example/springschedule/config/SpringConfig.java
@@ -2,9 +2,7 @@ package com.example.springschedule.config;
 
 import org.quartz.JobDetail;
 import org.quartz.Trigger;
-import org.quartz.spi.TriggerFiredBundle;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -25,7 +23,9 @@ public class SpringConfig {
 	@Bean
 	public SchedulerFactoryBean quartzScheduler(ApplicationContext context, Trigger[] triggers) {
 		SchedulerFactoryBean quartzScheduler = new SchedulerFactoryBean();
-		quartzScheduler.setJobFactory(new AutowireCapableBeanJobFactory(context.getAutowireCapableBeanFactory()));
+		SpringBeanJobFactory jobFactory = new SpringBeanJobFactory();
+		jobFactory.setApplicationContext(context);
+		quartzScheduler.setJobFactory(jobFactory);
 		quartzScheduler.setTriggers(triggers);
 		return quartzScheduler;
 	}
@@ -43,21 +43,5 @@ public class SpringConfig {
 		trigger.setCronExpression(cronExpression);
 		trigger.setJobDetail(jobDetail);
 		return trigger;
-	}
-
-	protected class AutowireCapableBeanJobFactory extends SpringBeanJobFactory {
-		private final AutowireCapableBeanFactory beanFactory;
-
-		protected AutowireCapableBeanJobFactory(AutowireCapableBeanFactory beanFactory) {
-			this.beanFactory = beanFactory;
-		}
-
-		@Override
-		protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
-			Object jobInstance = super.createJobInstance(bundle);
-			this.beanFactory.autowireBean(jobInstance);
-			this.beanFactory.initializeBean(jobInstance, null);
-			return jobInstance;
-		}
 	}
 }
