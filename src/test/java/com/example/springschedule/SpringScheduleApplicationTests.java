49f58de125fdc81f0d30ef0f3e263761f657719b
zww1990 <zww1990@foxmail.com>
2020-12-24 16:57:15 +0800

    SpringScheduleApplicationTests.java

--- src/test/java/com/example/springschedule/SpringScheduleApplicationTests.java
+++ src/test/java/com/example/springschedule/SpringScheduleApplicationTests.java
@@ -1,13 +1,11 @@
 package com.example.springschedule;
 
 import javax.annotation.Resource;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringScheduleApplicationTests {
 	@Resource
