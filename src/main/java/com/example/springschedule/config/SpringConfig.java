846700d70eb442b99a6c8eb219160f032a9db79d
zww1990 <zww1990@foxmail.com>
2020-12-24 16:15:53 +0800

    pom.xml SpringConfig.java application.properties

--- src/main/java/com/example/springschedule/config/SpringConfig.java
+++ src/main/java/com/example/springschedule/config/SpringConfig.java
@@ -1,49 +1,47 @@
 package com.example.springschedule.config;
 
-import java.util.Optional;
 import org.quartz.JobDetail;
 import org.quartz.Trigger;
 import org.quartz.spi.TriggerFiredBundle;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
 import com.example.springschedule.service.JobService;
 
 @Configuration
 @ComponentScan("com.example.springschedule")
+@PropertySource("classpath:/application.properties")
 public class SpringConfig {
 
 	@Bean
-	public SchedulerFactoryBean quartzScheduler(ApplicationContext context,
-			@Autowired(required = false) Trigger[] triggers) {
+	public SchedulerFactoryBean quartzScheduler(ApplicationContext context, Trigger[] triggers) {
 		SchedulerFactoryBean quartzScheduler = new SchedulerFactoryBean();
 		quartzScheduler.setJobFactory(new AutowireCapableBeanJobFactory(context.getAutowireCapableBeanFactory()));
-		Optional.ofNullable(triggers).ifPresent(quartzScheduler::setTriggers);
+		quartzScheduler.setTriggers(triggers);
 		return quartzScheduler;
 	}
 
 	@Bean
 	public JobDetailFactoryBean jobDetail() {
-		Class<JobService> job = JobService.class;
 		JobDetailFactoryBean jobDetail = new JobDetailFactoryBean();
-		jobDetail.setJobClass(job);
-		jobDetail.setName(job.getName());
+		jobDetail.setJobClass(JobService.class);
 		return jobDetail;
 	}
 
 	@Bean
-	public CronTriggerFactoryBean trigger(JobDetail jobDetail) {
+	public CronTriggerFactoryBean trigger(JobDetail jobDetail, @Value("${cron.expression}") String cronExpression) {
 		CronTriggerFactoryBean trigger = new CronTriggerFactoryBean();
-		trigger.setCronExpression("*/10 * * * * ?");
+		trigger.setCronExpression(cronExpression);
 		trigger.setJobDetail(jobDetail);
-		trigger.setName(jobDetail.getKey().getName());
 		return trigger;
 	}
 
