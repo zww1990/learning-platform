ae8694c3ae9abb5866888df651d0c2b5324d33e9
zww1990 <zww1990@foxmail.com>
2021-02-14 17:48:06 +0800

    application.yml bootstrap.yml

--- nacos-provider/src/main/java/com/example/demoprovider/nacosdiscovery/NacosDiscoveryConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.example.demoprovider.nacosdiscovery;
-
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
-
-@EnableDiscoveryClient
-@Configuration
-public class NacosDiscoveryConfiguration {
-}
