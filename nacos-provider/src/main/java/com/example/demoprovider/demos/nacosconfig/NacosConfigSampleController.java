b04677c25c6566adc1681b23e2cf066d7cba4380
zww1990 <zww1990@foxmail.com>
2021-02-14 17:41:10 +0800

    NacosConfigSampleController.java

--- nacos-provider/src/main/java/com/example/demoprovider/demos/nacosconfig/NacosConfigSampleController.java
+++ nacos-provider/src/main/java/com/example/demoprovider/demos/nacosconfig/NacosConfigSampleController.java
@@ -2,20 +2,19 @@ package com.example.demoprovider.demos.nacosconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope
 public class NacosConfigSampleController {
 
-    @Autowired
-    private User user;
+	@Autowired
+	private User user;
 
-    @RequestMapping("/user")
-    public String user() {
-        return "[HTTP] " + user;
-    }
+	@GetMapping("/user")
+	public String user() {
+		return "[HTTP] " + user;
+	}
 
 }
-
