bf2fb51bc5e0bf8d6c3d1dfb873ee029fbc7c1a1
zww1990 <zww1990@foxmail.com>
2021-02-14 18:27:01 +0800

    DemoConsumerApplication.java

--- nacos-consumer/src/main/java/com/example/democonsumer/DemoConsumerApplication.java
+++ nacos-consumer/src/main/java/com/example/democonsumer/DemoConsumerApplication.java
@@ -2,12 +2,16 @@ package com.example.democonsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients // 激活 @FeignClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoConsumerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoConsumerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConsumerApplication.class, args);
+	}
 
 }
