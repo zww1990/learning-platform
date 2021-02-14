578fb6195987777e1a86ca35d965aab295020984
zww1990 <zww1990@foxmail.com>
2021-02-14 17:40:11 +0800

    User.java

--- nacos-provider/src/main/java/com/example/demoprovider/demos/nacosconfig/User.java
+++ nacos-provider/src/main/java/com/example/demoprovider/demos/nacosconfig/User.java
@@ -1,49 +1,35 @@
 package com.example.demoprovider.demos.nacosconfig;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @RefreshScope
 @ConfigurationProperties(prefix = "user")
-public class User implements InitializingBean, DisposableBean {
+public class User {
 
-    private String name;
+	private String name;
 
-    private int age;
+	private int age;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public int getAge() {
-        return age;
-    }
+	public int getAge() {
+		return age;
+	}
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+	public void setAge(int age) {
+		this.age = age;
+	}
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "User{" + "name='" + name + '\'' + ", age=" + age + '}';
+	}
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("[afterPropertiesSet()] " + toString());
-    }
-
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("[destroy()] " + toString());
-    }
 }
