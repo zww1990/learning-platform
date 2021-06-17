91ac19640d6508bbbcdd3b9bd2dd96e30b473502
zww1990 <zww1990@foxmail.com>
2021-06-17 17:23:24 +0800

    refactor

--- spring-swagger-openapi/src/main/java/com/example/demo/config/SwaggerConfiguration.java
+++ spring-swagger-openapi/src/main/java/com/example/demo/config/SwaggerConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.demo.web.config;
+package com.example.demo.config;
 
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
