8ac809126282a74457b9e667ba82c25c8a6b0a7a
siqianwen <siqianwen@5i5j.com>
2021-09-18 21:27:00 +0800

    IndexController.java

--- src/main/java/com/stampede/changepwd/controller/IndexController.java
+++ src/main/java/com/stampede/changepwd/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.stampede.changepwd.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 import com.stampede.changepwd.constant.Constants;
 
 /**
@@ -19,6 +20,7 @@ public class IndexController {
 	 */
 	@GetMapping
 	public ModelAndView index() {
-		return new ModelAndView("index").addObject("image", Constants.randomImage());
+		return new ModelAndView("index")//
+				.addObject("image", Constants.randomImage());
 	}
 }
