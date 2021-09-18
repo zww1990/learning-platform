c7e100009f8db0166170e705ade2d7ed584a2dc2
siqianwen <siqianwen@5i5j.com>
2021-09-18 20:10:42 +0800

    PersonJobServiceTests.java

--- src/test/java/com/stampede/changepwd/PersonJobServiceTests.java
+++ src/test/java/com/stampede/changepwd/PersonJobServiceTests.java
@@ -1,13 +1,12 @@
 package com.stampede.changepwd;
 
 import javax.annotation.Resource;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 import com.stampede.changepwd.service.PersonJobService;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class PersonJobServiceTests {
 	@Resource
