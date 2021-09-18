53f87e61feac1ef4c8054b8e5041fb6616c7d81e
siqianwen <siqianwen@5i5j.com>
2021-09-18 21:30:13 +0800

    PersonParam.java

--- src/main/java/com/stampede/changepwd/domain/PersonParam.java
+++ src/main/java/com/stampede/changepwd/domain/PersonParam.java
@@ -1,69 +1,34 @@
 package com.stampede.changepwd.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
 /**
  * @author ZhangWeiWei
  * @date 2020年2月16日,下午10:45:03
  * @description 人员参数模型
  */
+@Getter
+@Setter
+@Accessors(chain = true)
+@ToString
 public class PersonParam {
+	/**
+	 * 用户名
+	 */
 	private String username;
+	/**
+	 * 旧密码
+	 */
 	private String password;
+	/**
+	 * 新密码
+	 */
 	private String newpassword;
+	/**
+	 * 确认密码
+	 */
 	private String repassword;
-
-	/**
-	 * @return 用户名
-	 */
-	public String getUsername() {
-		return username;
-	}
-
-	/**
-	 * @return 旧密码
-	 */
-	public String getPassword() {
-		return password;
-	}
-
-	/**
-	 * @return 新密码
-	 */
-	public String getNewpassword() {
-		return newpassword;
-	}
-
-	/**
-	 * @return 确认密码
-	 */
-	public String getRepassword() {
-		return repassword;
-	}
-
-	/**
-	 * @param username 用户名
-	 */
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	/**
-	 * @param password 旧密码
-	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	/**
-	 * @param newpassword 新密码
-	 */
-	public void setNewpassword(String newpassword) {
-		this.newpassword = newpassword;
-	}
-
-	/**
-	 * @param repassword 确认密码
-	 */
-	public void setRepassword(String repassword) {
-		this.repassword = repassword;
-	}
 }
