cf49f280c66c21a7700977e637eb2470bc49bf13
zhangweiwei <zww1990@foxmail.com>
2021-11-19 16:29:15 +0800

    AppStaffClockLogDTO.java index.html

--- src/main/java/cn/net/yzl/oa/entity/AppStaffClockLogDTO.java
+++ src/main/java/cn/net/yzl/oa/entity/AppStaffClockLogDTO.java
@@ -1,8 +1,5 @@
 package cn.net.yzl.oa.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -16,11 +13,9 @@ public class AppStaffClockLogDTO {
 	/** 首次打卡时间 */
 	private String clockTimeMin;
 	/** 下班打卡状态(611：正常，612：迟到，614：上班缺卡，616：早退) */
-	@JsonProperty(access = Access.WRITE_ONLY)
 	private Integer clockWorkOffStatus;
 	private String clockWorkOffStatusName;
 	/** 上班打卡状态(611：正常，612：迟到，614：上班缺卡，616：早退) */
-	@JsonProperty(access = Access.WRITE_ONLY)
 	private Integer clockWorkOnStatus;
 	private String clockWorkOnStatusName;
 
