a435a696831aa5aa32c4dcdc1e9f7e0eb07045b4
zww1990 <zww1990@foxmail.com>
2021-12-12 17:44:15 +0800

    升级spring boot、spring cloud、alibaba cloud版本

--- nacos-consumer/src/main/java/com/example/democonsumer/DemoConsumerApplication.java
+++ nacos-consumer/src/main/java/com/example/democonsumer/DemoConsumerApplication.java
@@ -3,7 +3,10 @@ package com.example.democonsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableFeignClients // 激活 @FeignClient
 @EnableDiscoveryClient
@@ -12,6 +15,12 @@ public class DemoConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoConsumerApplication.class, args);
+	}
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- nacos-consumer/src/main/java/com/example/democonsumer/demos/nacosdiscoveryconsumer/RestTemplateController.java
+++ nacos-consumer/src/main/java/com/example/democonsumer/demos/nacosdiscoveryconsumer/RestTemplateController.java
@@ -2,7 +2,6 @@ package com.example.democonsumer.demos.nacosdiscoveryconsumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +13,6 @@ public class RestTemplateController {
 	@LoadBalanced
 	@Autowired
 	public RestTemplate restTemplate;
-
-	@LoadBalanced
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 
 	@GetMapping("/call/echo/{message}")
 	public String callEcho(@PathVariable String message) {
