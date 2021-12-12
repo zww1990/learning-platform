e9974c9516601dafe860ba969659ec4a6b2ff74c
zww1990 <zww1990@foxmail.com>
2021-12-12 23:17:17 +0800

    seata client

---
+++ seata-client-a/src/test/java/com/example/seataclient/SeataClientAApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.seataclient;
+
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+public class SeataClientAApplicationTests {
+	@Resource
+	private ApplicationContext context;
+
+	@Test
+	public void contextLoads() {
+		System.err.println(this.context.getBeanDefinitionCount());
+	}
+
+}
