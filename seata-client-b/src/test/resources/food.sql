c0919e4b6c2e91df757cc0a3883843edd2c0e9d1
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-12-15 10:48:17 +0800

    food.sql

--- seata-client-b/src/test/resources/food.sql
+++ seata-client-b/src/test/resources/food.sql
@@ -1,8 +1,9 @@
-create table food(
-	food_id int(10) primary key not null auto_increment comment '主键',
-	food_name varchar(20) not null comment '食品名称',
-	stock int(10) not null comment '库存'
-)charset=utf8 comment='食品表';
+CREATE TABLE food (
+  food_id int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  food_name varchar(20) NOT NULL COMMENT '食品名称',
+  stock int(10) NOT NULL COMMENT '库存',
+  PRIMARY KEY (food_id)
+) CHARSET=utf8mb4 COMMENT='食品表';
 
 insert into food (food_name, stock) values ('干脆面', 10000);
 
