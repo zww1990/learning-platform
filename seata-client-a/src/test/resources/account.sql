ab73c565f0709c91da6ee8313bc44322aa86e00e
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-12-15 10:45:32 +0800

    account.sql

--- seata-client-a/src/test/resources/account.sql
+++ seata-client-a/src/test/resources/account.sql
@@ -1,8 +1,9 @@
-create table account(
-	user_id int(10) primary key not null auto_increment comment '主键',
-	user_name varchar(20) not null comment '姓名',
-	balance decimal(10,2) not null comment '余额'
-)charset=utf8 comment='账户表';
+CREATE TABLE account (
+  user_id int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  user_name varchar(20) NOT NULL COMMENT '姓名',
+  balance decimal(10,2) NOT NULL COMMENT '余额',
+  PRIMARY KEY (user_id)
+) CHARSET=utf8mb4 COMMENT='账户表';
 
 insert into account (user_name, balance) values ('张三', 10000);
 
