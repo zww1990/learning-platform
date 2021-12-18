42d84d17364b2871cb79befdc162cf073121e5dc
zww1990 <zww1990@foxmail.com>
2021-12-18 19:23:46 +0800

    FoodController.java Food.java FoodControllerTests.java

--- seata-client-b/src/main/java/com/example/seataclient/controller/FoodController.java
+++ seata-client-b/src/main/java/com/example/seataclient/controller/FoodController.java
@@ -2,8 +2,6 @@ package com.example.seataclient.controller;
 
 import javax.annotation.Resource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,11 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.seataclient.domain.Food;
 import com.example.seataclient.mapper.FoodMapper;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/food")
+@Slf4j
 public class FoodController {
-
-	private static final Logger log = LoggerFactory.getLogger(FoodController.class);
 
 	@Resource
 	private FoodMapper foodMapper;
