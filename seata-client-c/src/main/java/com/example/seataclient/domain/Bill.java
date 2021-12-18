97a9a9ff26491ce1747d668f372ea4ac3b907ad4
zww1990 <zww1990@foxmail.com>
2021-12-18 20:47:41 +0800

    BillController.java Bill.java BillMapper.java BillMapperTests.java

---
+++ seata-client-c/src/main/java/com/example/seataclient/domain/Bill.java
@@ -0,0 +1,32 @@
+package com.example.seataclient.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+/**
+ * 账单
+ */
+@Getter
+@Setter
+@ToString
+@Accessors(chain = true)
+public class Bill {
+	/** 账单主键 */
+	private Integer billId;
+	/** 账单编号 */
+	private String billNum;
+	/** 账户主键 */
+	private Integer userId;
+	/** 食品主键 */
+	private Integer foodId;
+	/** 下单时间 */
+	private LocalDateTime orderTime;
+	/** 金额 */
+	private Double amount;
+	/** 数量 */
+	private Long quantity;
+}
