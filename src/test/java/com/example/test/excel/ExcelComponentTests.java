9b98c30e8de249e9ebb4fd169607073986631e70
zww1990 <zww1990@foxmail.com>
2022-01-08 18:41:31 +0800

    pom.xml AreaExcelComponent.java ExcelComponent.java ExcelComponentTests.java ExcelData.java

---
+++ src/test/java/com/example/test/excel/ExcelComponentTests.java
@@ -0,0 +1,20 @@
+package com.example.test.excel;
+
+import java.io.FileOutputStream;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+
+public class ExcelComponentTests {
+	private AreaExcelComponent areaExcelComponent = new AreaExcelComponent();
+
+	@Test
+	public void testWrite() {
+		try {
+			this.areaExcelComponent.write(new FileOutputStream(Paths.get("e:", "test3.xlsx").toFile()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
