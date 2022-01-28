92f666e8612dd21d41a59d0427762f8cf3d28264
zhangweiwei <zww1990@foxmail.com>
2022-01-28 15:52:52 +0800

    pom.xml User.java application.yml bootstrap.yml

--- nacos-consumer/src/main/java/com/example/democonsumer/demos/nacosconfig/User.java
+++ nacos-consumer/src/main/java/com/example/democonsumer/demos/nacosconfig/User.java
@@ -2,34 +2,22 @@ package com.example.democonsumer.demos.nacosconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @RefreshScope
+@Configuration
 @ConfigurationProperties(prefix = "user")
+@Getter
+@Setter
+@ToString
 public class User {
 
 	private String name;
 
 	private int age;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	@Override
-	public String toString() {
-		return "User{" + "name='" + name + '\'' + ", age=" + age + '}';
-	}
 
 }
