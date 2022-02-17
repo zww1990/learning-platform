cdbaa636037c314dc4bd153abc58452425cb45e5
zhangweiwei <zww1990@foxmail.com>
2022-02-17 18:29:40 +0800

    Gateway Server Application

---
+++ nacos-gateway/src/main/java/com/example/demogateway/GatewayServerApplication.java
@@ -0,0 +1,27 @@
+package com.example.demogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * GatewayServerApplication
+ * 
+ * @author weiwei
+ * @version v1
+ * @since 2022年2月17日,下午2:10:11
+ */
+@SpringBootApplication
+public class GatewayServerApplication {
+	/**
+	 * main
+	 * 
+	 * @param args
+	 * @throws Exception
+	 * @author weiwei
+	 * @version v1
+	 * @since 2022年2月17日,下午2:13:10
+	 */
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+}
