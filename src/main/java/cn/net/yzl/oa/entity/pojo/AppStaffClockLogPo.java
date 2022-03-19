66b9ee552ce44da6487e5b737f6ab44626605ae5
zww1990 <zww1990@foxmail.com>
2022-03-19 17:11:29 +0800

    AppStaffClockLogPo.java

--- src/main/java/cn/net/yzl/oa/entity/pojo/AppStaffClockLogPo.java
+++ src/main/java/cn/net/yzl/oa/entity/pojo/AppStaffClockLogPo.java
@@ -3,18 +3,11 @@ package cn.net.yzl.oa.entity.pojo;
 import java.math.BigDecimal;
 import java.util.Date;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 /**
  * app_staff_clock_log
  * 
  * @author
  */
-@Getter
-@Setter
-@ToString
 public class AppStaffClockLogPo {
 	private Integer id;
 
@@ -99,5 +92,157 @@ public class AppStaffClockLogPo {
 	 * 记录考勤时对应的规则id
 	 */
 	private Integer departAttendRuleId;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getStaffNo() {
+		return staffNo;
+	}
+
+	public Date getClockTime() {
+		return clockTime;
+	}
+
+	public String getWorkBeginTime() {
+		return workBeginTime;
+	}
+
+	public String getWorkEndTime() {
+		return workEndTime;
+	}
+
+	public String getCloseBeginTime() {
+		return closeBeginTime;
+	}
+
+	public String getCloseEndTime() {
+		return closeEndTime;
+	}
+
+	public BigDecimal getLongitude() {
+		return longitude;
+	}
+
+	public BigDecimal getLatitude() {
+		return latitude;
+	}
+
+	public String getFileUrl() {
+		return fileUrl;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public Integer getClockType() {
+		return clockType;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public String getRestTime() {
+		return restTime;
+	}
+
+	public Integer getDepartAttendRuleId() {
+		return departAttendRuleId;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setStaffNo(String staffNo) {
+		this.staffNo = staffNo;
+	}
+
+	public void setClockTime(Date clockTime) {
+		this.clockTime = clockTime;
+	}
+
+	public void setWorkBeginTime(String workBeginTime) {
+		this.workBeginTime = workBeginTime;
+	}
+
+	public void setWorkEndTime(String workEndTime) {
+		this.workEndTime = workEndTime;
+	}
+
+	public void setCloseBeginTime(String closeBeginTime) {
+		this.closeBeginTime = closeBeginTime;
+	}
+
+	public void setCloseEndTime(String closeEndTime) {
+		this.closeEndTime = closeEndTime;
+	}
+
+	public void setLongitude(BigDecimal longitude) {
+		this.longitude = longitude;
+	}
+
+	public void setLatitude(BigDecimal latitude) {
+		this.latitude = latitude;
+	}
+
+	public void setFileUrl(String fileUrl) {
+		this.fileUrl = fileUrl;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public void setClockType(Integer clockType) {
+		this.clockType = clockType;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public void setRestTime(String restTime) {
+		this.restTime = restTime;
+	}
+
+	public void setDepartAttendRuleId(Integer departAttendRuleId) {
+		this.departAttendRuleId = departAttendRuleId;
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"AppStaffClockLogPo [id=%s, staffNo=%s, clockTime=%s, workBeginTime=%s, workEndTime=%s, closeBeginTime=%s, closeEndTime=%s, longitude=%s, latitude=%s, fileUrl=%s, fileName=%s, clockType=%s, remark=%s, address=%s, creator=%s, createTime=%s, restTime=%s, departAttendRuleId=%s]",
+				id, staffNo, clockTime, workBeginTime, workEndTime, closeBeginTime, closeEndTime, longitude, latitude,
+				fileUrl, fileName, clockType, remark, address, creator, createTime, restTime, departAttendRuleId);
+	}
 
 }
