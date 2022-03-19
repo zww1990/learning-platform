f5395181c9c0c932b4bb28c2a95ba35dbe0c9297
zww1990 <zww1990@foxmail.com>
2022-03-19 17:37:13 +0800

    ExcelData.java

--- src/test/java/com/example/test/excel/ExcelData.java
+++ src/test/java/com/example/test/excel/ExcelData.java
@@ -2,22 +2,36 @@ package com.example.test.excel;
 
 import java.util.List;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.experimental.Accessors;
-
 /**
  * ExcelData
  * 
  * @author zww1990@foxmail.com
  * @since 2022年1月8日,下午4:51:46
  */
-@Getter
-@Setter
-@ToString
-@Accessors(chain = true)
 public class ExcelData {
 	private String cellValue;
 	private List<ExcelData> childrens;
+
+	public String getCellValue() {
+		return cellValue;
+	}
+
+	public List<ExcelData> getChildrens() {
+		return childrens;
+	}
+
+	public ExcelData setCellValue(String cellValue) {
+		this.cellValue = cellValue;
+		return this;
+	}
+
+	public ExcelData setChildrens(List<ExcelData> childrens) {
+		this.childrens = childrens;
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("ExcelData [cellValue=%s, childrens=%s]", cellValue, childrens);
+	}
 }
