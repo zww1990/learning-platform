aaffe3b76050bc8f1fbb29849b97aa17b6d05930
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2022-04-08 18:07:20 +0800

    index2.html index2.js

---
+++ src/main/resources/static/js/index2.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#app',
+    vuetify: new Vuetify({
+        theme: {
+            dark: true
+        },
+    }),
+    data: {
+    	tabs: null,
+    },
+    computed: {
+    },
+    mounted() {
+    	console.log('hello world');
+    },
+    methods: {
+    }
+});
