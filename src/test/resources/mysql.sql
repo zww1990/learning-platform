a6277bc239e2e02cca3162eaaa0d3ba52674e548
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2022-04-26 17:52:33 +0800

    Spring Reactive

--- src/test/resources/mysql.sql
+++ src/test/resources/mysql.sql
@@ -1,6 +1,6 @@
 create table client_user
 (
-    user_id         varchar(64) primary key  not null comment '用户唯一标示',
+    user_id         varchar(64) primary key  not null comment '用户唯一标识',
     username        varchar(64)              null comment '名称',
     phone_number    varchar(64)              null comment '手机号',
     gender          integer(1) default 0     null comment '0=未知,1=男,2=女'
