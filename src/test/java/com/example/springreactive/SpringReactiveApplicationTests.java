889f64e07284420c2b191eb40541b2074fedabc0
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2022-04-26 17:53:58 +0800

    Spring Reactive

--- src/test/java/com/example/springreactive/SpringReactiveApplicationTests.java
+++ src/test/java/com/example/springreactive/SpringReactiveApplicationTests.java
@@ -6,6 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.r2dbc.core.DatabaseClient;
 
+/**
+ * SpringReactiveApplicationTests
+ * 
+ * @author weiwei
+ * @version v1
+ * @since 2022年4月26日,下午5:53:42
+ */
 @SuppressWarnings("deprecation")
 @SpringBootTest
 public class SpringReactiveApplicationTests {
