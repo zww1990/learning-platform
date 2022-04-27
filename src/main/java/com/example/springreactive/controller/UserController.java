69c98ed4c8979bcbdcd13040e50373d99d97ec06
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2022-04-27 18:00:28 +0800

    CRUD

--- src/main/java/com/example/springreactive/controller/UserController.java
+++ src/main/java/com/example/springreactive/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,5 +47,10 @@ public class UserController {
 		return this.entityTemplate.delete(ClientUser.class)//
 				.matching(Query.query(Criteria.where("userId").is(userId)))//
 				.all();
+	}
+
+	@PutMapping("/update")
+	public Mono<ClientUser> updateClientUser(@RequestBody ClientUser user) {
+		return this.entityTemplate.update(user);
 	}
 }
