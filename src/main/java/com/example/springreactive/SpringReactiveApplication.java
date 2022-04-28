60f1faf017e004210d30c056879ed55574ddc21d
zhangweiwei <zww1990@foxmail.com>
2022-04-28 16:11:23 +0800

    Spring Reactive CRUD

--- src/main/java/com/example/springreactive/SpringReactiveApplication.java
+++ src/main/java/com/example/springreactive/SpringReactiveApplication.java
@@ -2,6 +2,8 @@ package com.example.springreactive;
 
 import java.util.Arrays;
 
+import javax.annotation.Resource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,6 +11,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.example.springreactive.controller.UserController;
+import com.example.springreactive.model.ClientUser;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -29,10 +34,23 @@ public class SpringReactiveApplication {
 //		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.err::println);
 	}
 
+	@Resource
+	private UserController userController;
+
 	@Bean
 	public RouterFunction<ServerResponse> routerFunction() {
 		return RouterFunctions.route()//
 				.GET("/", request -> ServerResponse.ok().bodyValue(Arrays.asList("你好，", "世界！")))//
+				.GET("/user/get", request -> ServerResponse.ok().body(userController.getClientUser(), ClientUser.class))
+				.POST("/user/add", request -> request.bodyToMono(ClientUser.class)//
+						.flatMap(i -> userController.addClientUser(i))//
+						.flatMap(p -> ServerResponse.ok().bodyValue(p)))
+				.PUT("/user/update", request -> request.bodyToMono(ClientUser.class)//
+						.flatMap(i -> userController.updateClientUser(i))//
+						.flatMap(p -> ServerResponse.ok().bodyValue(p)))
+				.DELETE("/user/del/{userId}",
+						request -> ServerResponse.ok()
+								.body(userController.delClientUser(request.pathVariable("userId")), Integer.class))
 				.build();
 	}
 }
--- src/main/java/com/example/springreactive/controller/UserController.java
+++ src/main/java/com/example/springreactive/controller/UserController.java
@@ -6,14 +6,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.data.relational.core.query.Criteria;
 import org.springframework.data.relational.core.query.Query;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
 import com.example.springreactive.model.ClientUser;
 
@@ -27,33 +20,28 @@ import reactor.core.publisher.Mono;
  * @version v1
  * @since 2022年4月26日,下午4:14:03
  */
-@RestController
-@RequestMapping("/user")
+@Service
 public class UserController {
 	@Resource
 	private R2dbcEntityTemplate entityTemplate;
 
-	@GetMapping("/get")
 	public Flux<ClientUser> getClientUser() {
 		return this.entityTemplate.select(ClientUser.class)//
 				.matching(Query.empty().sort(Sort.by("userId")))//
 				.all();
 	}
 
-	@PostMapping("/add")
-	public Mono<ClientUser> addClientUser(@RequestBody ClientUser user) {
+	public Mono<ClientUser> addClientUser(ClientUser user) {
 		return this.entityTemplate.insert(user);
 	}
 
-	@DeleteMapping("/del/{userId}")
-	public Mono<Integer> delClientUser(@PathVariable String userId) {
+	public Mono<Integer> delClientUser(String userId) {
 		return this.entityTemplate.delete(ClientUser.class)//
 				.matching(Query.query(Criteria.where("userId").is(userId)))//
 				.all();
 	}
 
-	@PutMapping("/update")
-	public Mono<ClientUser> updateClientUser(@RequestBody ClientUser user) {
+	public Mono<ClientUser> updateClientUser(ClientUser user) {
 		return this.entityTemplate.update(user);
 	}
 }
