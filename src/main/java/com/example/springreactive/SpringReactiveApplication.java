55a16492c2ac523901f64734c97b721ebd439993
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2022-04-28 15:08:43 +0800

    SpringReactiveApplication.java

--- src/main/java/com/example/springreactive/SpringReactiveApplication.java
+++ src/main/java/com/example/springreactive/SpringReactiveApplication.java
@@ -1,8 +1,14 @@
 package com.example.springreactive;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,6 +26,13 @@ public class SpringReactiveApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(SpringReactiveApplication.class, args);
 		log.info("工厂中定义的 bean 数量 = {}", context.getBeanDefinitionCount());
+//		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.err::println);
 	}
 
+	@Bean
+	public RouterFunction<ServerResponse> routerFunction() {
+		return RouterFunctions.route()//
+				.GET("/", request -> ServerResponse.ok().bodyValue(Arrays.asList("你好，", "世界！")))//
+				.build();
+	}
 }
