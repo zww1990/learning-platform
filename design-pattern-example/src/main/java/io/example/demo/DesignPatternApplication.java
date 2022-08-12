bc4eef052c69d8cc7a34bb501c340d833195e81e
zww1990 <zww1990@foxmail.com>
2022-08-12 21:02:32 +0800

    refactor

--- design-pattern-example/src/main/java/io/example/demo/DesignPatternApplication.java
+++ design-pattern-example/src/main/java/io/example/demo/DesignPatternApplication.java
@@ -2,6 +2,7 @@ package io.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 /**
  * DesignPatternApplication
@@ -10,7 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2022年8月12日,下午8:45:37
  */
 @SpringBootApplication
+@EnableRetry
 public class DesignPatternApplication {
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(DesignPatternApplication.class, args);
 	}
---
+++ design-pattern-example/src/main/java/io/example/demo/service/impl/RetryServiceImpl.java
@@ -0,0 +1,56 @@
+package io.example.demo.service.impl;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.stereotype.Service;
+
+import io.example.demo.service.RetryService;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * RetryServiceImpl
+ * 
+ * @author zhangweiwei
+ * @date 2021年6月17日,下午1:08:51
+ */
+@Service
+@Slf4j
+public class RetryServiceImpl implements RetryService {
+
+	@Override
+	@Retryable(value = Exception.class, maxAttempts = 3)
+	public void doRetry(AtomicInteger times) {
+		log.info("当前调用次数: {}", times.incrementAndGet());
+		throw new RuntimeException("调用方法发生异常(doRetry): " + times.get());
+	}
+
+	@Recover
+	public void doRecover(Exception ex) {
+		log.info(ex.getLocalizedMessage(), ex);
+	}
+
+	@Override
+	@Retryable(value = Exception.class, maxAttempts = 3, backoff = @Backoff(value = 3000))
+	public void doRetryForBackOff(AtomicInteger times) {
+		log.info("当前调用次数: {}", times.incrementAndGet());
+		throw new RuntimeException("调用方法发生异常(doRetryForBackOff): " + times.get());
+	}
+
+	@Override
+	public void doRetryTemplate(AtomicInteger times) {
+		RetryTemplate template = RetryTemplate.builder().maxAttempts(3).fixedBackoff(3000).retryOn(Exception.class)
+				.build();
+		template.execute(ctx -> {
+			log.info("当前调用次数: {}", times.incrementAndGet());
+			throw new RuntimeException("调用方法发生异常(doRetryTemplate): " + times.get());
+		}, ctx -> {
+			Throwable ex = ctx.getLastThrowable();
+			log.info(ex.getLocalizedMessage(), ex);
+			return null;
+		});
+	}
+}
