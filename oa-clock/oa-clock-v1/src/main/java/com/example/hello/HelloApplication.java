f5200449076f5c7108e0a0c11dc1e21bfca3c968
zww1990 <zww1990@foxmail.com>
2022-08-12 22:13:02 +0800

    HelloApplication.java logback.xml

--- oa-clock/oa-clock-v1/src/main/java/com/example/hello/HelloApplication.java
+++ oa-clock/oa-clock-v1/src/main/java/com/example/hello/HelloApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.hello.config.ApplicationProperties;
@@ -22,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 @SpringBootApplication
 @Slf4j
+@EnableScheduling
 public class HelloApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
