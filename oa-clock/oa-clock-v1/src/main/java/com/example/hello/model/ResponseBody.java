cfa3b0d4b17fd16196f314a2dff760061974dba8
zww1990 <zww1990@foxmail.com>
2022-08-13 20:58:44 +0800

    ResponseBody.java

--- oa-clock/oa-clock-v1/src/main/java/com/example/hello/model/ResponseBody.java
+++ oa-clock/oa-clock-v1/src/main/java/com/example/hello/model/ResponseBody.java
@@ -5,6 +5,13 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.Accessors;
 
+/**
+ * ResponseBody
+ * 
+ * @author zhang weiwei
+ * @since 2022年8月13日,下午8:58:30
+ * @param <T>
+ */
 @Getter
 @Setter
 @ToString
