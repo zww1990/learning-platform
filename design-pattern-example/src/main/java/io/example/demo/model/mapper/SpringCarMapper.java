d58970ccc4c31cb942080a55f5acf400d7cedd5e
zww1990 <zww1990@foxmail.com>
2022-09-20 10:39:41 +0800

    class rename

--- design-pattern-example/src/main/java/io/example/demo/model/mapper/SpringCarMapper.java
+++ design-pattern-example/src/main/java/io/example/demo/model/mapper/SpringCarMapper.java
@@ -8,14 +8,14 @@ import io.example.demo.model.Car;
 import io.example.demo.model.CarDto;
 
 /**
- * CarMapperOfSpring
+ * Spring Car Mapper
  * 
  * @author weiwei
  * @version v1
  * @since 2022年9月19日,下午4:21:32
  */
 @Mapper(componentModel = ComponentModel.SPRING)
-public interface CarMapperOfSpring {
+public interface SpringCarMapper {
 	@Mapping(source = "numberOfSeats", target = "seatCount")
 	CarDto carToCarDto(Car car);
 }
