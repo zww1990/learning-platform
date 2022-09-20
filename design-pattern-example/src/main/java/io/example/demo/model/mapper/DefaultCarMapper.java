60133dd8ef249ff04d2e2376a77b31b4c203bd47
zhangweiwei <zww1990@foxmail.com>
2022-09-20 10:36:04 +0800

    class rename

--- design-pattern-example/src/main/java/io/example/demo/model/mapper/DefaultCarMapper.java
+++ design-pattern-example/src/main/java/io/example/demo/model/mapper/DefaultCarMapper.java
@@ -2,21 +2,22 @@ package io.example.demo.model.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants.ComponentModel;
 import org.mapstruct.factory.Mappers;
 
 import io.example.demo.model.Car;
 import io.example.demo.model.CarDto;
 
 /**
- * CarMapper
+ * Default Car Mapper
  * 
  * @author weiwei
  * @version v1
  * @since 2022年9月19日,下午3:01:51
  */
-@Mapper
-public interface CarMapper {
-	CarMapper INSTANCE = Mappers.getMapper(CarMapper.class);
+@Mapper(componentModel = ComponentModel.DEFAULT)
+public interface DefaultCarMapper {
+	DefaultCarMapper INSTANCE = Mappers.getMapper(DefaultCarMapper.class);
 
 	@Mapping(source = "numberOfSeats", target = "seatCount")
 	CarDto carToCarDto(Car car);
