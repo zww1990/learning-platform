c09024aa04a8d6a2597864c91bb42954e1a7703f
zhangweiwei <zww1990@foxmail.com>
2022-09-27 17:03:52 +0800

    小练习：九九乘法表（for循环 + range函数）

--- pycharm-projects/python-project/multiplication_table.py
+++ pycharm-projects/python-project/multiplication_table.py
@@ -9,5 +9,12 @@ def use_while():
         i += 1
 
 
+def use_for():
+    for i in range(1, 10):
+        for j in range(1, i + 1):
+            print(f'{j} * {i} = {j * i}', end='\t')
+        print()
+
+
 if __name__ == '__main__':
-    use_while()
+    use_for()
