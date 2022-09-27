dcbb3c7165c97420668ae04adcce2fd06ea9d949
zhangweiwei <zww1990@foxmail.com>
2022-09-27 16:50:39 +0800

    小练习：九九乘法表

--- pycharm-projects/python-project/multiplication_table.py
+++ pycharm-projects/python-project/multiplication_table.py
@@ -3,7 +3,7 @@ def use_while():
     while i <= 9:
         j = 1
         while j <= i:
-            print(f'{j} * {i} = {j * i}\t', end='')
+            print(f'{j} * {i} = {j * i}', end='\t')
             j += 1
         print()
         i += 1
