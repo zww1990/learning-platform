a1b0977355c1b9cec426550ab6da448888ffba0c
zww1990 <zww1990@foxmail.com>
2022-10-23 18:59:53 +0800

    使用sys模块输出python版本

--- pycharm-projects/python-project/main.py
+++ pycharm-projects/python-project/main.py
@@ -3,6 +3,8 @@
 # 按 Shift+F10 执行或将其替换为您的代码。
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
 
+import sys
+
 
 def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
@@ -11,6 +13,7 @@ def print_hi(name):
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
+    print(sys.version)
     print_hi('PyCharm')
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
