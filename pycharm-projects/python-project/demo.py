588267ce19b535081fd2cff16b0ac43027e9dfeb
zww1990 <zww1990@foxmail.com>
2022-10-23 18:06:07 +0800

    递归调用

--- pycharm-projects/python-project/demo.py
+++ pycharm-projects/python-project/demo.py
@@ -1,5 +1,8 @@
 def say_hello(text):
     print(f'你输入的内容是: {text}')
+    answer = input('是否继续输入?(Y/N): ')
+    if answer == 'Y':
+        say_hello(input('请输入你的内容: '))
 
 
 if __name__ == '__main__':
