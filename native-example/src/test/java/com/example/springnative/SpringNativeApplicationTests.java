7646e3da8e7dd768d17b2dc01d8484ba43197c0f
zww1990 <zww1990@foxmail.com>
2022-11-25 13:48:49 +0800

    使用Spring Native构建Spring Boot项目

---
+++ native-example/src/test/java/com/example/springnative/SpringNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringNativeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
