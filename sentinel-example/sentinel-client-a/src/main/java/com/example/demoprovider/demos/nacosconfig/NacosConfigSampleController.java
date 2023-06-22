b531b033223c42c044977454e98fb04393158b82
zww1990 <zww1990@foxmail.com>
2023-06-22 17:51:41 +0800

    sentinel example

--- sentinel-example/sentinel-client-a/src/main/java/com/example/demoprovider/demos/nacosconfig/NacosConfigSampleController.java
+++ sentinel-example/sentinel-client-a/src/main/java/com/example/demoprovider/demos/nacosconfig/NacosConfigSampleController.java
@@ -5,6 +5,9 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+
 @RestController
 @RefreshScope
 public class NacosConfigSampleController {
@@ -13,8 +16,12 @@ public class NacosConfigSampleController {
 	private User user;
 
 	@GetMapping("/user")
+	@SentinelResource(value = "user", blockHandler = "handleException")
 	public String user() {
 		return "[HTTP] " + user;
 	}
 
+	public String handleException(BlockException be) {
+		return "我靠，我被限流了！";
+	}
 }
