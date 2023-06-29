61b614542c93c6f753c795543e861a79272e338b
zww1990 <zww1990@foxmail.com>
2023-06-29 22:29:44 +0800

    mysql.sql

--- shardingsphere-example/sharding-readwrite-splitting/src/test/resources/mysql.sql
+++ shardingsphere-example/sharding-readwrite-splitting/src/test/resources/mysql.sql
@@ -1,5 +1,16 @@
 create database sharding_config;
+
 create database sharding_write;
+
+CREATE TABLE t_user (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  nickname varchar(100) DEFAULT NULL,
+  password varchar(100) DEFAULT NULL,
+  sex int(11) DEFAULT NULL,
+  birthday date DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 create database sharding_read;
 
 CREATE TABLE t_user (
