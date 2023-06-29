7b4c3afdbf8c6786b08ce119b1420f2e5c81fe0e
zww1990 <zww1990@foxmail.com>
2023-06-29 19:45:35 +0800

    UserRepository.java

--- shardingsphere-example/sharding-readwrite-splitting/src/main/java/com/example/shardingreadwritesplitting/repository/UserRepository.java
+++ shardingsphere-example/sharding-readwrite-splitting/src/main/java/com/example/shardingreadwritesplitting/repository/UserRepository.java
@@ -11,9 +11,14 @@ import com.example.shardingreadwritesplitting.domain.User;
 @Mapper
 public interface UserRepository {
 
-	@Insert("insert into t_user(nickname, password, sex, birthday) values(#{nickname}, #{password}, #{sex}, #{birthday})")
+	@Insert("""
+			insert into t_user (nickname, password, sex, birthday)
+			 values (#{nickname}, #{password}, #{sex}, #{birthday})
+			""")
 	int insertUser(User user);
 
-	@Select("select * from t_user")
+	@Select("""
+			select * from t_user
+			""")
 	List<User> selectUsers();
 }
