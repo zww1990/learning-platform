faf9bcef4c3b428a4594bc60e7e47ec8f125f6ba
zww1990 <zww1990@foxmail.com>
2023-06-30 12:14:00 +0800

    ShardingSphere 使用示例代码生成器

---
+++ shardingsphere-example/sharding-example-generator/src/main/resources/manual_schema.sql
@@ -0,0 +1,45 @@
+DROP DATABASE IF EXISTS demo_ds;
+DROP DATABASE IF EXISTS demo_ds_0;
+DROP DATABASE IF EXISTS demo_ds_1;
+
+DROP DATABASE IF EXISTS demo_write_ds;
+DROP DATABASE IF EXISTS demo_read_ds_0;
+DROP DATABASE IF EXISTS demo_read_ds_1;
+
+DROP DATABASE IF EXISTS demo_write_ds_0;
+DROP DATABASE IF EXISTS demo_write_ds_0_read_0;
+DROP DATABASE IF EXISTS demo_write_ds_0_read_1;
+DROP DATABASE IF EXISTS demo_write_ds_1;
+DROP DATABASE IF EXISTS demo_write_ds_1_read_0;
+DROP DATABASE IF EXISTS demo_write_ds_1_read_1;
+
+DROP DATABASE IF EXISTS shadow_demo_ds;
+DROP DATABASE IF EXISTS shadow_demo_ds_0;
+DROP DATABASE IF EXISTS shadow_demo_ds_1;
+
+DROP DATABASE IF EXISTS demo_shadow_write_ds;
+DROP DATABASE IF EXISTS demo_shadow_read_ds;
+DROP DATABASE IF EXISTS demo_read_ds;
+
+CREATE DATABASE demo_ds;
+CREATE DATABASE demo_ds_0;
+CREATE DATABASE demo_ds_1;
+
+CREATE DATABASE demo_write_ds;
+CREATE DATABASE demo_read_ds_0;
+CREATE DATABASE demo_read_ds_1;
+
+CREATE DATABASE demo_write_ds_0;
+CREATE DATABASE demo_write_ds_0_read_0;
+CREATE DATABASE demo_write_ds_0_read_1;
+CREATE DATABASE demo_write_ds_1;
+CREATE DATABASE demo_write_ds_1_read_0;
+CREATE DATABASE demo_write_ds_1_read_1;
+
+CREATE DATABASE shadow_demo_ds;
+CREATE DATABASE shadow_demo_ds_0;
+CREATE DATABASE shadow_demo_ds_1;
+
+CREATE DATABASE demo_shadow_write_ds;
+CREATE DATABASE demo_shadow_read_ds;
+CREATE DATABASE demo_read_ds;
