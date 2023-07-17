f42d2745870be247f8e69e81630917d1e60a1439
zww1990 <zww1990@foxmail.com>
2023-07-17 22:24:23 +0800

    Oauth2 Authorization Server

---
+++ security-example/oauth2-parent/oauth2-authorization-server/src/main/java/com/example/oauth2authorizationserver/DemoAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAuthorizationServerApplication.class, args);
+	}
+
+}
