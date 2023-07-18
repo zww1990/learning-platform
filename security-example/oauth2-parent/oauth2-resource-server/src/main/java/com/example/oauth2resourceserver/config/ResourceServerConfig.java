d1dc04d1c4bc4685fb19e3b0280d6dff44d47c07
zww1990 <zww1990@foxmail.com>
2023-07-18 16:48:54 +0800

    Enables Jwt-encoded bearer token support.

--- security-example/oauth2-parent/oauth2-resource-server/src/main/java/com/example/oauth2resourceserver/config/ResourceServerConfig.java
+++ security-example/oauth2-parent/oauth2-resource-server/src/main/java/com/example/oauth2resourceserver/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.example.oauth2resourceserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,11 +11,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration(proxyBeanMethods = false)
 public class ResourceServerConfig {
 
-	@SuppressWarnings("removal")
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.securityMatcher("/messages/**").authorizeHttpRequests().requestMatchers("/messages/**")
-				.hasAuthority("SCOPE_message.read").and().oauth2ResourceServer(server -> server.jwt());
+		http.securityMatcher("/messages/**")
+				.authorizeHttpRequests(
+						authorize -> authorize.requestMatchers("/messages/**").hasAuthority("SCOPE_message.read"))
+				.oauth2ResourceServer(server -> server.jwt(Customizer.withDefaults()));
 		return http.build();
 	}
 
