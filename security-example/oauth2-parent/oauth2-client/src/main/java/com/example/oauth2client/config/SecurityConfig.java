749a4507365c6ee39326a79639507d1816b51030
zww1990 <zww1990@foxmail.com>
2023-07-18 16:35:40 +0800

    use permitAll via HttpSecurity#authorizeHttpRequests

--- security-example/oauth2-parent/oauth2-client/src/main/java/com/example/oauth2client/config/SecurityConfig.java
+++ security-example/oauth2-parent/oauth2-client/src/main/java/com/example/oauth2client/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,15 +16,10 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
 public class SecurityConfig {
 
 	@Bean
-	WebSecurityCustomizer webSecurityCustomizer() {
-		return (web) -> web.ignoring().requestMatchers("/webjars/**", "/assets/**");
-	}
-
-	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http,
 			ClientRegistrationRepository clientRegistrationRepository) throws Exception {
-		http.authorizeHttpRequests(
-				authorize -> authorize.requestMatchers("/logged-out").permitAll().anyRequest().authenticated())//
+		http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/webjars/**", "/assets/**", "/logged-out")
+				.permitAll().anyRequest().authenticated())//
 				.oauth2Login(oauth2Login -> oauth2Login.loginPage("/oauth2/authorization/messaging-client-oidc"))//
 				.oauth2Client(withDefaults())//
 				.logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository)));
