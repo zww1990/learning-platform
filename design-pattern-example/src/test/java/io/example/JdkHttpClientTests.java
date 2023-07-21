99804d0e4b02b42490140ed9ae20878c7aff3653
zww1990 <zww1990@foxmail.com>
2023-07-21 12:46:14 +0800

    jdk提供的httpclient

---
+++ design-pattern-example/src/test/java/io/example/JdkHttpClientTests.java
@@ -0,0 +1,15 @@
+package io.example;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class JdkHttpClientTests {
+    public static void main(String[] args) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("https://www.baidu.com")).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.err.println(response.body());
+    }
+}
