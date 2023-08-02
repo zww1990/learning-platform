f5fcea51834f5c745f2f66fa11a152bc8a5e6530
zww1990 <zww1990@foxmail.com>
2023-08-02 13:44:44 +0800

    Enables the use of the Apache HTTP Client 5 by Feign.

--- spring-cloud-example/spring-netflix-example/eureka-example/eureka-consumer/src/test/java/com/example/eurekaconsumer/EurekaConsumerApplicationTests.java
+++ spring-cloud-example/spring-netflix-example/eureka-example/eureka-consumer/src/test/java/com/example/eurekaconsumer/EurekaConsumerApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.eurekaconsumer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.loadbalancer.FeignBlockingLoadBalancerClient;
+import org.springframework.context.ApplicationContext;
+
+import feign.Client;
 
 @SpringBootTest
 public class EurekaConsumerApplicationTests {
+	@Autowired
+	private ApplicationContext context;
 
 	@Test
 	public void testContextLoads() {
+		FeignBlockingLoadBalancerClient client = (FeignBlockingLoadBalancerClient) this.context.getBean(Client.class);
+		System.err.println(client.getDelegate().getClass());
 	}
 
 }
