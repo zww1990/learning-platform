5b22dbd14cdec159337aab132a7b467a3be8da32
zww1990 <zww1990@foxmail.com>
2023-08-02 18:53:07 +0800

    spring tencent cloud consumer example

--- polaris-example/polaris-consumer/src/test/java/com/example/democonsumer/controller/ConfigControllerTests.java
+++ polaris-example/polaris-consumer/src/test/java/com/example/democonsumer/controller/ConfigControllerTests.java
@@ -2,13 +2,18 @@ package com.example.democonsumer.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.loadbalancer.FeignBlockingLoadBalancerClient;
+import org.springframework.context.ApplicationContext;
 
+import feign.Client;
 import jakarta.annotation.Resource;
 
 @SpringBootTest
 public class ConfigControllerTests {
 	@Resource
 	private ConfigController controller;
+	@Resource
+	private ApplicationContext context;
 
 	@Test
 	public void testStudent() {
@@ -17,6 +22,12 @@ public class ConfigControllerTests {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Test
+	public void testContextLoads() {
+		FeignBlockingLoadBalancerClient client = (FeignBlockingLoadBalancerClient) this.context.getBean(Client.class);
+		System.err.println(client.getDelegate().getClass());
 	}
 
 }
