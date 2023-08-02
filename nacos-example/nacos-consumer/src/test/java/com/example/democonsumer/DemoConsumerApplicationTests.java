a2bb93070be73c7b750371c67b5be26fa0e90bd4
zww1990 <zww1990@foxmail.com>
2023-08-02 16:34:59 +0800

    Enables the use of the Apache HTTP Client 5 by Feign.

--- nacos-example/nacos-consumer/src/test/java/com/example/democonsumer/DemoConsumerApplicationTests.java
+++ nacos-example/nacos-consumer/src/test/java/com/example/democonsumer/DemoConsumerApplicationTests.java
@@ -3,7 +3,10 @@ package com.example.democonsumer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.loadbalancer.FeignBlockingLoadBalancerClient;
 import org.springframework.context.ApplicationContext;
+
+import feign.Client;
 
 @SpringBootTest
 public class DemoConsumerApplicationTests {
@@ -11,8 +14,9 @@ public class DemoConsumerApplicationTests {
 	private ApplicationContext context;
 
 	@Test
-	public void contextLoads() {
-		System.err.println(this.context.getBeanDefinitionCount());
+	public void testContextLoads() {
+		FeignBlockingLoadBalancerClient client = (FeignBlockingLoadBalancerClient) this.context.getBean(Client.class);
+		System.err.println(client.getDelegate().getClass());
 	}
 
 }
