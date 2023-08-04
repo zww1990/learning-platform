8a22bda4b4031a42b13b5c3dead5106733a4e99f
zww1990 <zww1990@foxmail.com>
2023-08-04 10:18:30 +0800

    spring eureka consumer example

--- spring-cloud-example/spring-netflix-example/eureka-example/eureka-consumer/src/test/java/com/example/eurekaconsumer/EurekaConsumerApplicationTests.java
+++ spring-cloud-example/spring-netflix-example/eureka-example/eureka-consumer/src/test/java/com/example/eurekaconsumer/EurekaConsumerApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.eurekaconsumer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.openfeign.loadbalancer.FeignBlockingLoadBalancerClient;
 import org.springframework.context.ApplicationContext;
 
@@ -17,6 +19,10 @@ public class EurekaConsumerApplicationTests {
 	public void testContextLoads() {
 		FeignBlockingLoadBalancerClient client = (FeignBlockingLoadBalancerClient) this.context.getBean(Client.class);
 		System.err.println(client.getDelegate().getClass());
+		DiscoveryClient discoveryClient = this.context.getBean(DiscoveryClient.class);
+		System.err.println(discoveryClient);
+		LoadBalancerClient loadBalancerClient = this.context.getBean(LoadBalancerClient.class);
+		System.err.println(loadBalancerClient);
 	}
 
 }
