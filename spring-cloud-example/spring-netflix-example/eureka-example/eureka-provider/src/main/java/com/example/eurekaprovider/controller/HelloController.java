9ca0a3b7f5c66dc0f83f99659ad35404a78671b5
zww1990 <zww1990@foxmail.com>
2023-08-04 10:18:19 +0800

    spring eureka provider example

--- spring-cloud-example/spring-netflix-example/eureka-example/eureka-provider/src/main/java/com/example/eurekaprovider/controller/HelloController.java
+++ spring-cloud-example/spring-netflix-example/eureka-example/eureka-provider/src/main/java/com/example/eurekaprovider/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.example.eurekaprovider.controller;
 
 import java.time.LocalDate;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,11 +23,14 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/hello")
 @Slf4j
 public class HelloController {
+	@Autowired
+	private ServerProperties server;
+
 	@GetMapping("/get")
 	public Hello get(@RequestParam String name) {
-		log.info("提供者：get(): name = {}", name);
+		log.info("提供者{}：get(): name = {}", this.server.getPort(), name);
 		return new Hello()//
-				.setId(System.currentTimeMillis())//
+				.setId(this.server.getPort())//
 				.setAge(18)//
 				.setBirthday(LocalDate.now().minusYears(18))//
 				.setName(name);
