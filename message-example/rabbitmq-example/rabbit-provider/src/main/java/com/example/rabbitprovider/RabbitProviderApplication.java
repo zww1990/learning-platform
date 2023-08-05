6295a9d521cebf08e0090b23d631a2887cc654a2
zww1990 <zww1990@foxmail.com>
2023-08-05 21:36:14 +0800

    spring rabbitmq provider example

--- message-example/rabbitmq-example/rabbit-provider/src/main/java/com/example/rabbitprovider/RabbitProviderApplication.java
+++ message-example/rabbitmq-example/rabbit-provider/src/main/java/com/example/rabbitprovider/RabbitProviderApplication.java
@@ -2,7 +2,9 @@ package com.example.rabbitprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitTemplateCustomizer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,4 +24,32 @@ public class RabbitProviderApplication {
 //		java.util.stream.Stream.of(context.getBeanDefinitionNames()).forEach(System.err::println);
 	}
 
+	/**
+	 * 可用于自定义RabbitTemplate的回调接口。
+	 * 
+	 * @author zhang weiwei
+	 * @since 2023年8月5日,下午9:24:03
+	 * @return
+	 */
+	@Bean
+	RabbitTemplateCustomizer rabbitTemplateCustomizer() {
+		return rabbitTemplate -> {
+			//在发送消息时设置强制性标志；仅在提供了returnCallback的情况下应用。
+			rabbitTemplate.setMandatory(true);
+			// 设置一个回调来接收返回的消息。
+			rabbitTemplate.setReturnsCallback(returned -> {
+				Object message = rabbitTemplate.getMessageConverter().fromMessage(returned.getMessage());
+				log.warn("返回的消息和元数据>> {}", returned);
+				log.warn("将消息转换为Java对象>> {}", message);
+			});
+			// 设置一个发布者确认的回调。
+			rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+				if (ack) {
+					log.info("{}", correlationData);
+				} else {
+					log.error("{} - {}", correlationData, cause);
+				}
+			});
+		};
+	}
 }
--- message-example/rabbitmq-example/rabbit-provider/src/test/java/com/example/rabbitprovider/RabbitProviderApplicationTests.java
+++ message-example/rabbitmq-example/rabbit-provider/src/test/java/com/example/rabbitprovider/RabbitProviderApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example.rabbitprovider;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.amqp.RabbitTemplateCustomizer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+
+import com.example.provider.api.domain.Hello;
 
 @SpringBootTest
 public class RabbitProviderApplicationTests {
@@ -15,9 +18,42 @@ public class RabbitProviderApplicationTests {
 	@Test
 	public void testContextLoads() {
 		RabbitTemplate template = this.context.getBean(RabbitTemplate.class);
-		System.err.println(template.getMessageConverter().getClass());
-		RabbitAdmin admin = this.context.getBean(RabbitAdmin.class);
-		System.err.println(admin.isAutoStartup());
+//		template.setConfirmCallback((correlationData, ack, cause) -> {
+//			System.err.println("重新设置一个confirm回调");
+//		});
+//		template.setReturnsCallback(returned -> {
+//			System.err.println("重新设置一个returns回调");
+//		});
+		System.err.println(template.getMessageConverter());
+		RabbitTemplateCustomizer customizer = this.context.getBean(RabbitTemplateCustomizer.class);
+		System.err.println(customizer);
 	}
 
+	@Test
+	public void testSendMessage() {
+		RabbitTemplate template = this.context.getBean(RabbitTemplate.class);
+		template.convertAndSend("ex.hello", "rk.hello1", new Hello());
+		System.err.println("done");
+	}
+
+	@Test
+	public void testSendMessage2() {
+		RabbitTemplate template = this.context.getBean(RabbitTemplate.class);
+		template.convertAndSend("ex.hello1", "rk.hello", new Hello(), new CorrelationData());
+		System.err.println("done");
+	}
+
+	@Test
+	public void testSendMessage3() {
+		RabbitTemplate template = this.context.getBean(RabbitTemplate.class);
+		template.convertAndSend("ex.hello", "rk.hello1", new Hello(), new CorrelationData());
+		System.err.println("done");
+	}
+
+	@Test
+	public void testSendMessage4() {
+		RabbitTemplate template = this.context.getBean(RabbitTemplate.class);
+		template.convertAndSend("ex.hello", "rk.hello", new Hello(), new CorrelationData());
+		System.err.println("done");
+	}
 }
