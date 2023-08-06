de055cd657ff11395b601ee5e71a4d01b607566b
zww1990 <zww1990@foxmail.com>
2023-08-06 10:34:09 +0800

    spring poi example

---
+++ spring-example/poi-example/src/main/java/io/example/demo/PoiExampleApplication.java
@@ -0,0 +1,24 @@
+package io.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * PoiExampleApplication
+ * 
+ * @author zhang weiwei
+ * @since 2022年8月12日,下午8:45:37
+ */
+@SpringBootApplication
+@Slf4j
+public class PoiExampleApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(PoiExampleApplication.class, args);
+		log.info("当前容器中的bean总数={}", context.getBeanDefinitionCount());
+//		java.util.Arrays.stream(context.getBeanDefinitionNames()).forEach(System.err::println);
+	}
+}
