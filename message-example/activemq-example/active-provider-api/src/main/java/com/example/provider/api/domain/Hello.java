0f0d1fc1dedf6eb73e7252aaa48b8471832ad798
zww1990 <zww1990@foxmail.com>
2023-08-07 18:40:44 +0800

    spring activemq provider example

---
+++ message-example/activemq-example/active-provider-api/src/main/java/com/example/provider/api/domain/Hello.java
@@ -0,0 +1,38 @@
+package com.example.provider.api.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+/**
+ * 示例模型
+ * 
+ * @author zhang weiwei
+ * @since 2023年8月2日,上午11:30:36
+ */
+@SuppressWarnings("serial")
+@Getter
+@Setter
+@ToString
+@Accessors(chain = true)
+public class Hello implements Serializable {
+	/** 编号 */
+	private long id;
+	/** 姓名 */
+	private String name;
+	/** 年龄 */
+	private int age;
+	/** 生日 */
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate birthday;
+	/** 发生时间 */
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+	private LocalDateTime sendtime;
+}
