75ad7ab68a76ce33fe4ac18358675fcf17342df9
zww1990 <zww1990@foxmail.com>
2023-08-08 16:26:56 +0800

    spring druid example

--- spring-example/druid-example/src/main/java/io/example/druid/DruidExampleApplication.java
+++ spring-example/druid-example/src/main/java/io/example/druid/DruidExampleApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Import;
 
 /**
  * Druid Example Application
+ *
  * @author zww
  * @since 2023-08-06 10:57:57
  */
@@ -17,10 +18,10 @@ import org.springframework.context.annotation.Import;
 @Slf4j
 public class DruidExampleApplication {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(DruidExampleApplication.class, args);
-		log.info("Get Bean Definition Count = {}",context.getBeanDefinitionCount());
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(DruidExampleApplication.class, args);
+        log.info("Get Bean Definition Count = {}", context.getBeanDefinitionCount());
 //		java.util.stream.Stream.of(context.getBeanDefinitionNames()).forEach(System.err::println);
-	}
+    }
 
 }
