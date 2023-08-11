eebec8e0ab9579435db269a31cfd6c38247c79de
zww1990 <zww1990@foxmail.com>
2023-08-11 10:40:57 +0800

    spring reactive example

--- spring-example/reactive-example/src/main/java/io/example/reactive/ReactiveExampleApplication.java
+++ spring-example/reactive-example/src/main/java/io/example/reactive/ReactiveExampleApplication.java
@@ -34,7 +34,7 @@ public class ReactiveExampleApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(ReactiveExampleApplication.class, args);
 		log.info("工厂中定义的 bean 数量 = {}", context.getBeanDefinitionCount());
-		java.util.stream.Stream.of(context.getBeanDefinitionNames()).forEach(System.err::println);
+//		java.util.stream.Stream.of(context.getBeanDefinitionNames()).forEach(System.err::println);
 	}
 
 	/**
