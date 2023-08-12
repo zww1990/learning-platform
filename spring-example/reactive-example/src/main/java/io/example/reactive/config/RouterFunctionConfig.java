ff7f02a001b37f49b6e902c3724a4bd4d6c828f3
zww1990 <zww1990@foxmail.com>
2023-08-12 20:58:47 +0800

    spring reactive example

--- spring-example/reactive-example/src/main/java/io/example/reactive/config/RouterFunctionConfig.java
+++ spring-example/reactive-example/src/main/java/io/example/reactive/config/RouterFunctionConfig.java
@@ -29,7 +29,7 @@ public class RouterFunctionConfig {
 
     @Bean
     RouterFunction<ServerResponse> routerFunction() {
-        RouterFunction<ServerResponse> a = RouterFunctions.route()
+        RouterFunction<ServerResponse> rf1 = RouterFunctions.route()
                 .path("/user", builder -> builder
                         .GET("/get", request -> ServerResponse.ok().body(userController.getClientUser(), ClientUser.class))
                         .POST("/add", request -> request.bodyToMono(ClientUser.class).flatMap(userController::addClientUser).flatMap(p -> ServerResponse.ok().bodyValue(p)))
@@ -37,7 +37,7 @@ public class RouterFunctionConfig {
                         .DELETE("/del/{userId}", request -> ServerResponse.ok().body(userController.delClientUser(request.pathVariable("userId")), Long.class))
                         .GET("/get/{userId}", request -> ServerResponse.ok().body(userController.getClientUserByUserId(request.pathVariable("userId")), ClientUser.class))
                 ).build();
-        RouterFunction<ServerResponse> b = RouterFunctions.route()
+        RouterFunction<ServerResponse> rf2 = RouterFunctions.route()
                 .path("/v2/user", builder -> builder
                         .GET("/get", request -> ServerResponse.ok().body(userControllerV2.getClientUser(), ClientUser.class))
                         .POST("/add", request -> request.bodyToMono(ClientUser.class).flatMap(userControllerV2::addClientUser).flatMap(p -> ServerResponse.ok().bodyValue(p)))
@@ -47,6 +47,6 @@ public class RouterFunctionConfig {
                 ).build();
         return RouterFunctions.route()
                 .GET("/", request -> ServerResponse.ok().bodyValue(List.of("你好，", "世界！")))
-                .add(a).add(b).build();
+                .add(rf1).add(rf2).build();
     }
 }
