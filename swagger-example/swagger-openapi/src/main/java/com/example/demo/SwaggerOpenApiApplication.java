b8f8e46a524e529d777df24643a990794148c8c7
zww1990 <zww1990@foxmail.com>
2023-08-16 16:43:42 +0800

    renamed

---
+++ swagger-example/swagger-openapi/src/main/java/com/example/demo/SwaggerOpenApiApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@SpringBootApplication
+@Slf4j
+@AllArgsConstructor
+public class SwaggerOpenApiApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(SwaggerOpenApiApplication.class, args);
+        log.info("应用程序上下文Bean定义计数={}", context.getBeanDefinitionCount());
+//		java.util.Arrays.stream(context.getBeanDefinitionNames()).forEach(System.err::println);
+    }
+
+    private List<HttpMessageConverter<?>> converters;
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.converters.stream().filter(p -> p instanceof AbstractHttpMessageConverter)
+                .forEach(c -> ((AbstractHttpMessageConverter<?>) c).setDefaultCharset(StandardCharsets.UTF_8));
+    }
+}
