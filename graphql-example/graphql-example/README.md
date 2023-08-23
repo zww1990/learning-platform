b27be8a63359b131913296334ea6e171c266c41e
zww1990 <zww1990@foxmail.com>
2023-08-23 11:03:49 +0800

    spring graphiql example

--- graphql-example/graphql-example/README.md
+++ graphql-example/graphql-example/README.md
@@ -4,12 +4,34 @@
 使用curl发送请求：
 
 ```
-curl -X POST http://localhost:8081/graphql --form "operations={ \"query\": \"mutation FileUpload($file: Upload!) {fileUpload(file: $file)}\" , \"variables\": {\"file\": null}}" --form "0=@E:/微信头像.jpg" --form "map={\"0\": [\"variables.file\"]}"
+curl -X POST http://localhost:8081/graphql --form "operations={ \"query\": \"mutation ($file: Upload!) { fileUpload(file: $file) { id } }\" , \"variables\": {\"file\": null}}" --form "0=@E:/微信头像.jpg" --form "map={\"0\": [\"variables.file\"]}"
+```
+
+使用 Altair GraphQL 浏览器插件进行测试，访问地址（http://localhost:8081/altair/index.html）
+查询代码如下：
+
+```
+mutation ($file: Upload!) {
+  fileUpload(file: $file) {
+    id
+  }
+}
 ```
 
 ### 多文件上传
 使用curl发送请求：
 
 ```
-curl -X POST http://localhost:8081/graphql --form "operations={ \"query\": \"mutation FilesUploads($files: [Upload!]!) {multiFileUpload(files: $files)}\" , \"variables\": {\"files\": [null, null]}}" --form "0=@E:/微信头像.jpg" --form "1=@E:/微信头像2.jpg" --form "map={\"0\": [\"variables.files.0\"], \"1\": [\"variables.files.1\"]}"
+curl -X POST http://localhost:8081/graphql --form "operations={ \"query\": \"mutation ($files: [Upload!]!) { multiFileUpload(files: $files) { id } }\" , \"variables\": {\"files\": [null, null]}}" --form "0=@E:/微信头像.jpg" --form "1=@E:/微信头像2.jpg" --form "map={\"0\": [\"variables.files.0\"], \"1\": [\"variables.files.1\"]}"
+```
+
+使用 Altair GraphQL 浏览器插件进行测试，访问地址（http://localhost:8081/altair/index.html）
+查询代码如下：
+
+```
+mutation ($files: [Upload!]!) {
+  multiFileUpload(files: $files) {
+    id
+  }
+}
 ```
--- graphql-example/graphql-example/src/main/java/io/example/graphql/controller/FileController.java
+++ graphql-example/graphql-example/src/main/java/io/example/graphql/controller/FileController.java
@@ -1,5 +1,6 @@
 package io.example.graphql.controller;
 
+import io.example.graphql.domain.FileInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
@@ -7,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * File Controller
@@ -19,30 +22,40 @@ import java.util.List;
 @Slf4j
 public class FileController {
     @MutationMapping("fileUpload")
-    public Boolean fileUpload(@Argument MultipartFile file) {
+    public FileInfo fileUpload(@Argument MultipartFile file) {
         if (file == null) {
-            return false;
+            return new FileInfo();
         }
         String contentType = file.getContentType();
         String name = file.getName();
         String originalFilename = file.getOriginalFilename();
         long size = file.getSize();
         log.info("contentType = {}, name = {}, originalFilename = {}, size = {}", contentType, name, originalFilename, size);
-        return true;
+        return new FileInfo()
+                .setId(UUID.randomUUID().toString())
+                .setName(name)
+                .setSize(size)
+                .setContentType(contentType)
+                .setOriginalFilename(originalFilename);
     }
 
     @MutationMapping("multiFileUpload")
-    public Boolean multiFileUpload(@Argument List<MultipartFile> files) {
+    public List<FileInfo> multiFileUpload(@Argument List<MultipartFile> files) {
         if (CollectionUtils.isEmpty(files)) {
-            return false;
+            return Collections.emptyList();
         }
-        files.forEach(file -> {
+        return files.stream().map(file -> {
             String contentType = file.getContentType();
             String name = file.getName();
             String originalFilename = file.getOriginalFilename();
             long size = file.getSize();
             log.info("contentType = {}, name = {}, originalFilename = {}, size = {}", contentType, name, originalFilename, size);
-        });
-        return true;
+            return new FileInfo()
+                    .setId(UUID.randomUUID().toString())
+                    .setName(name)
+                    .setSize(size)
+                    .setContentType(contentType)
+                    .setOriginalFilename(originalFilename);
+        }).toList();
     }
 }
