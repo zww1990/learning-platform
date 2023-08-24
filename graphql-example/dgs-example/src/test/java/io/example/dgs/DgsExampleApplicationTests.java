b342edaacd5a3474f713af3149248170dc448a74
zww1990 <zww1990@foxmail.com>
2023-08-24 10:15:40 +0800

    spring dgs example

--- graphql-example/dgs-example/src/test/java/io/example/dgs/DgsExampleApplicationTests.java
+++ graphql-example/dgs-example/src/test/java/io/example/dgs/DgsExampleApplicationTests.java
@@ -1,13 +1,25 @@
 package io.example.dgs;
 
+import com.netflix.graphql.dgs.mvc.DgsGraphQLRequestHeaderValidator;
+import com.netflix.graphql.dgs.mvc.GraphQLRequestHeaderValidationRule;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Map;
 
 @SpringBootTest
 public class DgsExampleApplicationTests {
+    @Autowired
+    private ApplicationContext applicationContext;
 
-	@Test
-	public void testContextLoads() {
-	}
+    @Test
+    public void testContextLoads() {
+        DgsGraphQLRequestHeaderValidator validator = this.applicationContext.getBean(DgsGraphQLRequestHeaderValidator.class);
+        Map<String, GraphQLRequestHeaderValidationRule> ruleMap = this.applicationContext.getBeansOfType(GraphQLRequestHeaderValidationRule.class);
+        System.err.println(validator);
+        System.err.println(ruleMap);
+    }
 
 }
