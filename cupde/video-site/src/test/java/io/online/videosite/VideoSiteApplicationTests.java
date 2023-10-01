dda4e54920e35a60ce2d02d38d0bd851151cf1e0
zww1990 <zww1990@foxmail.com>
2023-10-01 18:40:30 +0800

    初始化工程

---
+++ cupde/video-site/src/test/java/io/online/videosite/VideoSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.online.videosite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class VideoSiteApplicationTests {
+
+	@Test
+	public void testContextLoads() {
+	}
+
+}
