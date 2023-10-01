02303aaecfab8edf705f0414d3b3d3c0f7df7b05
zww1990 <zww1990@foxmail.com>
2023-10-01 19:29:23 +0800

    添加各个环境配置文件

--- cupde/video-site/src/main/java/io/online/videosite/VideoSiteApplication.java
+++ cupde/video-site/src/main/java/io/online/videosite/VideoSiteApplication.java
@@ -1,13 +1,30 @@
 package io.online.videosite;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.stream.Stream;
+
+/**
+ * 视频网站应用程序启动类
+ *
+ * @author 张维维
+ * @since 2023-10-01 18:48:38
+ */
 @SpringBootApplication
+@Slf4j
 public class VideoSiteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VideoSiteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(VideoSiteApplication.class, args);
+        log.info("Get Bean Definition Count = {}", context.getBeanDefinitionCount());
+//        printBeans(context);
+    }
 
+    static void printBeans(ApplicationContext context) {
+        Stream.of(context.getBeanDefinitionNames()).forEach(System.err::println);
+    }
 }
--- cupde/video-site/src/test/java/io/online/videosite/VideoSiteApplicationTests.java
+++ cupde/video-site/src/test/java/io/online/videosite/VideoSiteApplicationTests.java
@@ -1,13 +1,22 @@
 package io.online.videosite;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import java.util.stream.Stream;
 
 @SpringBootTest
 public class VideoSiteApplicationTests {
+	@Autowired
+	private ApplicationContext applicationContext;
 
 	@Test
 	public void testContextLoads() {
+		System.err.println(this.applicationContext.getBeanDefinitionCount());
+		Stream.of(this.applicationContext.getBeanDefinitionNames())
+				.forEach(System.err::println);
 	}
 
 }
