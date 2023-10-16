66baf97787124328ce71e4a303721f74c920bbaa
zww1990 <zww1990@foxmail.com>
2023-10-16 17:13:56 +0800

    初始化工程

---
+++ spring-vue-example/vuetify-example/src/test/java/io/example/vuetify/VuetifyExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.example.vuetify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class VuetifyExampleApplicationTests {
+
+	@Test
+	public void testContextLoads() {
+	}
+
+}
