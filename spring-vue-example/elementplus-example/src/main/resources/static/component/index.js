44f3fa737b56c043057814ee9d75ea1c7e31954f
zww1990 <zww1990@foxmail.com>
2023-10-19 10:47:37 +0800

    element-plus example

--- spring-vue-example/elementplus-example/src/main/resources/static/component/index.js
+++ spring-vue-example/elementplus-example/src/main/resources/static/component/index.js
@@ -1,6 +1,10 @@
 import MyComponent from './my-component.js'
 
 const { createApp } = Vue
-
 const app = createApp(MyComponent)
-app.use(ElementPlus).mount('#app')
+
+app.use(ElementPlus, { locale: ElementPlusLocaleZhCn })
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.mount('#app')
--- spring-vue-example/elementplus-example/src/main/resources/static/component/my-component.js
+++ spring-vue-example/elementplus-example/src/main/resources/static/component/my-component.js
@@ -1,9 +1,17 @@
 const { ref } = Vue
 export default {
   setup() {
-    return {  }
+    const value1 = ref('')
+    return { value1 }
   },
   template: `
     <el-button>我是一个按钮</el-button>
+    <div style="font-size: 20px">
+        <Edit style="width: 1em; height: 1em; margin-right: 8px" />
+        <Share style="width: 1em; height: 1em; margin-right: 8px" />
+        <Delete style="width: 1em; height: 1em; margin-right: 8px" />
+        <Search style="width: 1em; height: 1em; margin-right: 8px" />
+    </div>
+    <el-date-picker v-model="value1" type="datetime" placeholder="请选择日期和时间"/>
   `
 }
