b545eb4a0434b195fbbea20974222d5cc34555dc
zww1990 <zww1990@foxmail.com>
2023-10-19 16:41:38 +0800

    vuetify example

--- spring-vue-example/vuetify-example/src/main/resources/static/component/index.js
+++ spring-vue-example/vuetify-example/src/main/resources/static/component/index.js
@@ -3,7 +3,14 @@ import MyComponent from './my-component.js'
 const { createApp } = Vue
 const { createVuetify } = Vuetify
 
-const vuetify = createVuetify()
+const vuetify = createVuetify({
+    locale: {
+        locale: 'zhHans'
+    },
+    theme: {
+        defaultTheme: 'light'
+    }
+})
 
 const app = createApp(MyComponent)
 app.use(vuetify).mount('#app')
--- spring-vue-example/vuetify-example/src/main/resources/static/component/my-component.js
+++ spring-vue-example/vuetify-example/src/main/resources/static/component/my-component.js
@@ -1,7 +1,15 @@
 const { ref } = Vue
+const { useTheme } = Vuetify
+
 export default {
   setup() {
-    return {  }
+    const theme = useTheme()
+
+    function toggleTheme () {
+      theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+    }
+
+    return { toggleTheme, theme }
   },
   template: `
     <v-app id="inspire">
@@ -9,6 +17,10 @@ export default {
        <v-app-bar-nav-icon></v-app-bar-nav-icon>
        <v-app-bar-title>Application</v-app-bar-title>
        <v-spacer></v-spacer>
+       <v-btn
+           :icon="theme.global.current.value.dark ? 'mdi-weather-night' : 'mdi-weather-sunny'"
+           @click="toggleTheme">
+       </v-btn>
        <v-btn icon="mdi-dots-vertical"></v-btn>
      </v-app-bar>
 
@@ -17,6 +29,19 @@ export default {
          <v-row>
            <v-col v-for="n in 24" :key="n" cols="4">
              <v-card height="200"></v-card>
+           </v-col>
+         </v-row>
+         <v-row>
+           <v-col cols="4">
+             <v-date-picker
+                 cancel-text="取消"
+                 header="请输入日期"
+                 input-mode="keyboard"
+                 input-placeholder="yyyy-mm-dd"
+                 input-text="请输入日期"
+                 ok-text="确定"
+                 title="请选择日期">
+             </v-date-picker>
            </v-col>
          </v-row>
        </v-container>
