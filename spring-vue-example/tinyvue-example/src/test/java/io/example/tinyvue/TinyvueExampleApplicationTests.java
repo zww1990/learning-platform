99f32d87fa66cdee8732aafb56b2941deaa12cb2
zww1990 <zww1990@foxmail.com>
2023-10-20 09:26:56 +0800

    tiny-vue example

--- spring-vue-example/tinyvue-example/src/test/java/io/example/tinyvue/TinyvueExampleApplicationTests.java
+++ spring-vue-example/tinyvue-example/src/test/java/io/example/tinyvue/TinyvueExampleApplicationTests.java
@@ -1,13 +1,25 @@
 package io.example.tinyvue;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.accept.ContentNegotiationManager;
 
 @SpringBootTest
 public class TinyvueExampleApplicationTests {
+    @Autowired
+    private ApplicationContext applicationContext;
 
-	@Test
-	public void testContextLoads() {
-	}
+    @Test
+    public void testContextLoads() {
+        System.err.println(this.applicationContext.getBean(WebMvcProperties.class)
+                .getContentnegotiation().getMediaTypes());
+        ContentNegotiationManager manager = this.applicationContext.getBean(ContentNegotiationManager.class);
+        System.err.println(manager.getAllFileExtensions());
+        System.err.println(manager.getMediaTypeMappings());
+        System.err.println(manager.getStrategies());
+    }
 
 }
