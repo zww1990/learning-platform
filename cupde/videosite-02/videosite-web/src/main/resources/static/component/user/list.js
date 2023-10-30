b3df335f780c5d53c0d963a7a0ae742e91f833d6
zww1990 <zww1990@foxmail.com>
2023-10-30 22:05:05 +0800

    优化页面

--- cupde/videosite-02/videosite-web/src/main/resources/static/component/user/list.js
+++ cupde/videosite-02/videosite-web/src/main/resources/static/component/user/list.js
@@ -1,41 +1,40 @@
 import { userListApi } from '../utils/api.js'
 
+const { message } = antd
+
 export default {
-  setup() {
-    const dataSource = [
-       {
-         key: '1',
-         name: '胡彦斌',
-         age: 32,
-         address: '西湖区湖底公园1号',
-       },
-       {
-         key: '2',
-         name: '胡彦祖',
-         age: 42,
-         address: '西湖区湖底公园1号',
-       },
-    ]
+  async setup() {
+    const res = await userListApi()
+    let dataSource = []
+    if(res.ok){
+      dataSource = await res.json()
+    }else{
+      message.error(await res.text())
+    }
     const columns = [
-      {
-        title: '姓名',
-        dataIndex: 'name',
-        key: 'name',
-      },
-      {
-        title: '年龄',
-        dataIndex: 'age',
-        key: 'age',
-      },
-      {
-        title: '住址',
-        dataIndex: 'address',
-        key: 'address',
-      },
+      { title: '主键', dataIndex: 'id', },
+      { title: '用户名', dataIndex: 'username', },
+      { title: '昵称', dataIndex: 'nickname', },
+      { title: '类型', dataIndex: 'userType', },
+      { title: '创建人', dataIndex: 'creator', },
+      { title: '创建时间', dataIndex: 'createdDate', },
+      { title: '修改人', dataIndex: 'modifier', },
+      { title: '修改时间', dataIndex: 'modifiedDate', },
     ]
     return { dataSource, columns }
   },
   template:`
-    <a-table :dataSource="dataSource" :columns="columns" />
+    <a-row>
+      <a-col :span="24">
+        <a-table
+          :dataSource="dataSource"
+          :columns="columns"
+          bordered
+          :pagination="false"
+          rowKey="id"
+          size="middle"
+        />
+      </a-col>
+    </a-row>
   `
 }
