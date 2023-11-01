63d0d5af92022d4bf1229f76e73a4a9fd252148b
zww1990 <zww1990@foxmail.com>
2023-11-01 20:48:16 +0800

    pom.xml

--- excel-example/src/main/java/io/example/demo/JavaSpringApplication.java
+++ excel-example/src/main/java/io/example/demo/JavaSpringApplication.java
@@ -1,5 +1,14 @@
 package io.example.demo;
 
+import io.example.demo.config.ApplicationProperties;
+import io.example.demo.excel.ExcelUtils;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -7,65 +16,54 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-import javax.annotation.Resource;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import io.example.demo.config.ApplicationProperties;
-import io.example.demo.excel.ExcelUtils;
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * JavaSpringApplication
- * 
+ *
  * @author zhang weiwei
- * @since 2022年8月12日,下午8:45:37
+ * @since 2022年8月12日, 下午8:45:37
  */
 @SpringBootApplication
 @Slf4j
+@AllArgsConstructor
 public class JavaSpringApplication implements CommandLineRunner {
 
-	public static void main(String[] args) throws Exception {
-		ConfigurableApplicationContext context = SpringApplication.run(JavaSpringApplication.class, args);
-		log.debug("当前容器中的bean总数={}", context.getBeanDefinitionCount());
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(JavaSpringApplication.class, args);
+        log.debug("当前容器中的bean总数={}", context.getBeanDefinitionCount());
 //		java.util.Arrays.stream(context.getBeanDefinitionNames()).forEach(System.err::println);
-	}
+    }
 
-	@Resource
-	private ApplicationProperties properties;
+    private final ApplicationProperties properties;
 
-	@Override
-	public void run(String... args) throws Exception {
-		log.info("{}", this.properties);
-		this.doMergeExcel();
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("{}", this.properties);
+        this.doMergeExcel();
+    }
 
-	private void doMergeExcel() throws Exception {
-		Path path = Paths.get(this.properties.getReadFolder());
-		if (Files.notExists(path)) {
-			log.info("正在创建文件夹[ {} ]", this.properties.getReadFolder());
-			Files.createDirectories(path);
-		}
-		List<String> fileList = Files.walk(path).filter(p -> {
-			String tmp = p.toString().toLowerCase();
-			return tmp.endsWith(".xls") || tmp.endsWith(".xlsx");
-		}).map(Path::toString).sorted().collect(Collectors.toList());
-		if (fileList.isEmpty()) {
-			log.info("文件夹[ {} ]没有待合并的工作簿", this.properties.getReadFolder());
-			return;
-		}
-		log.info("文件夹[ {} ]待合并的工作簿总个数[ {} ]", this.properties.getReadFolder(), fileList.size());
-		ExcelUtils.mergeExcel(fileList, this.properties.getWriteFolder(), this.properties.getDateTimePattern());
-		try (Scanner sc = new Scanner(System.in)) {
-			System.out.println("是否继续合并?(Y/N):");
-			if ("Y".equalsIgnoreCase(sc.next())) {
-				this.doMergeExcel();
-			} else {
-				System.out.println("程序运行结束。");
-			}
-		}
-	}
+    private void doMergeExcel() throws Exception {
+        Path path = Paths.get(this.properties.getReadFolder());
+        if (Files.notExists(path)) {
+            log.info("正在创建文件夹[ {} ]", this.properties.getReadFolder());
+            Files.createDirectories(path);
+        }
+        List<String> fileList = Files.walk(path).filter(p -> {
+            String tmp = p.toString().toLowerCase();
+            return tmp.endsWith(".xls") || tmp.endsWith(".xlsx");
+        }).map(Path::toString).sorted().collect(Collectors.toList());
+        if (fileList.isEmpty()) {
+            log.info("文件夹[ {} ]没有待合并的工作簿", this.properties.getReadFolder());
+            return;
+        }
+        log.info("文件夹[ {} ]待合并的工作簿总个数[ {} ]", this.properties.getReadFolder(), fileList.size());
+        ExcelUtils.mergeExcel(fileList, this.properties.getWriteFolder(), this.properties.getDateTimePattern());
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("是否继续合并?(Y/N):");
+            if ("Y".equalsIgnoreCase(sc.next())) {
+                this.doMergeExcel();
+            } else {
+                System.out.println("程序运行结束。");
+            }
+        }
+    }
 }
--- excel-example/src/main/java/io/example/demo/excel/ExcelUtils.java
+++ excel-example/src/main/java/io/example/demo/excel/ExcelUtils.java
@@ -1,7 +1,11 @@
 package io.example.demo.excel;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.util.StringUtils;
+
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,209 +14,196 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.DateUtil;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.util.StringUtils;
-
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * ExcelUtils
- * 
+ *
  * @author zhang weiwei
- * @since 2022年10月21日,下午7:51:12
+ * @since 2022年10月21日, 下午7:51:12
  */
 @Slf4j
 public abstract class ExcelUtils {
 
-	/**
-	 * 合并多个excel文件
-	 * 
-	 * @param fileList        excel文件路径
-	 * @param folder          目标文件保存目录
-	 * @param dateTimePattern 日期时间格式
-	 */
-	public static void mergeExcel(List<String> fileList, String folder, String dateTimePattern) {
-		// 创建新的excel工作簿
-		try (Workbook tarWorkbook = new XSSFWorkbook()) {
-			log.info("开始合并工作簿");
-			// 遍历需要合并的excel文件
-			for (int j = 0, size = fileList.size(); j < size; j++) {
-				String filePath = fileList.get(j);
-				log.info("正在读取第[ {} ]个工作簿[ {} ]", j + 1, filePath);
-				// 读取工作簿
-				try (Workbook srcWorkbook = WorkbookFactory.create(new File(filePath))) {
-					// 获取工作簿中的Sheet个数
-					int number = srcWorkbook.getNumberOfSheets();
-					log.info("工作表总个数[ {} ]", number);
-					for (int i = 0; i < number; i++) {
-						Sheet srcSheet = srcWorkbook.getSheetAt(i);
-						log.info("正在读取工作表[ {} ]", srcSheet.getSheetName());
-						Sheet tarSheet = tarWorkbook.getSheet(srcSheet.getSheetName());
-						boolean isNew = tarSheet == null;
-						if (isNew) {
-							log.info("正在创建工作表[ {} ]", srcSheet.getSheetName());
-							tarSheet = tarWorkbook.createSheet(srcSheet.getSheetName());
-						}
-						try {
-							// 复制sheet内容
-							copyExcelSheet(tarWorkbook, srcSheet, tarSheet, isNew);
-							log.info("工作表[ {} ]已合并", tarSheet.getSheetName());
-						} catch (Exception e) {
-							log.error(e.getLocalizedMessage(), e);
-						}
-					}
-				}
-			}
-			Path path = Paths.get(folder);
-			if (Files.notExists(path)) {
-				log.info("正在创建文件夹[ {} ]", folder);
-				Files.createDirectory(path);
-			}
-			// 新生成的excel文件
-			String dateTime = DateTimeFormatter.ofPattern(dateTimePattern).format(LocalDateTime.now());
-			String fullPath = String.format("%s%s%s.xlsx", folder, File.separator, dateTime);
-			File file = new File(fullPath);
-			// 判断文件是否存在
-			if (file.exists()) {
-				log.info("正在删除工作簿[ {} ]", fullPath);
-				// 存在则删除
-				file.delete();
-			}
-			// 使用输出流写出
-			try (OutputStream out = new FileOutputStream(file)) {
-				log.info("正在写入工作簿[ {} ]", fullPath);
-				tarWorkbook.write(out);
-				out.flush();
-			}
-			log.info("工作簿合并完成");
-		} catch (Exception e) {
-			log.error(e.getLocalizedMessage(), e);
-		}
-	}
+    /**
+     * 合并多个excel文件
+     *
+     * @param fileList        excel文件路径
+     * @param folder          目标文件保存目录
+     * @param dateTimePattern 日期时间格式
+     */
+    public static void mergeExcel(List<String> fileList, String folder, String dateTimePattern) {
+        // 创建新的excel工作簿
+        try (Workbook tarWorkbook = new XSSFWorkbook()) {
+            log.info("开始合并工作簿");
+            // 遍历需要合并的excel文件
+            for (int j = 0, size = fileList.size(); j < size; j++) {
+                String filePath = fileList.get(j);
+                log.info("正在读取第[ {} ]个工作簿[ {} ]", j + 1, filePath);
+                // 读取工作簿
+                try (Workbook srcWorkbook = WorkbookFactory.create(new File(filePath))) {
+                    // 获取工作簿中的Sheet个数
+                    int number = srcWorkbook.getNumberOfSheets();
+                    log.info("工作表总个数[ {} ]", number);
+                    for (int i = 0; i < number; i++) {
+                        Sheet srcSheet = srcWorkbook.getSheetAt(i);
+                        log.info("正在读取工作表[ {} ]", srcSheet.getSheetName());
+                        Sheet tarSheet = tarWorkbook.getSheet(srcSheet.getSheetName());
+                        boolean isNew = tarSheet == null;
+                        if (isNew) {
+                            log.info("正在创建工作表[ {} ]", srcSheet.getSheetName());
+                            tarSheet = tarWorkbook.createSheet(srcSheet.getSheetName());
+                        }
+                        try {
+                            // 复制sheet内容
+                            copyExcelSheet(tarWorkbook, srcSheet, tarSheet, isNew);
+                            log.info("工作表[ {} ]已合并", tarSheet.getSheetName());
+                        } catch (Exception e) {
+                            log.error(e.getLocalizedMessage(), e);
+                        }
+                    }
+                }
+            }
+            Path path = Paths.get(folder);
+            if (Files.notExists(path)) {
+                log.info("正在创建文件夹[ {} ]", folder);
+                Files.createDirectory(path);
+            }
+            // 新生成的excel文件
+            String dateTime = DateTimeFormatter.ofPattern(dateTimePattern).format(LocalDateTime.now());
+            String fullPath = String.format("%s%s%s.xlsx", folder, File.separator, dateTime);
+            File file = new File(fullPath);
+            // 判断文件是否存在
+            if (file.exists()) {
+                log.info("正在删除工作簿[ {} ]", fullPath);
+                // 存在则删除
+                file.delete();
+            }
+            // 使用输出流写出
+            try (OutputStream out = Files.newOutputStream(file.toPath())) {
+                log.info("正在写入工作簿[ {} ]", fullPath);
+                tarWorkbook.write(out);
+                out.flush();
+            }
+            log.info("工作簿合并完成");
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage(), e);
+        }
+    }
 
-	/**
-	 * 合并单元格
-	 * 
-	 * @param srcSheet 来源sheet
-	 * @param tarSheet 目标sheet
-	 */
+    /**
+     * 合并单元格
+     *
+     * @param srcSheet 来源sheet
+     * @param tarSheet 目标sheet
+     */
 //	private static void mergeSheetAllRegion(Sheet srcSheet, Sheet tarSheet) {
 //		for (int i = 0, num = srcSheet.getNumMergedRegions(); i < num; i++) {
 //			tarSheet.addMergedRegion(srcSheet.getMergedRegion(i));
 //		}
 //	}
 
-	/**
-	 * 复制sheet到新的excel文件中
-	 * 
-	 * @param workbook excel工作簿
-	 * @param srcSheet 来源sheet
-	 * @param tarSheet 目标sheet
-	 * @param isNew    是否新建的sheet
-	 */
-	private static void copyExcelSheet(Workbook workbook, Sheet srcSheet, Sheet tarSheet, boolean isNew) {
-		// 合并单元格
+    /**
+     * 复制sheet到新的excel文件中
+     *
+     * @param workbook excel工作簿
+     * @param srcSheet 来源sheet
+     * @param tarSheet 目标sheet
+     * @param isNew    是否新建的sheet
+     */
+    private static void copyExcelSheet(Workbook workbook, Sheet srcSheet, Sheet tarSheet, boolean isNew) {
+        // 合并单元格
 //		mergeSheetAllRegion(srcSheet, tarSheet);
-		// 获取最后一个单元格位置
-		int cellNum = srcSheet.getRow(srcSheet.getFirstRowNum()).getLastCellNum();
-		for (int i = 0; i < cellNum; i++) {
-			// 设置单元格列宽度
-			tarSheet.setColumnWidth(i, srcSheet.getColumnWidth(i));
-		}
-		// 复制每行内容
-		for (Row srcRow : srcSheet) {
-			int rowNum;
-			if (isNew) {
-				rowNum = srcRow.getRowNum();
-			} else {
+        // 获取最后一个单元格位置
+        int cellNum = srcSheet.getRow(srcSheet.getFirstRowNum()).getLastCellNum();
+        for (int i = 0; i < cellNum; i++) {
+            // 设置单元格列宽度
+            tarSheet.setColumnWidth(i, srcSheet.getColumnWidth(i));
+        }
+        // 复制每行内容
+        for (Row srcRow : srcSheet) {
+            int rowNum;
+            if (isNew) {
+                rowNum = srcRow.getRowNum();
+            } else {
 //				if (srcRow.getRowNum() == 0) {
 //					// 从第二个工作簿开始，遇到表头行，直接跳过
 //					continue;
 //				}
-				rowNum = tarSheet.getLastRowNum() + 1;
-			}
-			// 创建新行
-			Row tarRow = tarSheet.createRow(rowNum);
-			// 复制行
-			copyExcelRow(workbook, srcRow, tarRow, tarSheet);
-		}
-	}
+                rowNum = tarSheet.getLastRowNum() + 1;
+            }
+            // 创建新行
+            Row tarRow = tarSheet.createRow(rowNum);
+            // 复制行
+            copyExcelRow(workbook, srcRow, tarRow, tarSheet);
+        }
+    }
 
-	/**
-	 * 复制excel中的行到新的sheet中
-	 * 
-	 * @param workbook 目标工作簿
-	 * @param srcRow   来源excel行
-	 * @param tarRow   目标excel行
-	 * @param tarSheet 目标sheet
-	 */
-	private static void copyExcelRow(Workbook workbook, Row srcRow, Row tarRow, Sheet tarSheet) {
-		// 设置行高
-		tarRow.setHeight(srcRow.getHeight());
-		// 是否有值
-		boolean hasValue = false;
-		// 获取所有列
-		for (Cell srcCell : srcRow) {
-			// 创建单元格
-			Cell tarCell = tarRow.createCell(srcCell.getColumnIndex());
-			// 复制单元格
-			CellStyle cellStyle = workbook.createCellStyle();
-			// 复制单元格样式
-			cellStyle.cloneStyleFrom(srcCell.getCellStyle());
-			// 单元格样式
-			tarCell.setCellStyle(cellStyle);
-			if (srcCell.getCellComment() != null) {
-				tarCell.setCellComment(srcCell.getCellComment());
-			}
-			// 不同数据类型处理
-			CellType cellType = srcCell.getCellTypeEnum();
-			tarCell.setCellType(cellType);
-			if (cellType == CellType.NUMERIC) {
-				if (DateUtil.isCellDateFormatted(srcCell)) {
-					tarCell.setCellValue(srcCell.getDateCellValue());
-					if (tarCell.getDateCellValue() != null) {
-						hasValue = true;
-					}
-				} else {
-					tarCell.setCellValue(srcCell.getNumericCellValue());
-					if (tarCell.getNumericCellValue() != 0) {
-						hasValue = true;
-					}
-				}
-			} else if (cellType == CellType.STRING) {
-				tarCell.setCellValue(srcCell.getRichStringCellValue());
-				if (tarCell.getRichStringCellValue().length() != 0) {
-					hasValue = true;
-				}
-			} else if (cellType == CellType.BOOLEAN) {
-				tarCell.setCellValue(srcCell.getBooleanCellValue());
-				if (tarCell.getBooleanCellValue()) {
-					hasValue = true;
-				}
-			} else if (cellType == CellType.ERROR) {
-				tarCell.setCellErrorValue(srcCell.getErrorCellValue());
-				if (tarCell.getErrorCellValue() != 0) {
-					hasValue = true;
-				}
-			} else if (cellType == CellType.FORMULA) {
-				tarCell.setCellFormula(srcCell.getCellFormula());
-				if (StringUtils.hasText(tarCell.getCellFormula())) {
-					hasValue = true;
-				}
-			}
-		}
-		if (!hasValue) {
-			// 当前行没有一个值，删除当前空白行
-			tarSheet.removeRow(tarRow);
-		}
-	}
+    /**
+     * 复制excel中的行到新的sheet中
+     *
+     * @param workbook 目标工作簿
+     * @param srcRow   来源excel行
+     * @param tarRow   目标excel行
+     * @param tarSheet 目标sheet
+     */
+    private static void copyExcelRow(Workbook workbook, Row srcRow, Row tarRow, Sheet tarSheet) {
+        // 设置行高
+        tarRow.setHeight(srcRow.getHeight());
+        // 是否有值
+        boolean hasValue = false;
+        // 获取所有列
+        for (Cell srcCell : srcRow) {
+            // 创建单元格
+            Cell tarCell = tarRow.createCell(srcCell.getColumnIndex());
+            // 复制单元格
+            CellStyle cellStyle = workbook.createCellStyle();
+            // 复制单元格样式
+            cellStyle.cloneStyleFrom(srcCell.getCellStyle());
+            // 单元格样式
+            tarCell.setCellStyle(cellStyle);
+            if (srcCell.getCellComment() != null) {
+                tarCell.setCellComment(srcCell.getCellComment());
+            }
+            // 不同数据类型处理
+            CellType cellType = srcCell.getCellTypeEnum();
+            tarCell.setCellType(cellType);
+            if (cellType == CellType.NUMERIC) {
+                if (DateUtil.isCellDateFormatted(srcCell)) {
+                    tarCell.setCellValue(srcCell.getDateCellValue());
+                    if (tarCell.getDateCellValue() != null) {
+                        hasValue = true;
+                    }
+                } else {
+                    tarCell.setCellValue(srcCell.getNumericCellValue());
+                    if (tarCell.getNumericCellValue() != 0) {
+                        hasValue = true;
+                    }
+                }
+            } else if (cellType == CellType.STRING) {
+                tarCell.setCellValue(srcCell.getRichStringCellValue());
+                if (tarCell.getRichStringCellValue().length() != 0) {
+                    hasValue = true;
+                }
+            } else if (cellType == CellType.BOOLEAN) {
+                tarCell.setCellValue(srcCell.getBooleanCellValue());
+                if (tarCell.getBooleanCellValue()) {
+                    hasValue = true;
+                }
+            } else if (cellType == CellType.ERROR) {
+                tarCell.setCellErrorValue(srcCell.getErrorCellValue());
+                if (tarCell.getErrorCellValue() != 0) {
+                    hasValue = true;
+                }
+            } else if (cellType == CellType.FORMULA) {
+                tarCell.setCellFormula(srcCell.getCellFormula());
+                if (StringUtils.hasText(tarCell.getCellFormula())) {
+                    hasValue = true;
+                }
+            }
+        }
+        if (!hasValue) {
+            // 当前行没有一个值，删除当前空白行
+            tarSheet.removeRow(tarRow);
+        }
+    }
 
 }
