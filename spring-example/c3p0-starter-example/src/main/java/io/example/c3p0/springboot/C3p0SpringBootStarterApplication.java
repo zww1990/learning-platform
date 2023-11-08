01775b931f91337eccc2d484a539d9945942bac1
zww1990 <zww1990@foxmail.com>
2023-11-08 14:37:05 +0800

    updated spring boot version

--- spring-example/c3p0-starter-example/src/main/java/io/example/c3p0/springboot/C3p0SpringBootStarterApplication.java
+++ spring-example/c3p0-starter-example/src/main/java/io/example/c3p0/springboot/C3p0SpringBootStarterApplication.java
@@ -1,19 +1,21 @@
 package io.example.c3p0.springboot;
 
+import io.gitlab.zww1990.c3p0.springboot.autoconfigure.C3p0DataSourceAutoConfigure;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(C3p0DataSourceAutoConfigure.class)
 @Slf4j
 public class C3p0SpringBootStarterApplication {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(C3p0SpringBootStarterApplication.class, args);
-		log.info("Get Bean Definition Count = {}", context.getBeanDefinitionCount());
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(C3p0SpringBootStarterApplication.class, args);
+        log.info("Get Bean Definition Count = {}", context.getBeanDefinitionCount());
 //		java.util.stream.Stream.of(context.getBeanDefinitionNames()).forEach(System.err::println);
-	}
+    }
 
 }
--- spring-example/c3p0-starter-example/src/test/java/io/example/c3p0/springboot/C3p0SpringBootStarterApplicationTests.java
+++ spring-example/c3p0-starter-example/src/test/java/io/example/c3p0/springboot/C3p0SpringBootStarterApplicationTests.java
@@ -2,6 +2,7 @@ package io.example.c3p0.springboot;
 
 import java.time.LocalDateTime;
 
+import io.gitlab.zww1990.c3p0.springboot.autoconfigure.C3p0DataSourceAutoConfigure;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +16,8 @@ public class C3p0SpringBootStarterApplicationTests {
 
 	@Test
 	public void testContextLoads() {
+		C3p0DataSourceAutoConfigure configure = context.getBean(C3p0DataSourceAutoConfigure.class);
+		System.err.println(configure.getClass());
 		JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
 		System.err.println(jdbcTemplate.queryForObject("select now()", LocalDateTime.class));
 	}
