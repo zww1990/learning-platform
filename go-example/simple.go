422bdc579d98b3b6a094589c6e7e8022df7552af
zww1990 <zww1990@foxmail.com>
2023-11-10 16:10:36 +0800

    学习go的第一天

--- go-example/simple.go
+++ go-example/simple.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
 
 func main() {
 	fmt.Println("Java是世界上最强大的编程语言，没有之一。")
+	fmt.Println(runtime.Version())
+	fmt.Println(os.UserHomeDir())
+	fmt.Println(os.Hostname())
 }
