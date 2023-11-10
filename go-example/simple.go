90568183e08a2e47b54bd450b3b4b9401228cebf
zww1990 <zww1990@foxmail.com>
2023-11-10 16:16:57 +0800

    学习go的第一天

--- go-example/simple.go
+++ go-example/simple.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	fmt.Println("Java是世界上最强大的编程语言，没有之一。")
 	fmt.Println(runtime.Version())
+	fmt.Println(runtime.GOOS)
 	fmt.Println(os.UserHomeDir())
 	fmt.Println(os.Hostname())
+	fmt.Println(os.Getenv("path"))
 }
