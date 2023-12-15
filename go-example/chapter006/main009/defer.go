132c0d015825dec8579f8d5989a1a1cec686bc2e
zww1990 <zww1990@foxmail.com>
2023-12-15 16:39:54 +0800

    学习go的第七天

---
+++ go-example/chapter006/main009/defer.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	function1()
+}
+
+func function1() {
+	fmt.Printf("在顶部的函数1中\n")
+	defer function2()
+	fmt.Printf("在底部的函数1中！\n")
+}
+
+func function2() {
+	fmt.Printf("函数2：延迟到调用函数结束！")
+}
