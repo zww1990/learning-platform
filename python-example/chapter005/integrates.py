83a6880e5997e47887f2c80a379915d276272c8f
zww1990 <zww1990@foxmail.com>
2023-12-21 10:01:35 +0800

    Sign Request

--- python-example/chapter005/integrates.py
+++ python-example/chapter005/integrates.py
@@ -3,4 +3,5 @@ import signrequest
 if __name__ == '__main__':
     sign_request = signrequest.SignRequest()
     params = {'current': 1, 'pageSize': 500}
-    resp = sign_request.get("/open-api/organization/v2/companies", params)
+    # resp = sign_request.get("/open-api/organization/v2/companies", params)
+    resp = sign_request.get("/open-api/supos/oodm/v2/template/system/mes/instances", {})
