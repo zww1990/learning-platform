a675ba54284c8546d871c5af9bbe6c56334ef28b
zww1990 <zww1990@foxmail.com>
2023-12-21 10:02:27 +0800

    Sign Request

--- python-example/chapter005/integrates.py
+++ python-example/chapter005/integrates.py
@@ -1,6 +1,7 @@
 import signrequest
 
 if __name__ == '__main__':
+    # 接口调用测试
     sign_request = signrequest.SignRequest()
     params = {'current': 1, 'pageSize': 500}
     # resp = sign_request.get("/open-api/organization/v2/companies", params)
