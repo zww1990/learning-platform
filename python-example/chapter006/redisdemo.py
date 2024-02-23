6ddc448f4a3c0457ad9f1881e65a6d63be0c1fff
zww1990 <zww1990@foxmail.com>
2024-02-23 15:58:51 +0800

    连接redis：读写字符串、读写哈希表、读写列表、读写集合。

--- python-example/chapter006/redisdemo.py
+++ python-example/chapter006/redisdemo.py
@@ -7,9 +7,20 @@ if __name__ == '__main__':
                       decode_responses=True)
 
     res = rdb.execute_command('info')
-
     print('redis_version:', res['redis_version'])
 
-    rdb.set('applicationcontext', 'hello 王五')
-
+    # 读写字符串
+    # rdb.set('applicationcontext', 'hello 王五')
     print('applicationcontext:', rdb.get('applicationcontext'))
+
+    # 读写哈希表
+    # rdb.hset('users', '1001', '李四')
+    print('users:', rdb.hgetall('users'))
+
+    # 读写列表
+    # rdb.lpush('computer', 'alienware', 'msi', 'rog')
+    print('computer:', rdb.lrange('computer', 0, -1))
+
+    # 读写集合
+    # rdb.sadd('onepiece', '路飞', '娜美', '索隆')
+    print('onepiece:', rdb.smembers('onepiece'))
