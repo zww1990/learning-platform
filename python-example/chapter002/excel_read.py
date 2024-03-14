48213baa9b8cb726d5fbb5c2dfa8a6df2b24ccf7
zww1990 <zww1990@foxmail.com>
2024-03-14 14:53:02 +0800

    excel read

---
+++ python-example/chapter002/excel_read.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import json
+
+if __name__ == '__main__':
+    full_name = "D:\\工作文档\\siloposition.xlsx"
+    df = pd.read_excel(full_name, header=None, sheet_name=None)
+    values = df.get('Sheet2').values
+    newList = [dict(zip(values[0], item)) for item in values[1:]]
+    print(json.dumps(newList, indent=2))
