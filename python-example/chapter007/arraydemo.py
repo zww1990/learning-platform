bdce44c570205219fb58359b05b81f40e95f2481
zww1990 <zww1990@foxmail.com>
2024-03-20 14:34:45 +0800

    array demo

--- python-example/chapter007/arraydemo.py
+++ python-example/chapter007/arraydemo.py
@@ -1,4 +1,7 @@
-if __name__ == '__main__':
+import json
+
+
+def array_extend_demo():
     arr1 = []
     arr2 = [1, 2, 3]
     arr3 = [4, 5, 6]
@@ -7,3 +10,14 @@ if __name__ == '__main__':
     arr1.extend(arr2)
     arr1.extend(arr3)
     print(arr1)
+
+
+def dict_zip_for_in_enumerate_demo():
+    data = [11, 22, 33, 44, 55, 66, 77, 88, 99, 1010]
+    newlist = [dict(zip(['read_add', 'read_value'], [index + 30001, item])) for index, item in enumerate(data)]
+    print(json.dumps(newlist, indent=2))
+
+
+if __name__ == '__main__':
+    # array_extend_demo()
+    dict_zip_for_in_enumerate_demo()
