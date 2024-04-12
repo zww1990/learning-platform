f328216614af8d66cb9050f92e0149fd5b40db7a
zww1990 <zww1990@foxmail.com>
2024-04-12 11:06:38 +0800

    read excel

--- python-example/chapter002/excel_read.py
+++ python-example/chapter002/excel_read.py
@@ -2,9 +2,10 @@ import pandas as pd
 import json
 
 if __name__ == '__main__':
-    full_name = "D:\\工作文档\\siloposition.xlsx"
+    full_name = "D:\\工作文档\\水一方筒仓地址.xlsx"
     df = pd.read_excel(full_name, header=None, sheet_name=None)
-    values = df.get('Sheet2').values
-    newList = [dict(zip(values[0], item)) for item in values[1:]]
+    # 累计流量 # 瞬时流量 # 计划流量 # 断料报警 # 皮带跑偏 # 皮带秤故障
+    values = df.get('皮带秤故障').values
+    newList = [dict(zip(['dataAddress', 'dataType', 'name'], [item[1], item[3], item[2]])) for item in values[1:]]
     print(len(newList))
-    print(json.dumps(newList, indent=2))
+    print(json.dumps(newList, indent=2, ensure_ascii=False))
