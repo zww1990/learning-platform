28fda95044c2ffd2f824e8b2d8145c8a7967038c
zww1990 <zww1990@foxmail.com>
2024-07-03 16:03:38 +0800

    helloworld.py

--- python-example/chapter001/helloworld.py
+++ python-example/chapter001/helloworld.py
@@ -5,21 +5,31 @@ def say_hello(text):
         say_hello(input('请输入你的内容: '))
 
 
-def ifelse(var1, var2):
+def ifelse(var1, var2, var3):
     if var1 is None:
         print(f'var1 is {var1} !!!')
     elif var2 is None:
         print(f'var2 is {var2} !!!')
+    elif var3 is None:
+        print(f'var3 is {var3} !!!')
     else:
-        print(f'var1 = {var1}, var2 = {var2}')
+        print(f'var1 = {var1}, var2 = {var2}, var3 = {var3}')
 
 
 if __name__ == '__main__':
     # say_hello(input('请输入你的内容: '))
-    ifelse(None, 2)
+    ifelse(None, None, None)
     print('--------------------------')
-    ifelse(1, None)
+    ifelse(None, None, 3)
     print('--------------------------')
-    ifelse(None, None)
+    ifelse(None, 2, None)
     print('--------------------------')
-    ifelse(1, 2)
+    ifelse(1, None, None)
+    print('--------------------------')
+    ifelse(None, 2, 3)
+    print('--------------------------')
+    ifelse(1, None, 3)
+    print('--------------------------')
+    ifelse(1, 2, None)
+    print('--------------------------')
+    ifelse(1, 2, 3)
