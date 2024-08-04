ecbe31b3ba01afcfd882f3e5a2a16a148ac65ccc
zww1990 <zww1990@foxmail.com>
2024-08-04 14:11:09 +0800

    beijing_subway

--- neo4j-example/postgresql/select.sql
+++ neo4j-example/postgresql/select.sql
@@ -1,10 +1,12 @@
-WITH RECURSIVE paths (start_station, stop_station, stops, path) AS (
-    SELECT station_name, next_station, 1, ARRAY[station_name::text, next_station::text]
-    FROM beijing_subway WHERE station_name = '阜成门'
+WITH RECURSIVE transfer (start_station, stop_station, stops, path) AS (
+    SELECT station_name, next_station, 1 as stops, ARRAY[station_name::text, next_station::text] as path
+    FROM beijing_subway
+    WHERE station_name = '阜成门'
     UNION ALL
-    SELECT p.start_station, e.next_station, p.stops + 1, p.path || ARRAY[e.next_station::text]
-    FROM paths p
-             JOIN beijing_subway e
-                  ON p.stop_station = e.station_name AND NOT e.next_station = ANY(p.path)
+    SELECT t.start_station, s.next_station, (t.stops + 1) as stops, (t.path || ARRAY[s.next_station::text]) as path
+    FROM transfer as t
+             JOIN beijing_subway as s
+                  ON t.stop_station = s.station_name AND NOT s.next_station = ANY(t.path)
 )
-SELECT * FROM paths WHERE stop_station = '丰台科技园' limit 10;
+SELECT * FROM transfer
+WHERE stop_station = '丰台科技园';
