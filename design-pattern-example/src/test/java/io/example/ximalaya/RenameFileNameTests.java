fb8e87c95ebd3d0305a83ebdc0e08c34b4bb4817
zww1990 <zww1990@foxmail.com>
2024-08-31 20:08:23 +0800

    喜马拉雅

---
+++ design-pattern-example/src/test/java/io/example/ximalaya/RenameFileNameTests.java
@@ -0,0 +1,92 @@
+package io.example.ximalaya;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class RenameFileNameTests {
+    public static void main(String[] args) throws Exception {
+        // 下载时的顺序：001 -> 360
+        ClassPathResource downloadfile = new ClassPathResource("ximalaya/download.txt");
+        // 播放列表的顺序：360 -> 001
+        ClassPathResource playlistfile = new ClassPathResource("ximalaya/playlist.json");
+        if (downloadfile.exists() && playlistfile.exists()) {
+            List<String> filenames = Files.readAllLines(Paths.get(downloadfile.getURI()))
+                    .stream()
+                    .map(line -> line.substring(line.lastIndexOf("/") + 1, line.indexOf("?")))
+                    .toList();
+            Gson gson = new Gson();
+            String jsonString = playlistfile.getContentAsString(StandardCharsets.UTF_8);
+            List<Player> players = gson.fromJson(jsonString, new TypeToken<List<Player>>(){}.getType());
+            if (filenames.size() != players.size()) {
+                System.err.println(filenames.size());
+                System.err.println(players.size());
+                System.err.println("总数不一致！");
+            } else {
+                // 对播放列表进行排序：001 -> 360
+                players.sort(Comparator.comparingInt(Player::getTrackId));
+                for (int i = 0, size = filenames.size(); i < size; i++) {
+                    Player player = players.get(i);
+                    String fileName = filenames.get(i);
+                    player.setFileName(fileName);
+                    player.setFileExt(StringUtils.getFilenameExtension(fileName));
+//                    System.err.println(player);
+                }
+                // 将list转成map，key是文件名，value是对象
+                Map<String, Player> playerMap = players.stream().collect(
+                        Collectors.toMap(Player::getFileName, Function.identity()));
+                String dir = "d:\\影视\\阿尔法胎教音乐";
+                try (Stream<Path> stream = Files.list(Paths.get(dir))) {
+                    stream.forEach(path -> {
+                        String s = path.toString();
+                        String filename = s.substring(s.lastIndexOf("\\") + 1);
+                        Optional.ofNullable(playerMap.get(filename))
+                                .ifPresentOrElse(player -> {
+                                    try {
+                                        // 文件重命名
+                                        Path target = Files.move(path, Paths.get(dir,
+                                                player.getTrackName() + "." + player.getFileExt()));
+                                        System.err.println("重命名\t" + target);
+                                    } catch (IOException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                },() -> System.err.println(filename + "\t文件已重命名"));
+                    });
+                }
+                System.err.println("done");
+            }
+        } else {
+            System.err.println(downloadfile);
+            System.err.println(playlistfile);
+            System.err.println("文件不存在！");
+        }
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class Player {
+        private Integer index;
+        private Integer trackId;
+        private String trackName;
+        private String fileName;
+        private String fileExt;
+    }
+}
