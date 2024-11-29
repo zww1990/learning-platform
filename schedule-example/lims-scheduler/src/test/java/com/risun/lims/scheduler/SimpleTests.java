20500d29e9f3a862ac7259809b6fe17a3db83573
zww1990 <zww1990@foxmail.com>
2024-11-29 10:00:52 +0800

    modified

--- schedule-example/lims-scheduler/src/test/java/com/risun/lims/scheduler/SimpleTests.java
+++ schedule-example/lims-scheduler/src/test/java/com/risun/lims/scheduler/SimpleTests.java
@@ -2,11 +2,13 @@ package com.risun.lims.scheduler;
 
 import org.junit.jupiter.api.Test;
 
+import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class SimpleTests {
     @Test
     public void testTimeUnit() {
         System.err.println(SECONDS.toMillis(5));
+        System.err.println(MINUTES.toMillis(10));
     }
 }
