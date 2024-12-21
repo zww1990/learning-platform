201f23d391f69590140ab2f7edccadb4bde07725
zww1990 <zww1990@foxmail.com>
2024-12-21 15:49:33 +0800

    day1.py

--- python-example/chapter010/day1.py
+++ python-example/chapter010/day1.py
@@ -41,7 +41,17 @@ def simulate(session):
     headers = { 'Content-Type': 'application/json' }
     response = session.post('https://api.worldquantbrain.com/simulations', data=params, headers=headers)
     print(response.status_code)
-    print(response.headers['Location'])
+    sim_progress_url = response.headers['Location']
+    print(sim_progress_url)
+
+    while True:
+        sim_progress_resp = session.get(sim_progress_url)
+        retry_after_sec = float(sim_progress_resp.headers.get('Retry-After', 0))
+        if retry_after_sec == 0:
+            break
+        sleep(retry_after_sec)
+    alpha_id = sim_progress_resp.json()['alpha']
+    print(alpha_id)
 
 if __name__ == '__main__':
     simulate(login())
