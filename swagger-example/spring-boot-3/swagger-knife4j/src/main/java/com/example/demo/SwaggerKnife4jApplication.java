cd6876a7084cf1a9230b0573f3e42a9ed4adf86e
zww1990 <zww1990@foxmail.com>
2024-12-23 15:35:50 +0800

    初始化工程

---
+++ swagger-example/spring-boot-3/swagger-knife4j/src/main/java/com/example/demo/SwaggerKnife4jApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerKnife4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerKnife4jApplication.class, args);
+	}
+
+}
---
+++ swagger-example/spring-boot-3/swagger-openapi/src/main/java/com/example/demo/SwaggerOpenapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerOpenapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerOpenapiApplication.class, args);
+	}
+
+}
