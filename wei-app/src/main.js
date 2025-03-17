369e62e90a3c8232066f04c386650e54c8725fce
zww1990 <zww1990@foxmail.com>
2025-03-17 17:02:06 +0800

    update main.js

--- wei-app/src/main.js
+++ wei-app/src/main.js
@@ -145,14 +145,14 @@ app.whenReady().then(() => {
     { label: '退出', type: 'normal', role: 'quit' },
   ])
   tray.setContextMenu(contextMenu)
+});
 
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
+// On OS X it's common to re-create a window in the app when the
+// dock icon is clicked and there are no other windows open.
+app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+        createWindow();
     }
-  });
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
