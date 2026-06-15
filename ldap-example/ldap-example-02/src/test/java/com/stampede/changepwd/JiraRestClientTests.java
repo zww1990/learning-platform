207c75542e426d95f5d07ff334af97b939fa6976
维维 <zww1990@foxmail.com>
2026-06-15 11:15:00 +0800

    Update JiraRestClientTests.java

--- ldap-example/ldap-example-02/src/test/java/com/stampede/changepwd/JiraRestClientTests.java
+++ ldap-example/ldap-example-02/src/test/java/com/stampede/changepwd/JiraRestClientTests.java
@@ -15,9 +15,9 @@ import com.atlassian.jira.rest.client.internal.async.DisposableHttpClient;
 public class JiraRestClientTests {
 	@Test
 	public void testJiraRestClient() {
-		String url = "http://jira.bacic5i5j.com/";
-		String username = "siqianwen";
-		String password = "ainilaoma1234A";
+		String url = "";
+		String username = "";
+		String password = "";
 		try (JiraRestClient client = new AsynchronousJiraRestClientFactory()
 				.createWithBasicHttpAuthentication(URI.create(url), username, password)) {
 			System.err.println(client.getUserClient().getUser(username).get());
@@ -30,10 +30,10 @@ public class JiraRestClientTests {
 	public void testAsynchronousHttpClient() {
 		DisposableHttpClient client = null;
 		try {
-			String url = "http://jira.bacic5i5j.com/";
-			String username = "siqianwen";
-			String password = "ainilaoma1234A";
-			String syncUrl = "http://jira.bacic5i5j.com/rest/crowd/1/directory/10000";
+			String url = "";
+			String username = "";
+			String password = "";
+			String syncUrl = "";
 			client = new AsynchronousHttpClientFactory().createClient(URI.create(url),
 					new BasicHttpAuthenticationHandler(username, password));
 			Response response = client.newRequest(syncUrl).get().get();
